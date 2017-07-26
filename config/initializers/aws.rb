792e0aba185a51d35f47e5448e1d42116a44d9e2
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-07-26 10:13:53 +0200

    Renamed AWS_REGION constant

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 Aws.config.update({
-  region: ENV["S3_BUCKET_REGION"],
+  region: ENV["AWS_REGION"],
   credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"]),
   endpoint: "https://s3.amazonaws.com"
 })
 
-S3_BUCKET_URL = "https://s3-#{ENV["S3_BUCKET_REGION"]}.amazonaws.com/#{ENV["S3_BUCKET_NAME"]}"
+S3_BUCKET_URL = "https://s3-#{ENV["AWS_REGION"]}.amazonaws.com/#{ENV["S3_BUCKET_NAME"]}"
