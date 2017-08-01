a8665a453b76f393f1a7e5e4ed7e93fac0c5e51f
vectorsize <vectorsize@gmail.com>
2017-08-01 18:58:59 +0200

    WIP components with state and styles

--- app/javascript/app/components/header/header-component.jsx
+++ app/javascript/app/components/header/header-component.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 // import { NavLink } from 'react-router-dom'
 
 import styles from './header-styles.scss'
+import uiStyles from 'styles/ui'
 // import routes from 'app/routes'
 import CountryExplorer from 'components/country-explorer'
 import COUNTRIES from 'data/countries'
 
 export default ({ toggleCountryExplorer }) => (
   <div className={styles.header}>
-    <h2 onClick={() => toggleCountryExplorer()}>Countries</h2>
-    <CountryExplorer countries={COUNTRIES} />
+    <div className={uiStyles.button}>
+      <span onClick={() => toggleCountryExplorer()}>Countries</span>
+      <CountryExplorer countries={COUNTRIES} />
+    </div>
   </div>
 )
