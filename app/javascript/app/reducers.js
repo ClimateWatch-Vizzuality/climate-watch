298a10d2baa5b571b3b8e2ed9cb99b4fcdae0a0e
vectorsize <vectorsize@gmail.com>
2017-08-02 15:38:03 +0200

    linting test

--- app/javascript/app/reducers.js
+++ app/javascript/app/reducers.js
@@ -3,6 +3,7 @@ import { routerReducer as router } from 'react-router-redux'
 import { handleActions } from 'app/utils/redux'
 
 import { reducers as countryExplorerReducers } from 'components/country-explorer'
+
 import initialState from './data/initial-state'
 import allActions from './actions'
 
