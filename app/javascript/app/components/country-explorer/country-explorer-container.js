5c47d7cc9143632bef9c334edc756ff5d70db221
vectorsize <vectorsize@gmail.com>
2017-08-02 13:35:11 +0200

    add clickoutside HOC

--- app/javascript/app/components/country-explorer/country-explorer-container.js
+++ app/javascript/app/components/country-explorer/country-explorer-container.js
@@ -1,7 +1,20 @@
+import React, { Component } from 'react'
+import { clickOutside } from 'utils/react'
 import { connect } from 'react-redux'
-import Component from './country-explorer-component'
+import CountryExplorer from './country-explorer-component'
 import { default as actions } from './country-explorer-actions'
+
+class CountryExplorerContainer extends Component {
+  componentWillReceiveProps (nextProps) {
+    const { clickedOutside, closeCountryExplorer } = nextProps
+    if (clickedOutside) closeCountryExplorer()
+  }
+
+  render () {
+    return React.createElement(CountryExplorer, this.props)
+  }
+}
 
 const mapStateToProps = state => state.countryExplorer
 
-export default connect(mapStateToProps, actions)(Component)
+export default connect(mapStateToProps, actions)(clickOutside(CountryExplorerContainer))
---
+++ app/javascript/app/utils/react.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export const clickOutside = Comp => class ClickOutside extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      clickedOutside: false
+    }
+  }
+
+  componentDidMount () {
+    document.addEventListener('click', this.handle, true)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('click', this.handle, true)
+  }
+
+  componentDidUpdate () {
+    this.state = { clickedOutside: false }
+  }
+
+  handle = e => {
+    const el = this.container
+    if (!el.contains(e.target)) this.setState({ clickedOutside: true })
+  };
+
+  render () {
+    const { clickedOutside } = this.state
+    const getRef = el => this.container = el
+
+    return (
+      <div ref={getRef}>
+        <Comp {...{...this.props, clickedOutside}} />
+      </div>
+    )
+  }
+}
