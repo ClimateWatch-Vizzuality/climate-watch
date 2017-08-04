00cad3acefaaea887fc2518c42d71e20c3939b2c
vectorsize <vectorsize@gmail.com>
2017-08-04 11:50:00 +0200

    remove references to redux-router

--- app/javascript/app/app.jsx
+++ app/javascript/app/app.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import { Route } from 'react-router-dom'
-import { store, history } from 'app/store'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
 
+import store from 'app/store'
 import Layout from './layout'
 import routes from './routes'
 
+const history = createBrowserHistory()
+
 export default ({ data }) => (
   <Provider store={store(data)}>
-    <ConnectedRouter history={history}>
+    <Router history={history}>
       <Layout>
         {routes.map(route =>
           <Route key={route.path} {...route} />
         )}
       </Layout>
-    </ConnectedRouter>
+    </Router>
   </Provider>
 )
--- app/javascript/app/store.js
+++ app/javascript/app/store.js
@@ -1,18 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import { routerMiddleware } from 'react-router-redux'
-
-import createBrowserHistory from 'history/createBrowserHistory'
 import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 
-const history = createBrowserHistory()
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const middlewares = [thunk, routerMiddleware(history)]
+const middlewares = [thunk]
 
 const store = (initialState = {}) =>
   createStore(reducers, initialState, composeEnhancers(
     applyMiddleware(...middlewares)
   ))
 
-export { store, history }
+export default store
