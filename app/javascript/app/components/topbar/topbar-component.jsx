c49bdd1b3b51876f4c553bcddc02db9e2fb8c838
Edward Brett <e.j.a.brett@gmail.com>
2017-08-09 16:44:29 +0200

    simplfy styles to merge cx styles

--- app/javascript/app/components/topbar/topbar-component.jsx
+++ app/javascript/app/components/topbar/topbar-component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import cx from 'classnames';
 
 import layout from 'styles/layout.scss';
 import styles from './topbar-styles.scss';
 
 export default () => (
   <div className={styles.topbar}>
-    <div className={layout.content}>
-      <a href="http://www.wri.org/" className={styles.wriLogo} target="_blank" rel="noopener noreferrer">
+    <div className={cx(layout.content, styles.wrapper)}>
+      <a href="http://www.wri.org/" target="_blank" rel="noopener noreferrer">
         <img src="" alt="WRI Logo" />
-        WORLD RESOURCE INSTITUTE
       </a>
     </div>
   </div>
