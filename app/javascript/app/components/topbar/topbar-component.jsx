dcffd7f90ee1b90d92148a2cb294612a83a369c3
Edward Brett <e.j.a.brett@gmail.com>
2017-08-09 16:13:10 +0200

    set top bar base styles

--- app/javascript/app/components/topbar/topbar-component.jsx
+++ app/javascript/app/components/topbar/topbar-component.jsx
@@ -6,8 +6,10 @@ import styles from './topbar-styles.scss';
 export default () => (
   <div className={styles.topbar}>
     <div className={layout.content}>
-      <img src="" alt="WRI Logo" />
-      <h1>WORLD RESOURCE INSTITUTE</h1>
+      <a href="http://www.wri.org/" className={styles.wriLogo} target="_blank" rel="noopener noreferrer">
+        <img src="" alt="WRI Logo" />
+        WORLD RESOURCE INSTITUTE
+      </a>
     </div>
   </div>
 );
