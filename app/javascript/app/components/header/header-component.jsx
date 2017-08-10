2707187ea6e630e0bdb6d13fa582e1a850686481
Edward Brett <e.j.a.brett@gmail.com>
2017-08-10 13:37:05 +0200

    add className to props

--- app/javascript/app/components/header/header-component.jsx
+++ app/javascript/app/components/header/header-component.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import layout from 'styles/layout.scss';
 import styles from './header-styles.scss';
 
 const Header = (props) => {
-  const { image } = props;
+  const { image, className } = props;
   return (
-    <div className={styles.header} style={{ backgroundImage: `url(${image})` }}>
+    <div
+      className={cx(className, styles.header)}
+      style={{ backgroundImage: `url(${image})` }}
+    >
       <div className={layout.content}>Child components go here</div>
     </div>
   );
 };
 
 Header.propTypes = {
-  image: PropTypes.string
+  image: PropTypes.string,
+  className: PropTypes.object
 };
 
 export default Header;
