fed689cc72ae69c5d9794ac523d154cdc808f048
Edward Brett <e.j.a.brett@gmail.com>
2017-08-11 12:45:45 +0200

    anchor nav that maps links from props to url

---
+++ app/javascript/app/components/anchor-nav/anchor-nav-component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+import styles from './anchor-nav-styles.scss';
+
+const AnchorNav = (props) => {
+  const { links } = props;
+  return (
+    <div className={styles.anchorNav}>
+      {links.map((link, index) =>
+        (<NavLink
+          exact={index === 0}
+          key={link.label}
+          className={styles.link}
+          activeClassName={styles.linkActive}
+          to={link.path}
+        >
+          {link.label}
+        </NavLink>)
+      )}
+    </div>
+  );
+};
+
+AnchorNav.propTypes = {
+  links: PropTypes.array
+};
+
+export default AnchorNav;
