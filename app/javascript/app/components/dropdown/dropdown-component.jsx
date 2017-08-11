74334e934c5494ce16781f1a96c83c8a00ba058a
Edward Brett <e.j.a.brett@gmail.com>
2017-08-11 16:50:39 +0200

    add rotate to icon

--- app/javascript/app/components/dropdown/dropdown-component.jsx
+++ app/javascript/app/components/dropdown/dropdown-component.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Select from 'react-select';
 import Icon from 'components/icon';
-
+import cx from 'classnames';
 import dropdownArrow from 'assets/icons/dropdown-arrow.svg';
-import styles from './dropdown-styles.scss';
 
 import 'react-select/dist/react-select.css';
+import styles from './dropdown-styles.scss';
 
 const Dropdown = props =>
   (<Select
     className={styles.dropdown}
     {...props}
-    arrowRenderer={() => <Icon className={styles.logo} icon={dropdownArrow} />}
+    arrowRenderer={({ isOpen }) =>
+      <Icon className={cx({ [styles.isOpen]: isOpen })} icon={dropdownArrow} />}
   />);
 
 export default Dropdown;
