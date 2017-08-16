04d3a2bd148ffb1d4724460219d4b766f346c4fa
Edward Brett <e.j.a.brett@gmail.com>
2017-08-16 17:38:04 +0200

    set compare state of accordion from url and render table like view

--- app/javascript/app/components/accordion/accordion.js
+++ app/javascript/app/components/accordion/accordion.js
@@ -1,10 +1,21 @@
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
+import qs from 'query-string';
 import Component from './accordion-component';
 
-const mapStateToProps = (state, { location, match }) => ({
-  location,
-  data: state.ndc.data[match.params.iso]
-});
+const mapStateToProps = (state, { location, match }) => {
+  const search = qs.parse(location.search);
+  return {
+    data: state.ndc.data,
+    country: match.params.iso,
+    activeSection: search.activeSection,
+    compare: parseInt(search.compare, 10) === 1,
+    countriesToCompare: search.countriesToCompare
+      ? search.countriesToCompare.split(',')
+      : [],
+    search,
+    location
+  };
+};
 
 export default withRouter(connect(mapStateToProps, null)(Component));
