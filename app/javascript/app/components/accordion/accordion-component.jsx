33ac9334559cbb57b41076d06856eb03a151fe44
Edward Brett <e.j.a.brett@gmail.com>
2017-08-16 14:13:08 +0200

    open/close accordion based on url, with qs query builder

--- app/javascript/app/components/accordion/accordion-component.jsx
+++ app/javascript/app/components/accordion/accordion-component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Collapse } from 'react-collapse';
 import qs from 'query-string';
@@ -28,46 +28,63 @@ const data = [
   }
 ];
 
-const Accordion = (props) => {
-  const { location } = props;
-  console.info(location);
-  const search = qs.parse(location.search);
-  const activeSection = search.activeSection ? search.activeSection : null;
-  return (
-    <div>
-      {data.map(section =>
-        (<Collapse
-          key={section.slug}
-          className={styles.accordion}
-          isOpened={activeSection === section.slug}
-        >
-          <section>
-            <div className={styles.header}>
+class Accordion extends Component {
+  handleOnClick = (slug) => {
+    const { location, history } = this.props;
+    const search = qs.parse(location.search);
+    search.activeSection = slug;
+    history.push({
+      pathname: location.pathname,
+      search: qs.stringify(search)
+    });
+  };
+
+  render() {
+    const { location } = this.props;
+    const search = qs.parse(location.search);
+    const activeSection = search.activeSection ? search.activeSection : null;
+    return (
+      <div>
+        {data.map((section, index) =>
+          (<section key={section.slug}>
+            <button
+              className={styles.header}
+              onClick={() => this.handleOnClick(section.slug)}
+            >
               {section.title}
-            </div>
-            <div className={styles.body}>
-              <dl>
-                {section.definitions.map(def =>
-                  (<div key={def.title}>
-                    <dt>
-                      {def.title}
-                    </dt>
-                    <dd>
-                      {def.description}
-                    </dd>
-                  </div>)
-                )}
-              </dl>
-            </div>
-          </section>
-        </Collapse>)
-      )}
-    </div>
-  );
-};
+            </button>
+            <Collapse
+              className={styles.accordion}
+              isOpened={
+                activeSection === section.slug ||
+                (index === 0 && !activeSection)
+              }
+            >
+              <div className={styles.body}>
+                <dl>
+                  {section.definitions.map(def =>
+                    (<div key={def.title}>
+                      <dt>
+                        {def.title}
+                      </dt>
+                      <dd>
+                        {def.description}
+                      </dd>
+                    </div>)
+                  )}
+                </dl>
+              </div>
+            </Collapse>
+          </section>)
+        )}
+      </div>
+    );
+  }
+}
 
 Accordion.propTypes = {
-  location: PropTypes.object
+  location: PropTypes.object,
+  history: PropTypes.object
 };
 
 export default Accordion;
