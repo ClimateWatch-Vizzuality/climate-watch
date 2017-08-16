f16f83ec463f46a9418052c5ae5fa6e7f091a179
Edward Brett <e.j.a.brett@gmail.com>
2017-08-16 15:09:10 +0200

    basic accordion styles

--- app/javascript/app/components/accordion/accordion-component.jsx
+++ app/javascript/app/components/accordion/accordion-component.jsx
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Collapse } from 'react-collapse';
+import Icon from 'components/icon';
 import qs from 'query-string';
+import cx from 'classnames';
 
+import dropdownArrow from 'assets/icons/dropdown-arrow.svg';
+import layout from 'styles/layout.scss';
 import styles from './accordion-styles.scss';
-
-const data = [
-  {
-    title: 'Section 1',
-    slug: 'mysection1',
-    definitions: [
-      {
-        title: 'coasa',
-        description: 'fdassadsadas'
-      }
-    ]
-  },
-  {
-    title: 'Section 2',
-    slug: 'mysection2',
-    definitions: [
-      {
-        title: 'coasa',
-        description: 'fdassadsadas'
-      }
-    ]
-  }
-];
 
 class Accordion extends Component {
   handleOnClick = (slug) => {
@@ -40,39 +21,51 @@ class Accordion extends Component {
   };
 
   render() {
-    const { location } = this.props;
+    const { location, data } = this.props;
     const search = qs.parse(location.search);
     const activeSection = search.activeSection ? search.activeSection : null;
     return (
       <div>
         {data.map((section, index) =>
-          (<section key={section.slug}>
+          (<section key={section.slug} className={styles.accordion}>
             <button
               className={styles.header}
               onClick={() => this.handleOnClick(section.slug)}
             >
-              {section.title}
+              <div className={layout.content}>
+                <div className={styles.title}>
+                  {section.title}
+                  <Icon
+                    icon={dropdownArrow}
+                    className={cx(
+                      { [styles.isOpen]: activeSection === section.slug },
+                      styles.iconArrow
+                    )}
+                  />
+                </div>
+              </div>
             </button>
             <Collapse
-              className={styles.accordion}
               isOpened={
                 activeSection === section.slug ||
                 (index === 0 && !activeSection)
               }
             >
-              <div className={styles.body}>
-                <dl>
-                  {section.definitions.map(def =>
-                    (<div key={def.title}>
-                      <dt>
-                        {def.title}
-                      </dt>
-                      <dd>
-                        {def.description}
-                      </dd>
-                    </div>)
-                  )}
-                </dl>
+              <div className={styles.accordionContent}>
+                <div className={layout.content}>
+                  <dl className={styles.definitionList}>
+                    {section.definitions.map(def =>
+                      (<div className={styles.definition} key={def.title}>
+                        <dt className={styles.definitionTitle}>
+                          {def.title}
+                        </dt>
+                        <dd className={styles.definitionDesc}>
+                          {def.description}
+                        </dd>
+                      </div>)
+                    )}
+                  </dl>
+                </div>
               </div>
             </Collapse>
           </section>)
@@ -84,7 +77,14 @@ class Accordion extends Component {
 
 Accordion.propTypes = {
   location: PropTypes.object,
-  history: PropTypes.object
+  history: PropTypes.object,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      slug: PropTypes.string,
+      definitions: PropTypes.array.isRequired
+    })
+  )
 };
 
 export default Accordion;
