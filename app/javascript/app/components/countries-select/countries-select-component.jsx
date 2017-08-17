83a70f74769ac6b6d1f96b76216e87ee72820fd2
j8seangel <jseangel@gmail.com>
2017-08-17 18:12:37 +0200

    Move logic to container

--- app/javascript/app/components/countries-select/countries-select-component.jsx
+++ app/javascript/app/components/countries-select/countries-select-component.jsx
@@ -1,9 +1,6 @@
 import React, { PureComponent } from 'react';
 import Proptypes from 'prop-types';
 import cx from 'classnames';
-import { deburrUpper } from 'app/utils';
-import includes from 'lodash/includes';
-import paths from 'app/data/world-50m-paths';
 
 import Search from 'components/search';
 import ResultsList from 'components/results-list';
@@ -13,63 +10,15 @@ import layout from 'styles/layout.scss';
 import styles from './countries-select-styles.scss';
 
 class CountriesSelect extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.queryUpper = deburrUpper(props.query);
-  }
-
-  componentWillReceiveProps(props) {
-    this.queryUpper = deburrUpper(props.query);
-  }
-
-  onCountryClick = (geometry) => {
-    const { history } = this.props;
-    const country = geometry.id;
-    history.push(`countries/${country}`);
-    this.props.onLeave();
-  };
-
-  computedStyles = (geography) => {
-    const nameUpper = deburrUpper(geography.properties.name);
-    const isInFilter = this.queryUpper
-      ? includes(nameUpper, this.queryUpper)
-      : false;
-    if (isInFilter) {
-      return {
-        default: {
-          fill: '#302463',
-          stroke: '#607D8B',
-          strokeWidth: 0.2,
-          outline: 'none'
-        },
-        hover: {},
-        pressed: {}
-      };
-    }
-    return {
-      default: {
-        fill: '#ECEFF1',
-        stroke: '#607D8B',
-        strokeWidth: 0.2,
-        outline: 'none'
-      },
-      hover: {
-        fill: '#302463',
-        stroke: '#607D8B',
-        strokeWidth: 0.2,
-        outline: 'none'
-      },
-      pressed: {
-        fill: '#FF5722',
-        stroke: '#607D8B',
-        strokeWidth: 0.5,
-        outline: 'none'
-      }
-    };
-  };
-
   render() {
-    const { query, countrySelectFilter, countriesList } = this.props;
+    const {
+      query,
+      paths,
+      countrySelectFilter,
+      countriesList,
+      onCountryClick,
+      computedStyles
+    } = this.props;
     return (
       <div className={cx(layout.content, styles.wrapper)}>
         <Search placeholder="" value={query} onChange={countrySelectFilter} />
@@ -77,8 +26,8 @@ class CountriesSelect extends PureComponent {
         <Map
           cache={false}
           paths={paths}
-          onCountryClick={this.onCountryClick}
-          computedStyles={this.computedStyles}
+          onCountryClick={onCountryClick}
+          computedStyles={computedStyles}
         />
       </div>
     );
@@ -86,11 +35,12 @@ class CountriesSelect extends PureComponent {
 }
 
 CountriesSelect.propTypes = {
-  history: Proptypes.object,
-  onLeave: Proptypes.func.isRequired,
   query: Proptypes.string,
+  onCountryClick: Proptypes.func.isRequired,
+  computedStyles: Proptypes.func.isRequired,
   countrySelectFilter: Proptypes.func.isRequired,
-  countriesList: Proptypes.array
+  countriesList: Proptypes.array,
+  paths: Proptypes.array
 };
 
 export default CountriesSelect;
--- app/javascript/app/components/countries-select/countries-select.js
+++ app/javascript/app/components/countries-select/countries-select.js
@@ -1,9 +1,13 @@
+import { createElement } from 'react';
+import Proptypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { deburrUpper } from 'app/utils';
 import includes from 'lodash/includes';
 
-import Component from './countries-select-component';
+import paths from 'app/data/world-50m-paths';
+
+import CountrySelectComponent from './countries-select-component';
 import actions from './countries-select-actions';
 
 export { default as component } from './countries-select-component';
@@ -11,9 +15,8 @@ export { default as reducers } from './countries-select-reducers';
 export { default as styles } from './countries-select-styles';
 export { default as actions } from './countries-select-actions';
 
-function getCountriesFiltered(countries, query) {
+function getCountriesFiltered(countries, queryUpper) {
   // TODO: use reselect here
-  const queryUpper = deburrUpper(query);
   const filteredCountries = queryUpper
     ? countries.filter(country =>
       includes(deburrUpper(country.label), queryUpper)
@@ -28,10 +31,75 @@ function getCountriesFiltered(countries, query) {
 
 const mapStateToProps = (state) => {
   const { query, countries } = state.countrySelect;
+  const queryUpper = deburrUpper(query);
   return {
-    query,
-    countriesList: getCountriesFiltered(countries, query)
+    query: queryUpper,
+    countriesList: getCountriesFiltered(countries, queryUpper)
   };
 };
 
-export default withRouter(connect(mapStateToProps, actions)(Component));
+const CountrySelectContainer = (props) => {
+  const onCountryClick = (geometry) => {
+    const { history, onLeave } = props;
+    const country = geometry.id;
+    if (country) {
+      history.push(`countries/${country}`);
+      onLeave();
+    }
+  };
+
+  const computedStyles = (geography) => {
+    const { query } = props;
+    const nameUpper = deburrUpper(geography.properties.name);
+    const isInFilter = query ? includes(nameUpper, query) : false;
+    if (isInFilter) {
+      return {
+        default: {
+          fill: '#302463',
+          stroke: '#607D8B',
+          strokeWidth: 0.2,
+          outline: 'none'
+        },
+        hover: {},
+        pressed: {}
+      };
+    }
+    return {
+      default: {
+        fill: '#ECEFF1',
+        stroke: '#607D8B',
+        strokeWidth: 0.2,
+        outline: 'none'
+      },
+      hover: {
+        fill: '#302463',
+        stroke: '#607D8B',
+        strokeWidth: 0.2,
+        outline: 'none'
+      },
+      pressed: {
+        fill: '#FF5722',
+        stroke: '#607D8B',
+        strokeWidth: 0.5,
+        outline: 'none'
+      }
+    };
+  };
+
+  return createElement(CountrySelectComponent, {
+    ...this.props,
+    onCountryClick,
+    computedStyles,
+    paths
+  });
+};
+
+CountrySelectContainer.propTypes = {
+  query: Proptypes.string,
+  history: Proptypes.object,
+  onLeave: Proptypes.func.isRequired
+};
+
+export default withRouter(
+  connect(mapStateToProps, actions)(CountrySelectContainer)
+);
