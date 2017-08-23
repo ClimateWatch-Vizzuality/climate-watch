c0191321282be92e96ce457ceae4156983c75531
Edward Brett <e.j.a.brett@gmail.com>
2017-08-23 10:35:55 +0200

    update all components needed to pages

--- app/javascript/app/pages/country/country-component.jsx
+++ app/javascript/app/pages/country/country-component.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Proptypes from 'proptypes';
 
 const Country = ({ match }) =>
   (<h1>
     Country: {match.params.iso}
   </h1>);
+
+Country.propTypes = {
+  match: Proptypes.object.isRequired
+};
+
 export default Country;
