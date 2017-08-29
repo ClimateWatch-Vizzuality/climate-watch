b32edefba9e778fa5f68e41b05eb9141a95e4181
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-08-29 15:18:23 +0100

    Style fix

--- app/models/ndc_sdg/ndc_target.rb
+++ app/models/ndc_sdg/ndc_target.rb
@@ -3,7 +3,7 @@ module NdcSdg
     belongs_to :ndc
     belongs_to :target, class_name: 'NdcSdg::Target'
     has_many :ndc_target_sectors,
-      class_name: 'NdcSdg::NdcTargetSector',
-      dependent: :destroy
+             class_name: 'NdcSdg::NdcTargetSector',
+             dependent: :destroy
   end
 end
