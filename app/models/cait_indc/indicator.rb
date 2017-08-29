acb0d423b632846da43906d88dcffceda803c603
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-08-29 14:22:01 +0100

    Made Chart and Category relations optional

--- app/models/cait_indc/indicator.rb
+++ app/models/cait_indc/indicator.rb
@@ -1,7 +1,7 @@
 class CaitIndc::Indicator < ApplicationRecord
-  belongs_to :chart, class_name: 'CaitIndc::Chart'
+  belongs_to :chart, class_name: 'CaitIndc::Chart', optional: true
   belongs_to :indicator_type, class_name: 'CaitIndc::IndicatorType'
-  belongs_to :category, class_name: 'CaitIndc::Category'
+  belongs_to :category, class_name: 'CaitIndc::Category', optional: true
   has_many :indicator_values, class_name: 'CaitIndc::IndicatorValue'
   has_many :location_indicator_values, class_name: 'CaitIndc::LocationIndicatorValue'
   validates :name, presence: true
