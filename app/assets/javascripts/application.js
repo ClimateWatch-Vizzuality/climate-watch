80fecd5ef27b07e0a72c1460c6277e594e1dab2b
j8seangel <jseangel@gmail.com>
2017-08-30 17:04:06 +0200

    Remove unused Imports

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
+// require rails-ujs
+// require turbolinks
+// require_tree .
