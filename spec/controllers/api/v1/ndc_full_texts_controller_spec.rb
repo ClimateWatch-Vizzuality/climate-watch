18a23b55932453293614bc44557f72c7de37a604
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-08-31 15:43:08 +0100

    Full text search with highlighting

--- spec/controllers/api/v1/ndc_full_texts_controller_spec.rb
+++ spec/controllers/api/v1/ndc_full_texts_controller_spec.rb
@@ -4,18 +4,37 @@ RSpec.describe Api::V1::NdcFullTextsController, type: :controller do
   let(:pol) {
     FactoryGirl.create(:location, iso_code3: 'POL', location_type: 'COUNTRY')
   }
+  let(:prt) {
+    FactoryGirl.create(:location, iso_code3: 'PRT', location_type: 'COUNTRY')
+  }
   let(:pol_img_src) { 'POL-1.PNG' }
   let(:pol_html) {
     "<h1>Hello</h1>\n\n<p>\
 <img src=\"#{S3_BUCKET_URL}/ndcs/#{pol_img_src}\" /></p>"
   }
+  let(:pol_html_with_highlight) {
+    pol_html.sub(
+      'Hello',
+      "#{Ndc::PG_SEARCH_HIGHLIGHT_START}Hello#{Ndc::PG_SEARCH_HIGHLIGHT_END}"
+    )
+  }
+  let(:prt_html) { 'foo bar baz' }
   let!(:pol_ndc) {
     FactoryGirl.create(:ndc, location: pol, full_text: pol_html)
   }
+  let!(:prt_ndc) {
+    FactoryGirl.create(:ndc, location: prt, full_text: prt_html)
+  }
+
+  before(:each) { Ndc.refresh_full_text_tsv }
 
   describe 'GET index' do
     it 'renders a list of NDCs' do
       get :index
+      expect(assigns(:ndcs)).to match_array([pol_ndc, prt_ndc])
+    end
+    it 'renders a list of matching NDCS' do
+      get :index, params: {query: 'hello'}
       expect(assigns(:ndcs)).to match_array([pol_ndc])
     end
   end
@@ -23,15 +42,22 @@ RSpec.describe Api::V1::NdcFullTextsController, type: :controller do
   describe 'GET show' do
     it 'renders NDC content in html' do
       get :show, params: {code: pol.iso_code3}
-      expect(response.body).to match(pol_html)
+      json_response = JSON.parse(response.body)
+      expect(json_response['html']).to match(pol_html)
     end
     it 'renders NDC content in html even if code downcased' do
       get :show, params: {code: pol.iso_code3.downcase}
-      expect(response.body).to match(pol_html)
+      json_response = JSON.parse(response.body)
+      expect(json_response['html']).to match(pol_html)
     end
     it 'responds with 404 if bogus code' do
       get :show, params: {code: 'LOL'}
       expect(response.status).to be(404)
+    end
+    it 'highlights matches' do
+      get :show, params: {code: pol.iso_code3, query: 'hello'}
+      json_response = JSON.parse(response.body)
+      expect(json_response['html']).to match(pol_html_with_highlight)
     end
   end
 end
