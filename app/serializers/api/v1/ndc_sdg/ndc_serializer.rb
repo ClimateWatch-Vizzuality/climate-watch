4cd91933d9f9596c2d716ac561c3dd3978cc58b1
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-09-05 15:35:20 +0100

    Response with custom structure to return sectors, goals, targets and target - sector mapping

--- app/serializers/api/v1/ndc_sdg/ndc_serializer.rb
+++ app/serializers/api/v1/ndc_sdg/ndc_serializer.rb
@@ -15,11 +15,44 @@ module Api
         end
 
         def sectors
-          NdcSdg::Sector.order(:name).pluck(:id, :name)
+          ary = ::NdcSdg::Sector.order(:name).select(:id, :name)
+          Hash[ary.map { |s| [s.id, {name: s.name}] }]
         end
 
         def sdgs
-          object.ndc_targets
+          result = {}
+          ndc_targets = object.ndc_targets.includes(:ndc_target_sectors)
+          ::NdcSdg::Goal.eager_load(:targets).
+            order('ndc_sdg_goals.number::INT, ndc_sdg_targets.number').
+            each do |goal|
+            result[goal.number] = goal_properties(goal, ndc_targets)
+          end
+          result
+        end
+
+        def goal_properties(goal, ndc_targets)
+          goal_properties = {
+            title: goal.cw_title
+          }
+          goal.targets.each do |target|
+            goal_properties[target.number] = target_properties(
+              target, ndc_targets
+            )
+          end
+          goal_properties
+        end
+
+        def target_properties(target, ndc_targets)
+          target_properties = {
+            title: target.title
+          }
+          ndc_target = ndc_targets.select { |o| o.target_id == target.id }.
+            first
+          if ndc_target
+            sector_ids = ndc_target.ndc_target_sectors.map(&:sector_id)
+            target_properties[:sectors] = sector_ids
+          end
+          target_properties
         end
       end
     end
