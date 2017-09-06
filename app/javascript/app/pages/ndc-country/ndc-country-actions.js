21981b138872483c7cde8ba8733faf4436500849
Edward Brett <e.j.a.brett@gmail.com>
2017-09-06 11:23:49 +0200

    fetch countryNDC data from api endpoint

--- app/javascript/app/pages/ndc-country/ndc-country-actions.js
+++ app/javascript/app/pages/ndc-country/ndc-country-actions.js
@@ -1,18 +1,26 @@
 import { createAction } from 'redux-actions';
 import { createThunkAction } from 'utils/redux';
 
-import exampleNDC from 'app/data/ndc-country.json';
-
 const fetchCountryNDCInit = createAction('fetchCountryNDCInit');
 const fetchCountryNDCReady = createAction('fetchCountryNDCReady');
 
-const fetchCountryNDC = createThunkAction('fetchCountryNDC', () => dispatch => {
-  dispatch(fetchCountryNDCInit());
-
-  setTimeout(() => {
-    dispatch(fetchCountryNDCReady(exampleNDC));
-  }, 1000);
-});
+const fetchCountryNDC = createThunkAction(
+  'fetchCountryNDC',
+  iso => dispatch => {
+    dispatch(fetchCountryNDCInit());
+    fetch(`/api/v1/ndcs?location=${iso}&filter=summary`)
+      .then(response => {
+        if (response.ok) return response.json();
+        throw Error(response.statusText);
+      })
+      .then(data => {
+        dispatch(fetchCountryNDCReady(data));
+      })
+      .catch(error => {
+        console.info(error);
+      });
+  }
+);
 
 export default {
   fetchCountryNDC,
