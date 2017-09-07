f924714b0567019db8911ca00b1597fb1a84311f
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-09-07 12:01:39 +0100

    Payload with not found status

--- app/controllers/api/v1/ndc_full_texts_controller.rb
+++ app/controllers/api/v1/ndc_full_texts_controller.rb
@@ -15,10 +15,12 @@ module Api
         )
         ndcs = apply_query_with_highlights(ndcs, true)
         @ndc = ndcs.first
-        render json: {
-          error: 'NDC not found',
-          status: 404
-        }, status: :not_found and return unless @ndc
+        unless @ndc
+          render json: {
+            error: 'NDC not found',
+            status: 404
+          }, status: :not_found and return
+        end
         render json: @ndc,
                serializer: Api::V1::NdcFullTextSerializer,
                query: params[:query]
--- app/controllers/api/v1/ndc_sdgs_controller.rb
+++ app/controllers/api/v1/ndc_sdgs_controller.rb
@@ -5,7 +5,12 @@ module Api
         @ndc = Ndc.joins(:location).where(
           'locations.iso_code3' => params[:code].upcase
         ).first
-        render status: :not_found and return unless @ndc
+        unless @ndc
+          render json: {
+            error: 'NDC not found',
+            status: 404
+          }, status: :not_found and return
+        end
         render json: @ndc,
                serializer: Api::V1::NdcSdg::NdcSerializer
       end
