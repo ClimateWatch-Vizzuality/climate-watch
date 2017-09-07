f7670207e182cb0490ae5dcfe3523c36753e8dce
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-07 16:33:03 +0100

    Add /emissions endpoint
    This endpoint has filters available:
    - location takes a comma-separated list of ISO country codes (3 character)
    - source takes the id attribute of the `historical_emissions_data_sources` table
    - gas takes the id attribute of the `historical_emissions_gases` table
    - sector takes the id attribute of the `historical_emissions_sectors` table
    
    example: /emissions?location=PRT,ESP&source=1&gas=1&sector=1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :countries, only: [:index], controller: :countries, param: :code
+      resources :emissions, only: [:index], controller: :historical_emissions
+
       resources :ndcs, param: :code, only: [:index, :show] do
         get :full, on: :member, controller: :ndc_full_texts, action: :show
         get :full, on: :collection, controller: :ndc_full_texts, action: :index
