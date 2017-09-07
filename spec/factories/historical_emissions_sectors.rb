0f19c1a641f04b5e31d8b8c8da1dd91c1f948329
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-07 17:36:13 +0100

    Update historical emissions fixture factories

---
+++ spec/factories/historical_emissions_sectors.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :historical_emissions_sector,
+          class: 'HistoricalEmissions::Sector' do
+    association :data_source,
+                factory: :historical_emissions_data_source
+    name 'MyText'
+  end
+end
