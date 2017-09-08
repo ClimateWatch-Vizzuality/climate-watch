0d56534426002af7b28aa4a167c2d82855ed2244
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-08 11:35:18 +0100

    Fix spec for historical emissions models

--- spec/models/historical_emissions/sector_spec.rb
+++ spec/models/historical_emissions/sector_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Sector, type: :model do
+RSpec.describe HistoricalEmissions::Sector, type: :model do
   it 'should be invalid when name not present' do
     expect(
-      FactoryGirl.build(:sector, name: nil)
+      FactoryGirl.build(:historical_emissions_sector, name: nil)
     ).to have(1).errors_on(:name)
   end
 
   it 'should be invalid when data_source not present' do
     expect(
-      FactoryGirl.build(:sector, data_source: nil)
+      FactoryGirl.build(:historical_emissions_sector, data_source: nil)
     ).to have(1).errors_on(:data_source)
   end
 end
