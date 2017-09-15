3ad8a3157f5908d27f8a1ff01a79778264f98436
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-15 11:37:21 +0100

    Add spec for adaptation import

---
+++ spec/services/import_adaptation_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+object_contents = {
+  'adaptation/adaptation_metadata.csv' => <<~END,
+  column_name,long_name
+  country,Country
+  poverty_14,Population living below the national income poverty line (%)
+  climate_risks,Climate Risk Index score
+  END
+  'adaptation/adaptation.csv' => <<~END,
+  country,poverty_14,climate_risks
+  AFG,35.8,36.17
+  END
+}
+
+RSpec.describe ImportAdaptation do
+  subject { ImportAdaptation.new.call }
+
+  before :all do
+    Aws.config[:s3] = {
+      stub_responses: {
+        get_object: lambda { |context|
+          {body: object_contents[context.params[:key]]}
+        }
+      }
+    }
+  end
+
+  before :each do
+    FactoryGirl.create(
+      :location, iso_code3: 'AFG', wri_standard_name: 'Afghanistan'
+    )
+  end
+
+  after :all do
+    Aws.config[:s3] = {
+      stub_responses: nil
+    }
+  end
+
+  it 'Creates new adaptation records' do
+    expect { subject }.to change { Adaptation::Value.count }.by(3)
+  end
+end
