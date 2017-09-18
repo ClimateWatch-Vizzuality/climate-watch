f71c2c2eef615353afdf9597c2055dfc8012ff4e
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-18 16:37:06 +0100

    Add location filter

--- app/controllers/api/v1/adaptations_controller.rb
+++ app/controllers/api/v1/adaptations_controller.rb
@@ -5,8 +5,24 @@ module Api
         variables = ::Adaptation::Variable.
           includes(values: [:location])
 
+        if location_list
+          variables = variables.where(
+            values: {locations: {iso_code3: location_list}}
+          )
+        end
+
         render json: variables,
                each_serializer: Api::V1::Adaptation::VariableSerializer
+      end
+
+      private
+
+      def location_list
+        if params[:location].blank?
+          nil
+        else
+          params[:location].split(',')
+        end
       end
     end
   end
