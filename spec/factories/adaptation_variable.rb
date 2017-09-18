b304bea7e26e53e6a7fbbb079ab1839f3faf6491
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-18 13:23:07 +0100

    Add spec to adaptation endpoints

--- spec/factories/adaptation_variable.rb
+++ spec/factories/adaptation_variable.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :adaptation_variable, class: 'Adaptation::Variable' do
     name 'MyText'
-    slug 'my-text'
+    slug { (0...5).map { (65 + rand(26)).chr }.join }
   end
 end
