5126cc7a860efe81527a22e818bc11633fb4c611
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-19 19:44:38 +0100

    Use sequence instead of random in factory

--- spec/factories/adaptation_variable.rb
+++ spec/factories/adaptation_variable.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :adaptation_variable, class: 'Adaptation::Variable' do
     name 'MyText'
-    slug { (0...5).map { (65 + rand(26)).chr }.join }
+    sequence :slug { |n| ('aa'..'zz').to_a[n] }
   end
 end
