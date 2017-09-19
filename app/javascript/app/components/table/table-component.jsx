fadd7e5bf02aa9e80d4f071367e5a6b141308820
j8seangel <jseangel@gmail.com>
2017-09-19 17:56:21 +0200

    Sort feature in table

--- app/javascript/app/components/table/table-component.jsx
+++ app/javascript/app/components/table/table-component.jsx
@@ -1,50 +1,64 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Column, Table, AutoSizer } from 'react-virtualized';
 import 'react-virtualized/styles.css'; // only needs to be imported once
 
 import styles from './table-styles.scss';
 
-const SimpleTable = props => {
-  const { data, rowHeight, headerHeight } = props;
-  if (!data.length) return null;
+class SimpleTable extends PureComponent {
+  render() {
+    const {
+      data,
+      rowHeight,
+      headerHeight,
+      sortBy,
+      sortDirection,
+      handleSort
+    } = this.props;
+    if (!data.length) return null;
 
-  const columns = Object.keys(data[0]);
-  return (
-    <AutoSizer disableHeight>
-      {({ width }) => (
-        <Table
-          className={styles.table}
-          width={width}
-          height={460}
-          headerHeight={headerHeight}
-          rowHeight={rowHeight}
-          rowCount={data.length}
-          rowGetter={({ index }) => data[index]}
-        >
-          {columns.map(column => (
-            <Column
-              className={styles.column}
-              key={column}
-              label={column}
-              dataKey={column}
-              width={300}
-            />
-          ))}
-        </Table>
-      )}
-    </AutoSizer>
-  );
-};
+    const columns = Object.keys(data[0]);
+    return (
+      <AutoSizer disableHeight>
+        {({ width }) => (
+          <Table
+            className={styles.table}
+            width={width}
+            height={460}
+            headerHeight={headerHeight}
+            rowHeight={rowHeight}
+            rowCount={data.length}
+            sort={handleSort}
+            sortBy={sortBy}
+            sortDirection={sortDirection}
+            rowGetter={({ index }) => data[index]}
+          >
+            {columns.map(column => (
+              <Column
+                className={styles.column}
+                key={column}
+                label={column}
+                dataKey={column}
+                width={300}
+              />
+            ))}
+          </Table>
+        )}
+      </AutoSizer>
+    );
+  }
+}
 
 SimpleTable.propTypes = {
   data: PropTypes.array,
   rowHeight: PropTypes.number.isRequired,
-  headerHeight: PropTypes.number.isRequired
+  headerHeight: PropTypes.number.isRequired,
+  sortBy: PropTypes.string.isRequired,
+  sortDirection: PropTypes.string.isRequired,
+  handleSort: PropTypes.func.isRequired
 };
 
 SimpleTable.defaultProps = {
-  data: [],
   rowHeight: 45,
   headerHeight: 30
 };
