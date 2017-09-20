8c11db27da9e568c8ef3f1e21fee379c59bd736b
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-20 16:50:47 +0100

    Move filter concern to model

--- app/controllers/api/v1/historical_emissions_controller.rb
+++ app/controllers/api/v1/historical_emissions_controller.rb
@@ -10,7 +10,7 @@ module Api
 
     class HistoricalEmissionsController < ApiController
       def index
-        render json: records,
+        render json: ::HistoricalEmissions::Record.find_by_params(params),
                each_serializer: Api::V1::HistoricalEmissions::RecordSerializer,
                params: params
       end
@@ -24,46 +24,6 @@ module Api
 
         render json: HistoricalEmissionsMetadata.new(*args),
                serializer: Api::V1::HistoricalEmissions::MetadataSerializer
-      end
-
-      private
-
-      def records
-        records = ::HistoricalEmissions::Record.
-          includes(
-            :location,
-            :data_source,
-            :sector,
-            :gas
-          )
-
-        filters(records)
-      end
-
-      def filters(records)
-        if location_list
-          records = records.where(
-            locations: {iso_code3: location_list}
-          )
-        end
-
-        {
-          historical_emissions_gases: :gas,
-          historical_emissions_data_sources: :source,
-          historical_emissions_sectors: :sector
-        }.each do |k, v|
-          records = records.where(k => {id: params[v]}) if params[v]
-        end
-
-        records
-      end
-
-      def location_list
-        if params[:location].blank?
-          nil
-        else
-          params[:location].split(',')
-        end
       end
     end
   end
