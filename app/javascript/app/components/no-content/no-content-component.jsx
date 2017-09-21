28800f985bfdc143552768dcc1fbe1be23190445
j8seangel <jseangel@gmail.com>
2017-09-21 12:55:29 +0200

    Better errors feedback and monete option

--- app/javascript/app/components/no-content/no-content-component.jsx
+++ app/javascript/app/components/no-content/no-content-component.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
+import monete from 'assets/icons/monete.png';
 import styles from './no-content-styles.scss';
 
-const NoContent = ({ className, message }) =>
-  (<div className={cx(styles.noContent, className)}>
-    {message}
-  </div>);
+const NoContent = ({ className, message, icon }) => (
+  <div className={cx(styles.noContent, className)}>
+    <p className={styles.text}>{message}</p>
+    {icon && <img src={monete} alt="Monete" />}
+  </div>
+);
 
 NoContent.propTypes = {
+  icon: PropTypes.bool,
   className: PropTypes.string,
   message: PropTypes.string
+};
+
+NoContent.defaultProps = {
+  icon: false
 };
 
 export default NoContent;
--- app/javascript/app/pages/error/error-component.jsx
+++ app/javascript/app/pages/error/error-component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import cx from 'classnames';
 
+import NoContent from 'components/no-content';
 import layout from 'styles/layout.scss';
+import styles from './error-styles.scss';
 
-const Error = () =>
-  (<div className={layout.content}>
-    <h1>Oops that route does not exist</h1>
-  </div>);
+const Error = () => (
+  <div className={cx(layout.content, styles.container)}>
+    <NoContent message="Oops that page does not exist" icon />
+  </div>
+);
 export default Error;
