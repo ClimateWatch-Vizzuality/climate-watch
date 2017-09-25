5a328e4aea089ea3fd8fa9292c5c2f74fb1676e4
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-25 19:54:00 +0100

    Prevent api routes falling into the FE catch-all

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class ApiController < ActionController::API
+      def not_found
+        render json: {
+          code: 404,
+          status: "#{request.params[:endpoint]} not found"
+        }, status: :not_found
+      end
     end
   end
 end
