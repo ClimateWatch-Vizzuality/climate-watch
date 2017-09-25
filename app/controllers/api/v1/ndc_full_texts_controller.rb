ccda715eb611ab85cc204501c860a338d683dfe8
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-25 12:05:24 +0100

    WIP

--- app/controllers/api/v1/ndc_full_texts_controller.rb
+++ app/controllers/api/v1/ndc_full_texts_controller.rb
@@ -3,7 +3,6 @@ module Api
     class NdcFullTextsController < ApiController
       def index
         @ndcs = Ndc.includes(:location)
-        apply_temporary_hack
         @ndcs = apply_query_with_highlights(@ndcs, false)
         render json: @ndcs,
                each_serializer: Api::V1::NdcFullTextSearchResultSerializer,
@@ -12,18 +11,13 @@ module Api
 
       def show
         ndcs = Ndc.joins(:location).where(
-          'locations.iso_code3' => params[:code].upcase
+          locations: {iso_code3: params[:code].upcase}
         )
+
         ndcs_highlighted = apply_query_with_highlights(ndcs, true)
-        @ndc = ndcs_highlighted.first || ndcs.first
-        unless @ndc
-          render json: {
-            error: 'NDC not found',
-            status: 404
-          }, status: :not_found and return
-        end
-        render json: @ndc,
-               serializer: Api::V1::NdcFullTextSerializer
+
+        render json: ndcs_highlighted,
+               each_serializer: Api::V1::NdcFullTextSerializer
       end
 
       private
@@ -39,27 +33,6 @@ module Api
           ndcs = ndcs.with_pg_search_highlight
         end
         ndcs
-      end
-
-      def apply_temporary_hack
-        # temporary hack to only return a single document per country
-        query = <<~EOT
-          WITH obsolete_ndcs AS (
-            SELECT min_id, dup_id FROM (
-              SELECT min_id, UNNEST(duplicated_ids) AS dup_id FROM (
-                SELECT MIN(id) AS min_id, ARRAY_AGG(id) AS duplicated_ids, COUNT(*)
-                FROM ndcs
-                GROUP BY (location_id)
-                HAVING COUNT(*) > 1
-              ) s
-            ) ss
-            WHERE ss.dup_id != ss.min_id
-          ) SELECT ARRAY_AGG(dup_id) AS dup_ids FROM obsolete_ndcs
-        EOT
-        result = Ndc.find_by_sql(query)
-        dup_ids = result.first && result.first['dup_ids'] || []
-        @ndcs = @ndcs.where('id NOT IN (?)', dup_ids) if dup_ids.any?
-        # end of temporary hack
       end
     end
   end
