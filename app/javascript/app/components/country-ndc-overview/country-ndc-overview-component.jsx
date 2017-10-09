ace1eef5aa8124f29fb0212ab96a3d6049aae44b
Edward Brett <e.j.a.brett@gmail.com>
2017-10-09 19:00:55 +0200

    Layout for content overview section

--- app/javascript/app/components/country-ndc-overview/country-ndc-overview-component.jsx
+++ app/javascript/app/components/country-ndc-overview/country-ndc-overview-component.jsx
@@ -1,32 +1,49 @@
 import React, { PureComponent } from 'react';
-import Intro from 'components/intro';
 import Button from 'components/button';
+import Card from 'components/card';
+import Intro from 'components/intro';
+import cx from 'classnames';
 
+import introThemeDark from 'styles/themes/intro/intro-dark.scss';
+import layout from 'styles/layout.scss';
 import styles from './country-ndc-overview-styles.scss';
 
 class CountryNdcOverview extends PureComponent {
   // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
-        <Intro
-          title="NDC Content Overview"
-          description="Brazil intends to commit to reduce greenhouse gas emissions by 37% below 2005 levels in 2025, with an indicative goal to reduce GHG emissions by 43% by 2030, relative to 2005 levels."
-        />
-        <Button
-          className={styles.exploreBtn}
-          color="white"
-          link={'/ghg-emissions?breakBy=location&filter='}
-        >
-          Explore emissions
-        </Button>
-        <Button
-          className={styles.exploreBtn}
-          color="yellow"
-          link={'/ghg-emissions?breakBy=location&filter='}
-        >
-          Explore emissions
-        </Button>
+      <div className={styles.wrapper}>
+        <div className={layout.content}>
+          <div className={cx(styles.header, styles.col2)}>
+            <Intro
+              theme={introThemeDark}
+              title="NDC Content Overview"
+              description="Brazil intends to commit to reduce greenhouse gas emissions by 37% below 2005 levels in 2025, with an indicative goal to reduce GHG emissions by 43% by 2030, relative to 2005 levels."
+            />
+            <div className={styles.actions}>
+              <Button
+                className={styles.exploreBtn}
+                color="white"
+                link={'/ghg-emissions?breakBy=location&filter='}
+              >
+                Compare
+              </Button>
+              <Button
+                className={styles.exploreBtn}
+                color="yellow"
+                link={'/ghg-emissions?breakBy=location&filter='}
+              >
+                See more
+              </Button>
+            </div>
+          </div>
+          <h4 className={styles.subTitle}>Mitigation contribution</h4>
+          <div className={styles.cards}>
+            <Card title="GHG target">
+              <div>test</div>
+            </Card>
+          </div>
+        </div>
       </div>
     );
   }
