c350ce44c2d86af6a19ffb29219e64322b91756f
Edward Brett <e.j.a.brett@gmail.com>
2017-10-10 19:10:30 +0200

    swap state direction for cleaner function

--- app/javascript/app/providers/regions-provider/regions-provider-reducers.js
+++ app/javascript/app/providers/regions-provider/regions-provider-reducers.js
@@ -4,11 +4,11 @@ export const initialState = {
   data: []
 };
 
-const setLoading = (loading, state) => ({ ...state, loading });
-const setLoaded = (loaded, state) => ({ ...state, loaded });
+const setLoading = (state, loading) => ({ ...state, loading });
+const setLoaded = (state, loaded) => ({ ...state, loaded });
 
 export default {
-  getRegionsInit: state => setLoading(true, state),
+  getRegionsInit: state => setLoading(state, true),
   getRegionsReady: (state, { payload }) =>
-    setLoaded(true, setLoading(false, { ...state, data: payload }))
+    setLoaded(setLoading({ ...state, data: payload }, false), true)
 };
