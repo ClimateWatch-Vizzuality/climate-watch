66837c75993a97d8a43adefa376fd06e1db2c303
Edward Brett <e.j.a.brett@gmail.com>
2017-10-10 11:46:33 +0200

    NDC content cards with dummy data

--- app/javascript/app/components/country-ndc-overview/country-ndc-overview-component.jsx
+++ app/javascript/app/components/country-ndc-overview/country-ndc-overview-component.jsx
@@ -8,6 +8,35 @@ import introTheme from 'styles/themes/intro/intro-simple.scss';
 import layout from 'styles/layout.scss';
 import styles from './country-ndc-overview-styles.scss';
 
+const cardData = [
+  {
+    title: 'GHG Target',
+    data: {
+      targetType: 'Absolute Emission Reduction',
+      targetYear: '2025'
+    }
+  },
+  {
+    title: 'Non-GHG Target',
+    data: {
+      targetType: 'Absolute Emission Reduction',
+      targetYear: '2030'
+    }
+  },
+  {
+    title: 'Sectoral coverage'
+  }
+];
+
+const listData = [
+  'Cross-cutting area',
+  'Education',
+  'Energy',
+  'Health',
+  'LULUCF/Forestry',
+  'Social Development'
+];
+
 class CountryNdcOverview extends PureComponent {
   // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -21,6 +50,7 @@ class CountryNdcOverview extends PureComponent {
               description="Brazil intends to commit to reduce greenhouse gas emissions by 37% below 2005 levels in 2025, with an indicative goal to reduce GHG emissions by 43% by 2030, relative to 2005 levels."
             />
             <div className={styles.actions}>
+              <div className={styles.printButton} />
               <Button
                 className={styles.exploreBtn}
                 color="white"
@@ -39,9 +69,42 @@ class CountryNdcOverview extends PureComponent {
           </div>
           <h4 className={styles.subTitle}>Mitigation contribution</h4>
           <div className={styles.cards}>
-            <Card title="GHG target">
-              <div>test</div>
-            </Card>
+            {cardData &&
+              cardData.map(card => (
+                <Card title={card.title} key={card.title}>
+                  <div className={styles.cardContent}>
+                    {card.data && (
+                      <div>
+                        <span className={styles.metaTitle}>Target type</span>
+                        <p className={styles.targetText}>
+                          {card.data.targetType}
+                        </p>
+                        <span className={styles.metaTitle}>Target year</span>
+                        <p className={styles.targetText}>
+                          {card.data.targetYear}
+                        </p>
+                      </div>
+                    )}
+                    {!card.data && (
+                      <div className={styles.noContent}>Not included</div>
+                    )}
+                  </div>
+                </Card>
+              ))}
+            {listData && (
+              <div>
+                <h4 className={cx(styles.subTitle, styles.adaptionList)}>
+                  Adaptation Contribution
+                </h4>
+                <ul className={styles.list}>
+                  {listData.map(item => (
+                    <li className={styles.listItem} key={item}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
