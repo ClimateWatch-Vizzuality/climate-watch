6f329868629aa45d61b1d489da5d1394fa047ce3
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-10-11 13:55:19 +0100

    Add slug to wb category

--- db/migrate/20171002162804_create_wb_indc_categories.rb
+++ db/migrate/20171002162804_create_wb_indc_categories.rb
@@ -2,6 +2,7 @@ class CreateWbIndcCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :wb_indc_categories do |t|
       t.text :name, null: false
+      t.text :slug, null: false
       t.timestamps
     end
   end
