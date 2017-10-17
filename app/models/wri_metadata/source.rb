44582e5fe62217ba1f103729d7e33f317ef0cb89
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-10-17 19:08:47 +0100

    Serialize all available metadata properties

--- app/models/wri_metadata/source.rb
+++ app/models/wri_metadata/source.rb
@@ -1,5 +1,13 @@
 module WriMetadata
   class Source < ApplicationRecord
     has_many :values, class_name: 'WriMetadata::Value'
+
+    def value_by_property(property)
+      self.values.
+        find do |value|
+          value.property.slug == property.to_s
+        end.
+        value
+    end
   end
 end
--- app/serializers/api/v1/wri_metadata/source_serializer.rb
+++ app/serializers/api/v1/wri_metadata/source_serializer.rb
@@ -2,49 +2,27 @@ module Api
   module V1
     module WriMetadata
       class SourceSerializer < ActiveModel::Serializer
-        attribute :name, key: :slug
         attribute :title
-        attribute :link
-        attribute :organization, key: :sourceOrganization
+        attribute :subtitle
+        attribute :technical_title
+        attribute :source_organization
+        attribute :learn_more_link
         attribute :summary
         attribute :description
+        attribute :cautions
+        attribute :geographic_coverage
+        attribute :date_of_content
+        attribute :frequency_of_updates
+        attribute :summary_of_licenses
+        attribute :terms_of_service_link
+        attribute :citation
+        attribute :published_language
+        attribute :published_title
 
-        def title
-          select_by_property('title_75_character_limit')
+        def read_attribute_for_serialization(attribute)
+          object.value_by_property(attribute)
         end
 
-        def link
-          select_by_property('learn_more_link')
-        end
-
-        def organization
-          select_by_property('source_organization')
-        end
-
-        def summary
-          select_by_property(
-            'functionsummary_tagline_description'\
-            '_of_what_data_says_150_character_limit'
-          )
-        end
-
-        def description
-          select_by_property(
-            'description_recap_data_name_source_'\
-            'organization_objectives_data_type_methodology'
-          )
-        end
-
-        private
-
-        def select_by_property(property)
-          object.values.
-            select do |value|
-              value.property.name == property
-            end.
-            first.
-            value
-        end
       end
     end
   end
