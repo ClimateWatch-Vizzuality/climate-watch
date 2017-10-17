a028893da405f8a12725724477805a390519980b
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-10-17 16:55:04 +0100

    Add spec

--- spec/controllers/api/v1/ndcs_controller_spec.rb
+++ spec/controllers/api/v1/ndcs_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe Api::V1::NdcsController, type: :controller do
   context do
+    let(:parsed_body) {
+      JSON.parse(response.body)
+    }
     let!(:some_cait_indc_values) {
       list = FactoryGirl.create_list(:cait_indc_indicator_with_dependants, 3)
       MaterializedView.refresh(
@@ -23,8 +26,20 @@ describe Api::V1::NdcsController, type: :controller do
 
       it 'lists all cait indc indicators' do
         get :index
-        parsed_body = JSON.parse(response.body)
         expect(parsed_body['indicators'].length).to eq(3)
+      end
+    end
+
+    describe 'GET content_overview' do
+      it 'returns a successful 200 response' do
+        code = some_cait_indc_values.first.values.first.location.iso_code3
+        get :content_overview, params: {code: code}
+        expect(response).to be_success
+      end
+
+      it 'returns a 404 not found when given an invalid iso code' do
+        get :content_overview, params: {code: 'AAAA'}
+        expect(response).to be_not_found
       end
     end
   end
