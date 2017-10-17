13202e943afe50cc9fe0f1497fda667af0d639a7
Edward Brett <e.j.a.brett@gmail.com>
2017-10-17 10:22:42 +0200

    Fix: crashing when no search entered

--- app/javascript/app/pages/ndc-country-full/ndc-country-full.js
+++ app/javascript/app/pages/ndc-country-full/ndc-country-full.js
@@ -34,7 +34,6 @@ const mapStateToProps = (state, { match }) => {
     content: getSelectedContent(contentData),
     contentOptions: getContentOptions(contentData),
     contentOptionSelected: getContentOptionSelected(contentData),
-    search: search.search,
     idx: search.idx
   };
 };
@@ -52,10 +51,12 @@ class NDCCountryFullContainer extends PureComponent {
   onSearchChange = option => {
     const { match, fetchCountryNDCFull } = this.props;
     const { iso } = match.params;
-    const optionValues = {
-      [option.groupId]: option.value
-    };
-    fetchCountryNDCFull(iso, optionValues);
+    if (option.groupId) {
+      const optionValues = {
+        [option.groupId]: option.value
+      };
+      fetchCountryNDCFull(iso, optionValues);
+    }
   };
 
   onSelectChange = selected => {
