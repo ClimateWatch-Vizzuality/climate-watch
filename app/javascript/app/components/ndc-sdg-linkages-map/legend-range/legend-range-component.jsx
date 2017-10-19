a5e6cdc487bd160a64f60878763d9587fcadfd20
j8seangel <jseangel@gmail.com>
2017-10-19 12:20:35 +0200

    Show sdgs color in map and list header

--- app/javascript/app/components/ndc-sdg-linkages-map/legend-range/legend-range-component.jsx
+++ app/javascript/app/components/ndc-sdg-linkages-map/legend-range/legend-range-component.jsx
@@ -6,10 +6,14 @@ import styles from './legend-range-styles.scss';
 class LegendRange extends PureComponent {
   // eslint-disable-line react/prefer-stateless-function
   render() {
+    const colour = this.props.colour || '#e5243b';
+    const rangeStyle = {
+      backgroundImage: `linear-gradient(to bottom, ${colour}, #fff)`
+    };
     return (
       <div className={cx(styles.container, this.props.className)}>
         <span className={styles.startPoint} />
-        <span className={styles.range} />
+        <span className={styles.range} style={rangeStyle} />
         <ul className={styles.scale}>
           <li>All targets linked</li>
           <li>Some tagets linked</li>
@@ -21,6 +25,7 @@ class LegendRange extends PureComponent {
 }
 
 LegendRange.propTypes = {
+  colour: PropTypes.string,
   className: PropTypes.string
 };
 
