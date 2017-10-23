1f82fc5da2d3c5d58952b98fa1e4a82eedfe3a67
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-10-23 19:06:43 +0200

    Create vulnerability graph component - WIP

--- app/javascript/app/providers/adaptations-provider/adaptations-provider-reducers.js
+++ app/javascript/app/providers/adaptations-provider/adaptations-provider-reducers.js
@@ -10,8 +10,8 @@ const setLoaded = (loaded, state) => ({ ...state, loaded });
 const setError = (state, error) => ({ ...state, error });
 
 export default {
-  fetchEmissionsMetaInit: state => setLoading(true, state),
-  fetchEmissionsMetaReady: (state, { payload }) =>
+  fetchAdaptationsInit: state => setLoading(true, state),
+  fetchAdaptationsReady: (state, { payload }) =>
     setLoaded(true, setLoading(false, { ...state, data: payload })),
-  fetchEmissionsMetaFail: state => setLoading(setError(state, true), false)
+  fetchAdaptationsFail: state => setLoading(setError(state, true), false)
 };
