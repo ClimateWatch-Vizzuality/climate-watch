2067a2a90c8337ac636a4df40fa98197c4a00ed6
Alvaro Leal <aleal@icometrix.com>
2017-10-26 18:04:32 +0100

    Create teaser component and fix padding

---
+++ app/javascript/app/components/teaser/teaser.js
@@ -0,0 +1,6 @@
+import Component from './teaser-component';
+
+export { default as component } from './teaser-component';
+export { default as styles } from './teaser-styles';
+
+export default Component;
