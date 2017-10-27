87ac384ea149049f0040db426091727da8d35138
Kuba Kuźma <kuba@jah.pl>
2017-10-27 15:20:18 +0200

    replaced double quotes with single quotes

--- spec/factories/quantification_values.rb
+++ spec/factories/quantification_values.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :quantification_value, class: 'Quantification::Value' do
     location
     association :label, factory: :quantification_label
-    year "2025"
+    year '2025'
     value 1.5
     range false
   end
