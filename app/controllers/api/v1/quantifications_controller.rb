e1d6dad9ee7f7c60a863082a8b05cc7e4e441748
Kuba Kuźma <kuba@jah.pl>
2017-10-30 11:40:44 +0100

    added quantification endpoint

---
+++ app/controllers/api/v1/quantifications_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class QuantificationsController < ApiController
+      def index
+        values = ::Quantification::Value.
+          includes(:location, :label)
+
+        if locations
+          values = values.where(locations: {iso_code3: locations})
+        end
+
+        render json: values,
+               each_serializer: Api::V1::Quantification::ValueSerializer
+      end
+
+      private
+
+      def locations
+        params[:location].presence && params[:location].split(',')
+      end
+    end
+  end
+end
