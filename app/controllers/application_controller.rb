9e4bda8664af5d025d2096666794f1338476a445
Simao Belchior <simaobelchior@gmail.com>
2017-10-31 20:54:21 +0100

    Removes basic http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :basic_http_authentication
-
   def index
     @data = {}
     @isProduction = Rails.env.production?
     render 'index'
-  end
-
-  private
-
-  def basic_http_authentication
-    return unless ENV['PASSWORD_PROTECT']
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['AUTH_USERNAME'] && password == ENV['AUTH_PASSWORD']
-    end
   end
 end
