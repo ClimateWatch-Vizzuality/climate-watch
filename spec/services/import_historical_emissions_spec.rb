f789080a0c43bf098b495598856a469fb6c1de28
Simao Belchior <simaobelchior@gmail.com>
2017-11-04 09:32:13 +0100

    Fixes file name

--- spec/services/import_historical_emissions_spec.rb
+++ spec/services/import_historical_emissions_spec.rb
@@ -8,15 +8,15 @@ object_contents = {
     PIK,,Total including LUCF,
     UNFCCC,AI,Total GHG emissions without LULUCF,
   END
-  "#{CW_FILES_PREFIX}historical_emissions/CW_HistoricalEmissions_sampledata_CAIT.csv" => <<~END,
+  "#{CW_FILES_PREFIX}historical_emissions/CW_HistoricalEmissions_CAIT.csv" => <<~END,
     Country,Source,Sector,Gas,GWP,1990,1991,1992
     ABW,CAIT,Total excluding LUCF,All GHG,AR2,15.21284765,15.28643902,14.01053087,14.02811754
   END
-  "#{CW_FILES_PREFIX}historical_emissions/CW_HistoricalEmissions_sampledata_PIK.csv" => <<~END,
+  "#{CW_FILES_PREFIX}historical_emissions/CW_HistoricalEmissions_PIK.csv" => <<~END,
     Country,Source,Sector,Gas,GWP,1850,1851,1852
     ABW,PIK,Total including LUCF,CH4,AR2,0.00000469,0.00000475,0.00000483
   END
-  "#{CW_FILES_PREFIX}historical_emissions/CW_HistoricalEmissions_sampledata_UNFCCC.csv" => <<~END,
+  "#{CW_FILES_PREFIX}historical_emissions/CW_HistoricalEmissions_UNFCCC.csv" => <<~END,
     Country,Source,Sector,Gas,GWP,1990,1991,1992
     ABW,UNFCCC,Total GHG emissions without LULUCF,Aggregate F-gases,AR4,6.242714951,6.264371648,6.183325393
   END
