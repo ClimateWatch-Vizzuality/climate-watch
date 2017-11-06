83cfef7cd373bf442b8930afa77cf5a839549b3f
Edward Brett <e.j.a.brett@gmail.com>
2017-11-06 19:43:30 +0100

    Show selectedLabel if required in component

--- app/javascript/app/components/multiselect/multiselect-component.jsx
+++ app/javascript/app/components/multiselect/multiselect-component.jsx
@@ -23,9 +23,12 @@ class Multiselect extends Component {
   }
 
   getSelectorValue() {
-    const { values, options } = this.props;
+    const { values, options, selectedLabel } = this.props;
     const { search } = this.state;
     const hasValues = values && values.length;
+    if (selectedLabel) {
+      return <span>{selectedLabel}</span>;
+    }
     if (hasValues && !search) {
       return values.length === options.length ? (
         <span>All selected</span>
@@ -89,7 +92,8 @@ Multiselect.propTypes = {
   onMultiValueChange: Proptypes.func,
   filterOptions: Proptypes.func,
   handleChange: Proptypes.func,
-  label: Proptypes.string
+  label: Proptypes.string,
+  selectedLabel: Proptypes.string
 };
 
 Multiselect.defaultProps = {
