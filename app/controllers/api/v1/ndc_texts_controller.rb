de0e79d414bd627f34ea7fb71c20bc412ed7ec59
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-11-07 17:02:07 +0000

    Make rubocop happy
    Also tuned some rubocop settings

--- app/controllers/api/v1/ndc_texts_controller.rb
+++ app/controllers/api/v1/ndc_texts_controller.rb
@@ -5,7 +5,7 @@ module Api
         ndcs = Ndc.includes(:location)
         ndcs =
           if params[:target] || params[:goal] || params[:sector]
-            with_linkage_highlights(ndcs, false, false)
+            with_linkage_highlights(ndcs, false)
           elsif params[:query]
             with_highlights(ndcs, false, false)
           else
@@ -60,85 +60,67 @@ module Api
         highlighted_ndcs
       end
 
-      def highlight_text(text, linkages)
-        return text unless linkages.length.positive?
-
+      def highlight_indices(linkages, text_length)
         indices = linkages.reduce([]) do |list, linkage|
           list.push(linkage.starts_at, linkage.ends_at)
         end
 
         if indices.length.positive?
           indices.unshift(1)
-          indices.push(text.length)
+          indices.push(text_length)
         end
 
-        parts = indices.each_cons(2).map.with_index do |(i1, i2), idx|
-          if idx % 2 == 0 && idx == 0
-            text[i1..(i2-1)]
-          elsif idx % 2 == 0
-            text[(i1+1)..(i2-1)]
-          else
-            text[i1..i2]
+        indices
+      end
+
+      def highlight_text(text, linkages)
+        return text unless linkages.length.positive?
+
+        parts = highlight_indices(linkages, text.length).
+          each_cons(2).
+          map.with_index do |(i1, i2), idx|
+            if idx.zero?
+              text[i1..(i2 - 1)]
+            elsif idx.even?
+              text[(i1 + 1)..(i2 - 1)]
+            else
+              text[i1..i2]
+            end
           end
-        end
 
-        idx = 0
-        parts.reduce("") do |memo, part|
-          memo +=
-            if idx % 2 > 0
+        parts.each.with_index.reduce('') do |memo, (part, idx)|
+          memo +
+            if idx.even?
+              part
+            else
               [
                 Ndc::PG_SEARCH_HIGHLIGHT_START,
                 part,
                 Ndc::PG_SEARCH_HIGHLIGHT_END
               ].join
-            else
-              part
             end
-          idx += 1
-          memo
         end
       end
 
       def with_linkage_highlights(
         ndcs,
-        include_not_matched = true,
-        highlight_matches = true
+        include_not_matched = true
       )
-        linkages = Ndc.linkages(params)
-
         unless include_not_matched
           if params[:target]
-            ids = ::NdcSdg::Target.
-              includes(:ndc_targets).
-              find_by!(number: params[:target]).
-              ndc_targets.
-              map(&:ndc_id).
-              uniq
-            ndcs = ndcs.where(id: ids)
+            ndcs = ndcs.where(id: ::NdcSdg::Target.ndc_ids(params[:target]))
           end
 
           if params[:goal]
-            ids = ::NdcSdg::Goal.
-              includes(targets: :ndc_targets).
-              find_by!(number: params[:goal]).
-              targets.
-              flat_map(&:ndc_targets).
-              map(&:ndc_id).
-              uniq
-            ndcs = ndcs.where(id: ids)
+            ndcs = ndcs.where(id: ::NdcSdg::Goal.ndc_ids(params[:goal]))
           end
 
           if params[:sector]
-            ids = ::NdcSdg::Sector.
-              includes(:ndc_targets).
-              find_by!(id: params[:sector]).
-              ndc_targets.
-              map(&:ndc_id).
-              uniq
-            ndcs = ndcs.where(id: ids)
+            ndcs = ndcs.where(id: ::NdcSdg::Sector.ndc_ids(params[:sector]))
           end
         end
 
+        linkages = Ndc.linkages(params)
         ndcs.map do |ndc|
           ndc_linkages = linkages.
             select { |linkage| linkage.ndc_id == ndc.id }
