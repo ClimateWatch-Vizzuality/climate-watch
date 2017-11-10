512cbf66e5582abe0d3110e091193519a5876224
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-11-10 16:47:42 +0000

    Fix lost query params on route change

--- app/javascript/app/components/emission-pathways-table-wrapper/emission-pathways-table-wrapper-selectors.js
+++ app/javascript/app/components/emission-pathways-table-wrapper/emission-pathways-table-wrapper-selectors.js
@@ -1,14 +1,17 @@
 import { createSelector } from 'reselect';
 
-const getRoutes = routeData => (routeData && routeData.routes) || null;
-const getHash = routeData => (routeData && routeData.hash) || null;
+const getRoutes = routeData => routeData.routes || null;
+const getHash = routeData => routeData.hash || null;
+const getSearch = routeData =>
+  (routeData.location && routeData.location.search) || null;
 export const getRouteLinks = createSelector(
-  [getRoutes, getHash],
-  (routes, hash) =>
+  [getRoutes, getHash, getSearch],
+  (routes, hash, search) =>
     routes &&
     routes.filter(r => r.anchor).map(route => ({
       label: route.label,
       path: route.path,
+      search,
       hash
     }))
 );
