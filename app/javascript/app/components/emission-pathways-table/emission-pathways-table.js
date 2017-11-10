dbbc9d60c47b1e4e0eea3900a2706b77905d8ddb
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-11-10 12:21:48 +0000

    Always use map state to props when using a selector

--- app/javascript/app/components/emission-pathways-table/emission-pathways-table.js
+++ app/javascript/app/components/emission-pathways-table/emission-pathways-table.js
@@ -1,31 +1,17 @@
-import { PureComponent, createElement } from 'react';
 import { withRouter } from 'react-router';
-import PropTypes from 'prop-types';
-
+import { connect } from 'react-redux';
 import { getRouteLinks } from './emission-pathways-table-selectors';
-import EmissionPathwaysTableComponent from './emission-pathways-table-component';
+import Component from './emission-pathways-table-component';
 
-class EmissionPathwaysTableContainer extends PureComponent {
-  render() {
-    const routeData = {
-      routes: this.props.routes,
-      hash: location && location.hash
-    };
+const mapStateToProps = (state, { location, routes }) => {
+  const routeData = {
+    routes,
+    hash: location && location.hash
+  };
 
-    return createElement(
-      EmissionPathwaysTableComponent,
-      Object.assign(
-        {
-          ...this.props
-        },
-        { anchorLinks: getRouteLinks(routeData) }
-      )
-    );
-  }
-}
-
-EmissionPathwaysTableContainer.propTypes = {
-  routes: PropTypes.array
+  return {
+    anchorLinks: getRouteLinks(routeData)
+  };
 };
 
-export default withRouter(EmissionPathwaysTableContainer);
+export default withRouter(connect(mapStateToProps, null)(Component));
