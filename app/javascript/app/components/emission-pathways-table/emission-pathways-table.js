8f7db4040992e30130121a0d74992e7e1fa3ff84
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-11-13 10:46:47 +0100

    Add search funcionality to table in every field

--- app/javascript/app/components/emission-pathways-table/emission-pathways-table.js
+++ app/javascript/app/components/emission-pathways-table/emission-pathways-table.js
@@ -1,9 +1,71 @@
+import { PureComponent, createElement } from 'react';
 import { connect } from 'react-redux';
-import { filteredModelData } from './emission-pathways-table-selectors';
+import { withRouter } from 'react-router';
+import { getLocationParamUpdated } from 'utils/navigation';
+import qs from 'query-string';
+import PropTypes from 'prop-types';
+import { filteredDataBySearch } from './emission-pathways-table-selectors';
 import Component from './emission-pathways-table-component';
 
-const mapStateToProps = (state, { model }) => ({
-  data: filteredModelData({ state, model })
-});
+const mapStateToProps = (state, { model }) => {
+  const search = qs.parse(location.search);
+  const EspData = {
+    state,
+    model,
+    query: search.search
+    // categorySelected: search.category,
+    // indicatorSelected: search.indicator
+  };
 
-export default connect(mapStateToProps, null)(Component);
+  return {
+    data: filteredDataBySearch(EspData),
+    query: EspData.query
+    // selectedCategory: getSelectedCategory(ndcsWithSelection),
+    // selectedIndicator: getSelectedIndicator(ndcsWithSelection)
+  };
+};
+
+class EmissionPatwaysTableComponent extends PureComponent {
+  // handleCategoryChange = category => {
+  //   this.updateUrlParam({
+  //     name: 'category',
+  //     value: category.value,
+  //     clear: true
+  //   });
+  // };
+
+  // handleIndicatorChange = indicator => {
+  //   this.updateUrlParam({ name: 'indicator', value: indicator.value });
+  // };
+
+  handleSearchChange = query => {
+    this.updateUrlParam({ name: 'search', value: query });
+  };
+
+  updateUrlParam(param) {
+    const { history, location } = this.props;
+    history.replace(getLocationParamUpdated(location, param));
+  }
+
+  render() {
+    const { query } = this.props;
+    const noContentMsg = query
+      ? 'No results found'
+      : 'There is no data for this indicator';
+    return createElement(Component, {
+      ...this.props,
+      noContentMsg,
+      handleSearchChange: this.handleSearchChange
+    });
+  }
+}
+
+EmissionPatwaysTableComponent.propTypes = {
+  query: PropTypes.string,
+  history: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired
+};
+
+export default withRouter(
+  connect(mapStateToProps, null)(EmissionPatwaysTableComponent)
+);
