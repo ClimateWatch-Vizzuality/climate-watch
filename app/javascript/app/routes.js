33ff7b737a209f9c4e5412f0c2c9ab3ce7dd6a47
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-11-13 13:19:27 +0100

    Rename Wrapper component to Menu

--- app/javascript/app/routes.js
+++ app/javascript/app/routes.js
@@ -20,6 +20,7 @@ import NDCSearch from 'pages/ndc-search';
 import GHGEmissions from 'pages/ghg-emissions';
 import EmissionPathways from 'pages/emission-pathways';
 import EmissionPathwaysTable from 'components/emission-pathways-table';
+import EmissionPathwaysTableMenu from 'components/emission-pathways-table-menu';
 import EmissionPathwayGraph from 'components/emission-pathway-graph';
 import EmissionPathwaysTableWrapper from 'components/emission-pathways-table-wrapper';
 import About from 'pages/about';
@@ -286,7 +287,7 @@ export default [
             label: 'Models, Scenarios & Indicators',
             anchor: true,
             nav: true,
-            component: EmissionPathwaysTableWrapper
+            component: EmissionPathwaysTableMenu
           }
         ],
         routes: [
