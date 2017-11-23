1f40d0439415764113dc7a0919407314f8ad748c
Tiago Santos <santos.tiago@gmail.com>
2017-11-23 17:47:08 +0100

    Changed VCR to allow external connections when no cassete is provided

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'vcr'
 
 VCR.configure do |config|
   config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.allow_http_connections_when_no_cassette = true
   config.hook_into :webmock
 end
 
