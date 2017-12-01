ee1cf79dd7f57d10bc91f6d13aa0845d82fbd6f2
Victor Saiz <vs@vsaiz.me>
2017-12-01 16:26:34 +0100

    graphs version with structured groupByScenarios

--- app/javascript/app/utils/graphs.js
+++ app/javascript/app/utils/graphs.js
@@ -1,6 +1,7 @@
 import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
 import chroma from 'chroma-js';
+import map from 'lodash/map';
 import find from 'lodash/find';
 import findIndex from 'lodash/findIndex';
 
@@ -17,18 +18,55 @@ export const sortLabelByAlpha = array =>
     return 0;
   });
 
+// pick from constants when merged
+const COLORS = [
+  '#2D9290',
+  '#B25BD0',
+  '#7EA759',
+  '#FF0D3A',
+  '#687AB7',
+  '#BC6332',
+  '#F97DA1',
+  '#00971D',
+  '#F1933B',
+  '#938126',
+  '#2D9290',
+  '#B25BD0',
+  '#7EA759'
+];
+
+
+const flatmap = (res, v) => Object.assign(res, v);
+export const extractValues = data => key => data.map(d =>
+  map(d, (v, k) => ({ [k]: d[k][key] || v }))
+    .reduce(flatmap, {}));
+
+export const columns = data => Object.keys(data[0]).map(d => ({ label: data[0][d].label, value: d }));
+
 const found = v => v !== -1;
+
 export const groupDataByScenario = (data, scenarios) =>
   data.reduce((res, d) => {
-    const x = d.year;
-    const idx = findIndex(res, { x });
+    const year = { label: 'year', value: d.year };
+    const idx = findIndex(res, y =>
+      y.year.value === year.value
+    );
     // append y values if x exists, otherwise create new row
-    const row = found(idx) ? res[idx] : { x };
-    row[scenarios
+    const row = found(idx)
+      ? res[idx]
+      : { year };
+
+    const label = scenarios
       // use real name if scenarios are passed
       ? find(scenarios, { id: d.scenario_id }).name
-      : `y${d.scenario_id}`
-    ] = parseInt(d.value, 10);
+      : d.scenario_id;
+    const key = getColumnValue(label);
+
+    // row[key] = {
+    //   label,
+    //   value: parseInt(d.value, 10)
+    // };
+    row[key] = parseInt(d.value, 10);
     // override row if exists otherwise append new row
     res[found(idx) ? idx : res.length] = row;
     return res;
@@ -51,7 +89,8 @@ export const sortEmissionsByValue = array =>
     return 0;
   });
 
-export const getYColumnValue = column => `y${upperFirst(camelCase(column))}`;
+export const getColumnValue = column => upperFirst(camelCase(column));
+export const getYColumnValue = column => `y${getColumnValue(column)}`;
 
 export const getThemeConfig = (columns, colors) => {
   const theme = {};
