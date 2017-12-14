d33968cfc6373d0139b8657b4e96f2bdff027ec1
Simao Belchior <simaobelchior@gmail.com>
2017-12-14 12:35:45 +0100

    Adds spec to describe the desired behaviour

--- spec/controllers/api/v1/historical_emissions_controller_spec.rb
+++ spec/controllers/api/v1/historical_emissions_controller_spec.rb
@@ -1,6 +1,68 @@
 require 'rails_helper'
 
 describe Api::V1::HistoricalEmissionsController, type: :controller do
+
+  context do
+    let(:location) {FactoryGirl.create(:location)}
+    let(:data_source) {FactoryGirl.create(:historical_emissions_data_source)}
+    let(:gas) {FactoryGirl.create(:historical_emissions_gas)}
+    let(:gwp2) {FactoryGirl.create(:historical_emissions_gwp, name: 'AR2')}
+    let(:gwp4) {FactoryGirl.create(:historical_emissions_gwp, name: 'AR4')}
+    let(:sector) {FactoryGirl.create(:historical_emissions_sector,
+                                  data_source: data_source)}
+    let!(:some_historical_emissions_records) {
+      FactoryGirl.create(:historical_emissions_record,
+        data_source: data_source,
+        sector: sector,
+        gas: gas,
+        gwp: gwp2,
+        location: location
+      )
+      FactoryGirl.create(:historical_emissions_record,
+        data_source: data_source,
+        sector: sector,
+        gas: gas,
+        gwp: gwp4,
+        location: location
+      )
+    }
+
+    let(:data_source2) {FactoryGirl.create(:historical_emissions_data_source)}
+    let!(:record_with_just_ar2) {
+      FactoryGirl.create(:historical_emissions_record,
+        data_source: data_source2,
+        sector: sector,
+        gas: gas,
+        gwp: gwp2,
+        location: location
+      )
+    }
+
+    it 'when filtering by location, source, and gas, return 1 record' do
+      get :index, params: {source: data_source.id, gas: gas.id,
+                           location: location.id}
+      parsed_body = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(parsed_body.length).to eq(1)
+    end
+
+    it 'returns results for the correct gwp' do
+      get :index, params: {source: data_source.id, gas: gas.id,
+                           location: location.id, gwp: gwp2.id}
+      parsed_body = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(parsed_body.length).to eq(1)
+    end
+
+    it 'returns the AR2 record when no AR4 is present' do
+      get :index, params: {source: data_source2.id, gas: gas.id,
+                           location: location.id}
+      parsed_body = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(parsed_body.length).to eq(1)
+    end
+  end
+
   context do
     let!(:some_historical_emissions_records) {
       data_source = FactoryGirl.create(:historical_emissions_data_source)
