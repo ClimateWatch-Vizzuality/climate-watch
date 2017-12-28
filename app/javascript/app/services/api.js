0798936ece5ded09a7d071aeb30df577e4d2dfbe
j8seangel <jseangel@gmail.com>
2017-12-28 15:59:22 +0100

    Api service refactor

--- app/javascript/app/services/api.js
+++ app/javascript/app/services/api.js
@@ -1,64 +1,51 @@
 import isFunction from 'lodash/isFunction';
 
 const { ESP_API } = process.env;
+const { CW_API } = process.env;
 
 const mockUrl = (endpoint, params) =>
   `/mocks/${endpoint}.json${params ? `?${params}` : ''}`;
 
-const api = (baseURL = '', version = 'v1') => ({
+const baseConfig = {
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  credentials: 'same-origin'
+};
+
+const handleResponse = d => {
+  const data = isFunction(d.json) ? d.json() : d;
+  // fallback
+  data.json = () => data;
+  return data;
+};
+
+const api = (baseURL = '') => ({
   get: (endpoint, params, mock = false) => {
     const url = mock
       ? mockUrl(endpoint, params)
-      : `${baseURL}/api/${version}/${endpoint}${params ? `?${params}` : ''}`;
-    return fetch(url, {
-      credentials: 'same-origin'
-    }).then(d => {
-      const data = isFunction(d.json) ? d.json() : d;
-      // fallback
-      data.json = () => data;
-      return data;
-    });
+      : `${baseURL}/${endpoint}${params ? `?${params}` : ''}`;
+    return fetch(url, baseConfig).then(handleResponse);
   },
   post: (endpoint, params, mock = false) => {
-    const url = mock
-      ? mockUrl(endpoint, params)
-      : `${baseURL}/api/${version}/${endpoint}`;
+    const url = mock ? mockUrl(endpoint, params) : `${baseURL}/${endpoint}`;
     return fetch(url, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      credentials: 'same-origin',
+      ...baseConfig,
       method: 'POST',
       body: JSON.stringify(params)
-    }).then(d => {
-      const data = isFunction(d.json) ? d.json() : d;
-      // fallback
-      data.json = () => data;
-      return data;
-    });
+    }).then(handleResponse);
   },
   patch: (endpoint, params, mock = false) => {
-    const url = mock
-      ? mockUrl(endpoint, params)
-      : `${baseURL}/api/${version}/${endpoint}`;
+    const url = mock ? mockUrl(endpoint, params) : `${baseURL}/${endpoint}`;
     return fetch(url, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      credentials: 'same-origin',
+      ...baseConfig,
       method: 'PATCH',
       body: JSON.stringify(params)
-    }).then(d => {
-      const data = isFunction(d.json) ? d.json() : d;
-      // fallback
-      data.json = () => data;
-      return data;
-    });
+    }).then(handleResponse);
   }
 });
 
 export const EPAPI = api(ESP_API);
 
-export const CWAPI = api();
+export const CWAPI = api(CW_API);
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -13,7 +13,7 @@ module.exports = merge(sharedConfig, {
   stats: 'normal',
 
   plugins: [
-    new webpack.EnvironmentPlugin(['GOOGLE_ANALYTICS_ID']),
+    new webpack.EnvironmentPlugin(['GOOGLE_ANALYTICS_ID', 'CW_API', 'ESP_API']),
 
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
