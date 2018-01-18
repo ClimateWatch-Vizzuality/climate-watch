60a5e85f991ca8508af2b27273f8ca73669de556
Javi Abia <javier.abia@vizzuality.com>
2018-01-18 18:59:55 +0100

    Add images folder

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -1,29 +1,45 @@
 module SeoHelper
-
-  @@TITLE = "Climate Watch"
-  @@HOME_PAGE = "Climate Watch is an open online platform designed to empower users with the climate data, visualizations and resources they need to gather insights on national and global progress on climate change, sustainable development, and help advance the goals of the Paris Agreement."
-  @@COUNTRY_PROFILES = "Snapshots of countries climate progress, including historical and projected emissions, their Nationally Determined Contribution (NDC), risks and vulnerability to climate change, and linkages between NDCs and the Sustainable Development Goals (SDGs)."
-  @@NDC_CONTENT = "Analyze and compare every national climate pledge (Nationally Determined Contribution – or NDC) under the Paris Agreement, with the ability to search key words and see summaries by topic across all."
-  @@NDC_SDG_LINKAGES = "Comprehensive mapping of linkages between Nationally Determined Contributions (NDCs) and the Sustainable Development Goals (SDGs) and associated targets of the 2030 Agenda for Sustainable Development."
-  @@HISTORICAL_GHG_EMISIONS = "Analyze and visualize latest available international greenhouse gas emissions data. Climate Watch lets you explore global emissions by sector, gases, countries, or regions."
-  @@EMISSION_PATHWAYS = "Data and visuals of emission scenario pathways for major emitting countries and sectors, derived from a growing library of models."
-  @@ABOUT = "Climate Watch is an open online platform designed to empower users with the climate data, visualizations and resources they need to gather insights on national and global progress on climate change, sustainable development, and help advance the goals of the Paris Agreement."
+  @@title = 'Climate Watch'
+  @@home_page = 'Climate Watch is an open online platform designed to empower'\
+  ' users with the climate data, visualizations and resources they need'\
+  ' to gather insights on national and global progress on climate change,'\
+  ' sustainable development, and help advance the goals of the Paris Agreement.'
+  @@country_profiles = 'Snapshots of countries climate progress, including'\
+  ' historical and projected emissions, their Nationally Determined Contribution'\
+  ' (NDC), risks and vulnerability to climate change, and linkages between NDCs'\
+  ' and the Sustainable Development Goals (SDGs).'
+  @@ndc_content = 'Analyze and compare every national climate pledge '\
+  '(Nationally Determined Contribution – or NDC) under the Paris Agreement, '\
+  'with the ability to search key words and see summaries by topic across all.'
+  @@ndc_sdg_linkages = 'Comprehensive mapping of linkages between Nationally '\
+  'Determined Contributions (NDCs) and the Sustainable Development Goals (SDGs)'\
+  ' and associated targets of the 2030 Agenda for Sustainable Development.'
+  @@historical_ghg_emissions = 'Analyze and visualize latest available international'\
+  ' greenhouse gas emissions data. Climate Watch lets you explore global emissions'\
+  ' by sector, gases, countries, or regions.'
+  @@emission_pathways = 'Data and visuals of emission scenario pathways for major'\
+  ' emitting countries and sectors, derived from a growing library of models.'
+  @@about = 'Climate Watch is an open online platform designed to empower users '\
+  'with the climate data, visualizations and resources they need to gather insights'\
+  ' on national and global progress on climate change, sustainable development, '\
+  'and help advance the goals of the Paris Agreement.'
 
   @@subtitles = {
-    "/" => "Data for climate action",
-    "/ndcs" => 'NDCs',
-    "/ndcs-sdg" => 'NDCs-SDG linkages',
-    "/emission-pathways/models" => 'Emission pathways',
-    "/countries" => "Country profiles",
-    "/ghg-emissions" => "Historical GHG emissions"
+    '/' => 'Data for climate action',
+    '/ndcs' => 'NDCs',
+    '/ndcs-sdg' => 'NDCs-SDG linkages',
+    '/emission-pathways/models' => 'Emission pathways',
+    '/countries' => 'Country profiles',
+    '/ghg-emissions' => 'Historical GHG emissions'
   }
 
   @@images_urls = {
-    "/ndcs" => "http://0.0.0.0:8080/packs/ndc-explore-screenshot.jpg",
-    "/ndcs-sdg" => "http://0.0.0.0:8080/packs/ndc-sdg-screenshot.jpg",
-    "/emission-pathways/models" => "http://0.0.0.0:8080/packs/sectors-screenshot.jpg",
-    "/countries" => "http://0.0.0.0:8080/packs/country-screenshot.jpg",
-    "/ghg-emissions" => "http://0.0.0.0:8080/packs/sectors-screenshot.jpg"
+    '/ndcs' => '/images/ndc-explore-screenshot.jpg',
+    '/ndcs-sdg' => '/images/ndc-sdg-screenshot.jpg',
+    '/emission-pathways' => '/images/emission-pathways-screenshot.jpg',
+    '/emission-pathways/models' => '/images/emission-pathways-screenshot.jpg',
+    '/countries' => '/images/country-screenshot.jpg',
+    '/ghg-emissions' => '/images/sectors-screenshot.jpg'
   }
 
   def split_country_name(path)
@@ -32,39 +48,61 @@ module SeoHelper
 
   def ndc_country(path)
     @country_name = split_country_name(path)
-    "Explore the Commitments (NDCs) made by #{@country_name} to act on climate change, as part of the Paris Agreement"
+    "Explore the Commitments (NDCs) made by #{@country_name} to act on climate'\
+    ' change, as part of the Paris Agreement"
   end
 
   def image_src(path)
     if @@images_urls[path]
-      asset_path(@@images_urls[path])
+      @@images_urls[path]
     else
-      asset_path("http://0.0.0.0:8080/packs/country-screenshot.jpg")
+      asset_path('/images/country-screenshot.jpg')
     end
   end
 
   def title_content(path)
     @subtitle = @@subtitles[path]
-    @subtitle ? "#{@@TITLE} - #{@subtitle}" : @@TITLE
+    @subtitle ? "#{@@title} - #{@subtitle}" : @@title
   end
 
+  # def description_content(path)
+  #   if path === '/'
+  #     @@home_page
+  #   elsif path === '/ndcs'
+  #     @@ndc_content
+  #   elsif path === '/ndcs-sdg'
+  #     @@ndc_sdg_linkages
+  #   elsif path.include? '/ndcs/country'
+  #     ndc_country(path)
+  #   elsif path.include? '/countries'
+  #     @@country_profiles
+  #   elsif path === '/ghg-emissions'
+  #     @@historical_ghg_emissions
+  #   elsif path.include? '/emission-pathways'
+  #     @@emission_pathways
+  #   elsif path.include? '/about'
+  #     @@about
+  #   end
+  # end
+
   def description_content(path)
-    if path === '/'
-      @@HOME_PAGE
-    elsif path === '/ndcs'
-      @@NDC_CONTENT
-    elsif path === '/ndcs-sdg'
-      @@NDC_SDG_LINKAGES
-    elsif path.include? '/ndcs/country'
+    case path
+    when '/'
+      @@home_page
+    when '/ndcs'
+      @@ndc_content
+    when '/ndcs-sdg'
+      @@ndc_sdg_linkages
+    when '/ndcs/country'
       ndc_country(path)
-    elsif path.include? '/countries'
-      @@COUNTRY_PROFILES
-    elsif path === '/ghg-emissions'
-      @@HISTORICAL_GHG_EMISIONS
-    elsif path.include? '/emission-pathways'
-      @@EMISSION_PATHWAYS
-    elsif path.include? '/about'
-      @@ABOUT
+    when '/countries'
+      @@country_profiles
+    when '/ghg-emissions'
+      @@istorical_ghg_emissions
+    when '/emission-pathways'
+      @@emission_pathways
+    when '/about'
+      @@about
     end
   end
 end
