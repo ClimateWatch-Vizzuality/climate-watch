7b2d04a30e01e9f63a631ba390f7832bb7edde5f
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-01-22 17:53:50 +0100

    Reset search when the table menu is changed

--- app/javascript/app/components/emission-pathways/emission-pathways-table-menu/emission-pathways-table-menu.js
+++ app/javascript/app/components/emission-pathways/emission-pathways-table-menu/emission-pathways-table-menu.js
@@ -1,4 +1,29 @@
 import { withRouter } from 'react-router';
+import { PureComponent, createElement } from 'react';
+import { getLocationParamUpdated } from 'utils/navigation';
+import PropTypes from 'prop-types';
 import Component from './emission-pathways-table-menu-component';
 
-export default withRouter(Component);
+class EmissionPathwaysTableMenuContainer extends PureComponent {
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.pathname !== this.props.location.pathname) {
+      this.updateUrlParam({ name: 'search', value: '' });
+    }
+  }
+
+  updateUrlParam(param) {
+    const { history, location } = this.props;
+    history.replace(getLocationParamUpdated(location, param));
+  }
+
+  render() {
+    return createElement(Component, { ...this.props });
+  }
+}
+
+EmissionPathwaysTableMenuContainer.propTypes = {
+  history: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired
+};
+
+export default withRouter(EmissionPathwaysTableMenuContainer);
