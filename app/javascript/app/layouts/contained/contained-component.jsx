cb8fafa3dc57a496f2862805d04ec69242862128
j8seangel <jseangel@gmail.com>
2018-01-22 17:52:29 +0100

    New contained layout

---
+++ app/javascript/app/layouts/contained/contained-component.jsx
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import Proptypes from 'prop-types';
+import { renderRoutes } from 'react-router-config';
+
+import CountriesProvider from 'providers/countries-provider';
+import { HOME_PAGE } from 'data/SEO';
+import { MetaDescription, SocialMetadata } from 'components/seo';
+
+import styles from './contained-styles.scss'; // eslint-disable-line
+
+class App extends PureComponent {
+  render() {
+    const { route, location } = this.props;
+    return (
+      <div>
+        <MetaDescription descriptionContext={HOME_PAGE} />
+        <SocialMetadata descriptionContext={HOME_PAGE} href={location.href} />
+        <CountriesProvider />
+        {renderRoutes(route.routes.filter(r => r.path))}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  route: Proptypes.object,
+  location: Proptypes.object.isRequired
+};
+
+export default App;
