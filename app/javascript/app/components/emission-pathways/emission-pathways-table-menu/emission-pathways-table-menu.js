1b48bf0ec157f3df80af50da467bfd3477b1f904
j8seangel <jseangel@gmail.com>
2018-01-22 18:33:39 +0100

    Simplify the search query reset

--- app/javascript/app/components/emission-pathways/emission-pathways-table-menu/emission-pathways-table-menu.js
+++ app/javascript/app/components/emission-pathways/emission-pathways-table-menu/emission-pathways-table-menu.js
@@ -1,29 +1,4 @@
 import { withRouter } from 'react-router';
-import { PureComponent, createElement } from 'react';
-import { getLocationParamUpdated } from 'utils/navigation';
-import PropTypes from 'prop-types';
 import Component from './emission-pathways-table-menu-component';
 
-class EmissionPathwaysTableMenuContainer extends PureComponent {
-  componentDidUpdate(prevProps) {
-    if (prevProps.location.pathname !== this.props.location.pathname) {
-      this.updateUrlParam({ name: 'search', value: '' });
-    }
-  }
-
-  updateUrlParam(param) {
-    const { history, location } = this.props;
-    history.replace(getLocationParamUpdated(location, param));
-  }
-
-  render() {
-    return createElement(Component, { ...this.props });
-  }
-}
-
-EmissionPathwaysTableMenuContainer.propTypes = {
-  history: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired
-};
-
-export default withRouter(EmissionPathwaysTableMenuContainer);
+export default withRouter(Component);
