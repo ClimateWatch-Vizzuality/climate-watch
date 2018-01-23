fb03684165a815b2eb21ebd15f306a4091d688ef
Javi Abia <javier.abia@vizzuality.com>
2018-01-23 09:28:00 +0100

    Add title sanitizer and link split.

--- app/services/import_stories.rb
+++ app/services/import_stories.rb
@@ -23,14 +23,15 @@ class ImportStories
     url = 'http://www.wri.org/blog/rss2.xml'
     rss = open(url)
     feed = RSS::Parser.parse(rss, false)
+    link_sanitizer = Rails::Html::LinkSanitizer.new
     puts "Channel Title: #{feed.channel.title}"
     feed.items.each do |item|
-      title = item.title
+      title = link_sanitizer.sanitize(item.title)
       published_at = item.pubDate
       story = Story.find_or_initialize_by(title: title,
                                           published_at: published_at)
       story.description = item.description
-      story.link = item.link
+      story.link = item.link.split(/href="|">/)[1]
       story.background_image_url = item.enclosure ? item.enclosure.url : ''
       story.tags = item.category ? item.category.content : ''
       story.save
