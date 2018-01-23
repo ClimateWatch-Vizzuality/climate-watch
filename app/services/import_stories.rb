31be1c58ac4f77f90894e52a912166196de328f5
Javi Abia <javier.abia@vizzuality.com>
2018-01-23 10:44:27 +0100

    Add domain part to construct URL in blog link

--- app/services/import_stories.rb
+++ app/services/import_stories.rb
@@ -22,16 +22,16 @@ class ImportStories
     existing = Story.count
     url = 'http://www.wri.org/blog/rss2.xml'
     rss = open(url)
-    feed = RSS::Parser.parse(rss, false)
+    feed = RSS::Parser.parse(rss)
     link_sanitizer = Rails::Html::LinkSanitizer.new
     puts "Channel Title: #{feed.channel.title}"
     feed.items.each do |item|
       title = link_sanitizer.sanitize(item.title)
       published_at = item.pubDate
-      story = Story.find_or_initialize_by(id: id,
+      story = Story.find_or_initialize_by(title: title,
                                           published_at: published_at)
       story.description = item.description
-      story.link = item.link.split(/href="|">/)[1]
+      story.link = "http://www.wri.org#{item.link.split(/href="|">/)[1]}"
       story.background_image_url = item.enclosure ? item.enclosure.url : ''
       story.tags = item.category ? item.category.content : ''
       story.save
