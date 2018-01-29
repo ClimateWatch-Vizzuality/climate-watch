4d64f1ccdc4954ed227d5f6b81bef565b8511a45
Javi Abia <javier.abia@vizzuality.com>
2018-01-29 19:00:00 +0100

    Update navbarmobile styles

--- app/javascript/app/components/navbar-mobile/navbar-mobile-component.jsx
+++ app/javascript/app/components/navbar-mobile/navbar-mobile-component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import cx from 'classNames';
@@ -13,31 +13,44 @@ import Contact from 'components/contact';
 import cwLogo from 'assets/icons/cw-logo.svg';
 import styles from './navbar-mobile-styles.scss';
 
-const NavBarMobile = ({ routes, hamburgerIsOpen }) => (
-  <div className={styles.navbarMobile}>
-    <div className={cx(styles.navbar, hamburgerIsOpen ? styles.isOpen : '')}>
-      <NavLink exact to="/">
-        <Icon className={styles.logo} icon={cwLogo} />
-      </NavLink>
-      <Hamburger
-        text={'MENU'}
-        className={cx(
-          styles.hamburgerIcon,
-          hamburgerIsOpen ? styles.isOpen : ''
-        )}
-      />
-    </div>
-    {hamburgerIsOpen && (
-      <div className={styles.fullMenu}>
-        <Nav routes={routes} allowNested={false} className={styles.navMenu} />
-        <div className={styles.toolsContainer}>
-          <ToolsNav className={styles.tools} />
-          <Contact />
+class NavBarMobile extends Component {
+  render() {
+    return (
+      <div className={styles.navbarMobile}>
+        <div
+          className={cx(
+            styles.navbar,
+            this.props.hamburgerIsOpen ? styles.isOpen : ''
+          )}
+        >
+          <NavLink exact to="/">
+            <Icon className={styles.logo} icon={cwLogo} />
+          </NavLink>
+          <Hamburger
+            text={'MENU'}
+            className={cx(
+              styles.hamburgerIcon,
+              this.props.hamburgerIsOpen ? styles.isOpen : ''
+            )}
+          />
         </div>
+        {this.props.hamburgerIsOpen && (
+          <div className={styles.fullMenu}>
+            <Nav
+              routes={this.props.routes}
+              allowNested={false}
+              className={styles.navMenu}
+            />
+            <div className={styles.toolsContainer}>
+              <ToolsNav className={styles.tools} />
+              <Contact />
+            </div>
+          </div>
+        )}
       </div>
-    )}
-  </div>
-);
+    );
+  }
+}
 
 NavBarMobile.propTypes = {
   hamburgerIsOpen: PropTypes.bool,
