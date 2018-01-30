1e296132a1998dfee0583eedb1c01b881d9a55a9
pjosh <jorgegar@gmail.com>
2018-01-30 15:36:16 +0000

    use an enviroment variable to set the CORS header

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -20,7 +20,7 @@ module Api
       before_action :set_access_control_headers
 
       def set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Origin'] = ENV['CORS_WHITELIST']
         headers['Access-Control-Allow-Methods'] = 'GET'
       end
     end
