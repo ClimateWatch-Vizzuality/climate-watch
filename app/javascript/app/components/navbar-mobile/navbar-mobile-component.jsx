ac6cc2bba3733e2566044816d9c5a7a8b1d8b98f
Javi Abia <javier.abia@vizzuality.com>
2018-01-30 13:10:44 +0100

    Add closing menu actions to nav and navbar mobile

--- app/javascript/app/components/navbar-mobile/navbar-mobile-component.jsx
+++ app/javascript/app/components/navbar-mobile/navbar-mobile-component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import cx from 'classNames';
@@ -9,52 +9,44 @@ import Nav from 'components/nav';
 import ToolsNav from 'components/tools-nav';
 import Contact from 'components/contact';
 
-// import contactIcon from 'assets/icons/contact.svg';
 import cwLogo from 'assets/icons/cw-logo.svg';
 import styles from './navbar-mobile-styles.scss';
 
-class NavBarMobile extends Component {
-  render() {
-    return (
-      <div className={styles.navbarMobile}>
-        <div
-          className={cx(
-            styles.navbar,
-            this.props.hamburgerIsOpen ? styles.isOpen : ''
-          )}
-        >
-          <NavLink exact to="/">
-            <Icon className={styles.logo} icon={cwLogo} />
-          </NavLink>
-          <Hamburger
-            text={'MENU'}
-            className={cx(
-              styles.hamburgerIcon,
-              this.props.hamburgerIsOpen ? styles.isOpen : ''
-            )}
-          />
-        </div>
-        {this.props.hamburgerIsOpen && (
-          <div className={styles.fullMenu}>
-            <Nav
-              routes={this.props.routes}
-              allowNested={false}
-              className={styles.navMenu}
-            />
-            <div className={styles.toolsContainer}>
-              <ToolsNav className={styles.tools} />
-              <Contact />
-            </div>
-          </div>
+const NavBarMobile = ({ closeMenu, hamburgerIsOpen, routes }) => (
+  <div className={styles.navbarMobile}>
+    <div className={cx(styles.navbar, hamburgerIsOpen ? styles.isOpen : '')}>
+      <NavLink exact to="/" onClick={closeMenu}>
+        <Icon className={styles.logo} icon={cwLogo} />
+      </NavLink>
+      <Hamburger
+        text={'MENU'}
+        className={cx(
+          styles.hamburgerIcon,
+          hamburgerIsOpen ? styles.isOpen : ''
         )}
+      />
+    </div>
+    {hamburgerIsOpen && (
+      <div className={styles.fullMenu}>
+        <Nav
+          routes={routes}
+          allowNested={false}
+          className={styles.navMenu}
+          isMobile
+        />
+        <div className={styles.toolsContainer}>
+          <ToolsNav className={styles.tools} />
+          <Contact />
+        </div>
       </div>
-    );
-  }
-}
+    )}
+  </div>
+);
 
 NavBarMobile.propTypes = {
   hamburgerIsOpen: PropTypes.bool,
-  routes: PropTypes.array
+  routes: PropTypes.array,
+  closeMenu: PropTypes.func
 };
 
 export default NavBarMobile;
