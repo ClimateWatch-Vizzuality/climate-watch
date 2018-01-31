22cc224adca50500200c74d05cda79f145c96565
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-01-31 13:07:09 +0100

    Add footer to embed, style and embed-test file

--- app/javascript/app/layouts/embed/embed-component.jsx
+++ app/javascript/app/layouts/embed/embed-component.jsx
@@ -2,16 +2,25 @@ import React, { PureComponent } from 'react';
 import Proptypes from 'prop-types';
 import { renderRoutes } from 'react-router-config';
 import CountriesProvider from 'providers/countries-provider';
+import cwLogo from 'assets/icons/cw-logo.svg';
+import Icon from 'components/icon';
 
 import layout from 'styles/layout';
+import styles from './embed-styles.scss';
 
 class Embed extends PureComponent {
   render() {
     const { route } = this.props;
     return (
-      <div className={layout.content}>
+      <div className={[layout.content, styles.embed]}>
         <CountriesProvider />
-        {renderRoutes(route.routes)}
+        <div className={styles.embedContent}>{renderRoutes(route.routes)}</div>
+        <div className={styles.footer}>
+          For more information, visit
+          <a href="https://www.climatewatchdata.org/" parent="_blank">
+            <Icon className={styles.logo} icon={cwLogo} />
+          </a>
+        </div>
       </div>
     );
   }
