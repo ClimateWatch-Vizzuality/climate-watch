d637f7e89aa388de8c469a35805cf3cb5426855c
Javi Abia <javier.abia@vizzuality.com>
2018-02-01 19:08:54 +0100

    Delete mobileConstrain prop and reorganize classes

--- app/javascript/app/components/section/section-component.jsx
+++ app/javascript/app/components/section/section-component.jsx
@@ -5,30 +5,21 @@ import cn from 'classnames';
 import layout from 'styles/layout.scss';
 import styles from './section-styles.scss';
 
-const Section = ({ children, backgroundImage, className, mobileConstrain }) => (
+const Section = ({ children, backgroundImage, className }) => (
   <section
     className={cn(styles.section, className)}
     style={
       backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : null
     }
   >
-    <div
-      className={cn(
-        layout.content,
-        styles.doubleFold,
-        mobileConstrain ? layout.mobileConstrain : ''
-      )}
-    >
-      {children}
-    </div>
+    <div className={cn(layout.content, styles.doubleFold)}>{children}</div>
   </section>
 );
 
 Section.propTypes = {
   children: Proptypes.node,
   backgroundImage: Proptypes.string,
-  className: Proptypes.string,
-  mobileConstrain: Proptypes.bool
+  className: Proptypes.string
 };
 
 export default Section;
