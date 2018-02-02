6222754163497092a69eda94e482228fa9caa59e
Javi Abia <javier.abia@vizzuality.com>
2018-02-02 16:59:33 +0100

    Add layout styles only if tablet or bigger

--- app/javascript/app/components/ndc-sdg/ndc-sdg-linkages-content/ndc-sdg-linkages-content-component.jsx
+++ app/javascript/app/components/ndc-sdg/ndc-sdg-linkages-content/ndc-sdg-linkages-content-component.jsx
@@ -6,6 +6,7 @@ import NdcsSdgsMetaProvider from 'providers/ndcs-sdgs-meta-provider';
 import NdcSdgLinkagesTable from 'components/ndc-sdg/ndc-sdg-linkages-table';
 import NdcSdgLinkagesMap from 'components/ndc-sdg/ndc-sdg-linkages-map';
 import ModalMetadata from 'components/modal-metadata';
+import { TabletLandscape } from 'components/responsive';
 
 import layout from 'styles/layout';
 import styles from './ndc-sdg-linkages-content-styles.scss';
@@ -20,17 +21,26 @@ class NdcSdgLinkagesContent extends PureComponent {
       handleTargetHover
     } = this.props;
     return (
-      <div className={cx(layout.content, styles.grid)}>
-        <NdcsSdgsMetaProvider />
-        <NdcSdgLinkagesTable
-          goalHover={goalHover}
-          onGoalHover={handleGoalHover}
-          targetHover={targetHover}
-          onTargetHover={handleTargetHover}
-        />
-        <NdcSdgLinkagesMap goalHover={goalHover} targetHover={targetHover} />
-        <ModalMetadata />
-      </div>
+      <TabletLandscape>
+        {matches => (
+          <div
+            className={cx({ [layout.content]: matches === true }, styles.grid)}
+          >
+            <NdcsSdgsMetaProvider />
+            <NdcSdgLinkagesTable
+              goalHover={goalHover}
+              onGoalHover={handleGoalHover}
+              targetHover={targetHover}
+              onTargetHover={handleTargetHover}
+            />
+            <NdcSdgLinkagesMap
+              goalHover={goalHover}
+              targetHover={targetHover}
+            />
+            <ModalMetadata />
+          </div>
+        )}
+      </TabletLandscape>
     );
   }
 }
