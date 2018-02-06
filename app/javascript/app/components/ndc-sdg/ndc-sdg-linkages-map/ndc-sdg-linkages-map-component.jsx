36a559c8c3160afc9f9d3ff73f89da9d4966e8c0
Javi Abia <javier.abia@vizzuality.com>
2018-02-06 13:57:11 +0100

    Add tooltip and styles to ndc-sdg map

--- app/javascript/app/components/ndc-sdg/ndc-sdg-linkages-map/ndc-sdg-linkages-map-component.jsx
+++ app/javascript/app/components/ndc-sdg/ndc-sdg-linkages-map/ndc-sdg-linkages-map-component.jsx
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import Map from 'components/map';
+import { Link } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip';
 import ButtonGroup from 'components/button-group';
 import { TabletLandscape } from 'components/responsive';
 import LegendRange from './legend-range';
@@ -10,7 +12,29 @@ import LegendSteps from './legend-steps';
 import styles from './ndc-sdg-linkages-map-styles.scss';
 
 class NdcSdgLinkagesMap extends PureComponent {
-  // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      country: ''
+    };
+  }
+
+  getTooltip() {
+    const { country } = this.state;
+    const targets =
+      this.props.goal &&
+      this.props.goal.locations &&
+      this.props.goal.locations[country.id];
+    const locations = targets && targets.length;
+    return (
+      <div className={styles.tooltip}>
+        <Link className={styles.link} to={`/countries/${country.id}`}>
+          {country.name}
+        </Link>
+        <p className={styles.tooltipText}>{locations || '0'} targets linked</p>
+      </div>
+    );
+  }
 
   getLegend() {
     const { goal } = this.props;
@@ -24,6 +48,10 @@ class NdcSdgLinkagesMap extends PureComponent {
       <LegendRange className={styles.legend} colour={colour} />
     );
   }
+
+  handleMapInteraction = country =>
+    this.setState({ country: country.properties });
+
   render() {
     return (
       <TabletLandscape>
@@ -47,12 +75,23 @@ class NdcSdgLinkagesMap extends PureComponent {
             <Map
               style={{ height: '100%', width: '100%' }}
               zoomEnable
+              dragEnable
               paths={this.props.paths}
               className={styles.map}
+              onCountryEnter={this.handleMapInteraction}
+              onCountryFocus={this.handleMapInteraction}
               onCountryClick={this.props.onCountryClick}
               controlPosition="bottom"
+              tooltipId="sdg-linkages"
             />
             {this.getLegend()}
+            <ReactTooltip
+              className={styles.tooltipContainer}
+              id="sdg-linkages"
+              delayHide={matches ? 0 : 3000}
+            >
+              {this.getTooltip()}
+            </ReactTooltip>
           </div>
         )}
       </TabletLandscape>
