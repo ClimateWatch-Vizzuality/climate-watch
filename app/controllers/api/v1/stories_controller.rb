45339e816f64a37c3eb480d8abbbce05af2c41a1
Javi Abia <javier.abia@vizzuality.com>
2018-02-08 12:03:11 +0100

    Move methods from controller to model

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -2,33 +2,8 @@ module Api
   module V1
     class StoriesController < ApiController
       def index
-        stories = stories_filter(params[:tags])
+        stories = Story.stories_filter(params[:tags], params[:limit])
         render json: stories, each_serializer: Api::V1::StorySerializer
-      end
-
-      private
-
-      def stories_filter(tags)
-        tagged_stories = tagged_stories(tags)
-        return tagged_stories if tagged_stories.length >= 5
-        more_stories = untagged_stories
-        return (tagged_stories + more_stories).first(5) if tagged_stories
-        more_stories
-      end
-
-      def tagged_stories(tags)
-        tags_array = tags.split(',')
-        # rubocop:disable LineLength
-        Story.where('tags && ARRAY[?]::varchar[]', tags_array).order(published_at: :desc).limit(stories_limit)
-        # rubocop:enable LineLength
-      end
-
-      def untagged_stories
-        Story.order(published_at: :desc).limit(stories_limit)
-      end
-
-      def stories_limit
-        params[:limit] || 5
       end
     end
   end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,20 @@
 class Story < ApplicationRecord
+  def self.stories_filter(tags, limit = 5)
+    tagged_stories = tagged_stories(tags, limit)
+    return tagged_stories if tagged_stories.length >= 5
+    more_stories = untagged_stories(limit)
+    return (tagged_stories + more_stories).first(5) if tagged_stories
+    more_stories
+  end
+
+  def self.tagged_stories(tags, limit)
+    tags_array = tags.split(',')
+    Story.where('tags && ARRAY[?]::varchar[]', tags_array).
+      order(published_at: :desc).
+      limit(limit)
+  end
+
+  def self.untagged_stories(limit)
+    Story.order(published_at: :desc).limit(limit)
+  end
 end
