762c9e8fb6378eadf1ae3e6d5978d2a67005a2de
Javi Abia <javier.abia@vizzuality.com>
2018-02-08 10:53:53 +0100

    Add filtering logic to stories controller

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -2,12 +2,28 @@ module Api
   module V1
     class StoriesController < ApiController
       def index
-        stories = Story.limit(stories_limit).order(:published_at)
-
+        stories = storiesFilter(params[:tags])
         render json: stories, each_serializer: Api::V1::StorySerializer
       end
 
       private
+
+      def storiesFilter tags
+        taggedStories = taggedStories(tags)
+        return taggedStories if taggedStories.length >= 5
+        moreStories = untaggedStories
+        return (taggedStories + moreStories).first(5) if taggedStories
+        return moreStories
+      end
+
+      def taggedStories tags
+        tagsArray = tags.split(',')
+        Story.where("tags && ARRAY[?]::varchar[]", tagsArray).order(published_at: :desc).limit(stories_limit)
+      end
+
+      def untaggedStories
+        Story.order(published_at: :desc).limit(stories_limit)
+      end
 
       def stories_limit
         params[:limit] || 5
