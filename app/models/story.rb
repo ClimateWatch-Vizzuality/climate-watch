af8434299e3409171289ef0693b353de545d7e7a
Javi Abia <javier.abia@vizzuality.com>
2018-02-08 16:19:39 +0100

    Update not_tagged_by method and specs

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,20 +1,22 @@
 class Story < ApplicationRecord
   def self.stories_filter(tags, limit = 5)
-    tagged_stories = tagged_stories(tags, limit)
+    tags_array = tags.split(',')
+    tagged_stories = tagged_stories(tags_array, limit)
     return tagged_stories if tagged_stories.length >= 5
-    more_stories = untagged_stories(limit)
+    more_stories = not_tagged_by(tags_array, limit)
     return (tagged_stories + more_stories).first(5) if tagged_stories
     more_stories
   end
 
   def self.tagged_stories(tags, limit)
-    tags_array = tags.split(',')
-    Story.where('tags && ARRAY[?]::varchar[]', tags_array).
+    Story.where('tags && ARRAY[?]::varchar[]', tags).
       order(published_at: :desc).
       limit(limit)
   end
 
-  def self.untagged_stories(limit)
-    Story.order(published_at: :desc).limit(limit)
+  def self.not_tagged_by(tags, limit)
+    Story.where('NOT tags && ARRAY[?]::varchar[] OR tags IS NULL', tags).
+      order(published_at: :desc).
+      limit(limit)
   end
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,27 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Story, type: :model do
+  before(:each) do
+    FactoryGirl.create(:story, tags: ['NDC'])
+    FactoryGirl.create(:story, tags: ['NDC', 'esp'])
+    FactoryGirl.create(:story, tags: ['OP23'])
+    FactoryGirl.create(:story, tags: nil)
+  end
   describe '#tagged_stories' do
-    it 'should return one story' do
-      FactoryGirl.create(:story, tags: ['NDC'])
-      expect(Story.tagged_stories('NDC, esp, climate watch', 5)).to have(1).items
-    end
-
-    it 'should not return any story' do
-      FactoryGirl.create(:story, tags: ['OP23'])
-      expect(Story.tagged_stories('NDC, esp', 5)).to have(0).items
+    it 'should return two stories' do
+      expect(Story.tagged_stories(['NDC', 'esp', 'climate watch'], 5)).to have(2).items
     end
   end
 
   describe '#untagged_stories' do
-    it 'should return no story' do
-      FactoryGirl.create(:story)
-      expect(Story.untagged_stories(0)).to have(0).items
-    end
-
-    it 'should return one story' do
-      FactoryGirl.create(:story)
-      expect(Story.untagged_stories(1)).to have(1).items
+    it 'should return 2 stories' do
+      expect(Story.not_tagged_by(['NDC', 'esp', 'climate watch'], 5)).to have(2).items
     end
   end
 end
