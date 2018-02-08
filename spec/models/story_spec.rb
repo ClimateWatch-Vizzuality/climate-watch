f43e2575fd0b245b1bdffc36e379456bd2741abb
Javi Abia <javier.abia@vizzuality.com>
2018-02-08 19:12:25 +0100

    Add spec to stories_filter method

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Story, type: :model do
     FactoryGirl.create(:story, tags: %w[NDC])
     FactoryGirl.create(:story, tags: %w[NDC esp])
     FactoryGirl.create(:story, tags: %w[OP23])
+    FactoryGirl.create(:story, tags: %w[OP])
     FactoryGirl.create(:story, tags: nil)
   end
   describe '#tagged_stories' do
@@ -15,7 +16,13 @@ RSpec.describe Story, type: :model do
 
   describe '#untagged_stories' do
     it 'should return 2 stories' do
-      expect(Story.not_tagged_by(['NDC', 'esp', 'climate watch'], 5)).to have(2).items
+      expect(Story.not_tagged_by(['NDC', 'esp', 'climate watch'], 5)).to have(3).items
+    end
+  end
+
+  describe '#stories_filter' do
+    it 'should return 5 stories' do
+      expect(Story.stories_filter('NDC, esp, climate watch', 5)).to have(5).items
     end
   end
 end
