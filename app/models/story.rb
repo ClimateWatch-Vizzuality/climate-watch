91bf3faf61e892e3a1efa048ab404bfc6333c4a6
Javi Abia <javier.abia@vizzuality.com>
2018-02-08 19:11:16 +0100

    Fixed limit default arg and cleaning

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,10 +1,13 @@
 class Story < ApplicationRecord
-  def self.stories_filter(tags, limit = 5)
-    tags_array = tags.split(',') if tags
+  def self.stories_filter(tags, limit)
+    tags_array = tags.try(:split, ',')
+    limit = limit.try(:to_i) || 5
+
     tagged_stories = tagged_stories(tags_array, limit)
-    return tagged_stories if tagged_stories.length >= limit.to_i
+    return tagged_stories if tagged_stories.length >= limit
+
     more_stories = not_tagged_by(tags_array, limit)
-    return (tagged_stories + more_stories).first(limit.to_i) if tagged_stories
+    return (tagged_stories + more_stories).first(limit) if tagged_stories
     more_stories
   end
 
@@ -15,7 +18,7 @@ class Story < ApplicationRecord
   end
 
   def self.not_tagged_by(tags, limit)
-    Story.where('NOT tags && ARRAY[?]::varchar[] OR tags IS NULL', tags).
+    Story.where('(NOT tags && ARRAY[?]::varchar[]) OR tags IS NULL', tags).
       order(published_at: :desc).
       limit(limit)
   end
