5d50b6a01957bbde6c60c308597dfb62ffcee178
Javi Abia <javier.abia@vizzuality.com>
2018-02-14 10:35:17 +0100

    Add anchorLinks prop withProps

--- app/javascript/app/pages/country-compare/country-compare.js
+++ app/javascript/app/pages/country-compare/country-compare.js
@@ -1,3 +1,11 @@
+import { withProps } from 'recompose';
+
+import { getAnchorLinks } from './country-compare-selectors';
+
 import Component from './country-compare-component';
 
-export default Component;
+const withSections = withProps(state => ({
+  anchorLinks: getAnchorLinks(state)
+}));
+
+export default withSections(Component);
