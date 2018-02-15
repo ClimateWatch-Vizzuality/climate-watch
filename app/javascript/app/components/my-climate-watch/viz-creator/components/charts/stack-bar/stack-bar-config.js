d749f2f2e70603b482999ad377d165894c349968
Victor Saiz <vs@vsaiz.me>
2018-02-15 12:06:39 +0100

    Last chart in

--- app/javascript/app/components/my-climate-watch/viz-creator/components/charts/stack-bar/stack-bar-config.js
+++ app/javascript/app/components/my-climate-watch/viz-creator/components/charts/stack-bar/stack-bar-config.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CHART_COLORS } from 'data/constants';
 import { assign } from 'app/utils';
-import { groupBy, pick } from '../utils';
+import { groupBy, groupBys, pick } from '../utils';
 
 import Tick from '../tick';
 
@@ -52,47 +52,49 @@ export const stackBarChart1Data = (timeSeries, indicators) =>
   makeConfig(groupBy('year', 'indicator', timeSeries, indicators));
 
 export const stackBarChart2Data = (timeseries, locations, indicators) => {
-  console.log(timeseries, locations, indicators);
-  const data = groupBy('location', 'indicator', locations, indicators);
-  // console.log(data);
-  // const keys = Object.keys(data[0]).filter(k => k !== 'year');
-  // const names = pick('name', data); // only data name key
+  const data = groupBys(
+    ['location', 'indicator'],
+    timeseries,
+    locations,
+    indicators
+  );
+  const keys = Object.keys(data[0]).filter(k => k !== 'location');
+  const names = pick('name', data); // only data name key
 
-  // return {
-  //   chart: {
-  //     data: pick('value', data), // only data value key
-  //     margin: { top: 20, right: 30, left: 20, bottom: 5 },
-  //     layout: 'vertical'
-  //   },
-  //   columns: {
-  //     x: ['year'],
-  //     y: keys
-  //   },
-  //   xAxis: {
-  //     type: 'number',
-  //     hide: true
-  //   },
-  //   yAxis: {
-  //     type: 'category',
-  //     dataKey: 'location'
-  //   },
-  //   cartesianGrid: {
-  //     horizontal: false
-  //   },
-  //   theme: keys.reduce(
-  //     (th, k, i) =>
-  //       assign(th, {
-  //         [k]: {
-  //           fill: CHART_COLORS[i],
-  //           stroke: ''
-  //         }
-  //       }),
-  //     {}
-  //   ),
-  //   legend: keys.map((k, i) => ({
-  //     color: CHART_COLORS[i],
-  //     label: names[0][k]
-  //   }))
-  // };
-  return {};
+  return {
+    chart: {
+      data: pick('value', data), // only data value key
+      margin: { top: 20, right: 30, left: 20, bottom: 5 },
+      layout: 'vertical'
+    },
+    columns: {
+      x: ['year'],
+      y: keys
+    },
+    xAxis: {
+      type: 'number',
+      hide: true
+    },
+    yAxis: {
+      type: 'category',
+      dataKey: 'location'
+    },
+    cartesianGrid: {
+      horizontal: false
+    },
+    theme: keys.reduce(
+      (th, k, i) =>
+        assign(th, {
+          [k]: {
+            fill: CHART_COLORS[i],
+            stroke: ''
+          }
+        }),
+      {}
+    ),
+    legend: keys.map((k, i) => ({
+      color: CHART_COLORS[i],
+      label: names[0][k]
+    }))
+  };
 };
--- app/javascript/app/components/my-climate-watch/viz-creator/components/charts/utils.js
+++ app/javascript/app/components/my-climate-watch/viz-creator/components/charts/utils.js
@@ -1,19 +1,20 @@
 import _camelCase from 'lodash/camelCase';
+import _startCase from 'lodash/startCase';
 import _find from 'lodash/find';
 import _difference from 'lodash/difference';
 import _isUndefined from 'lodash/isUndefined';
 import { assign } from 'app/utils';
 
+// keyA = 'year', keyB = 'indicator', data = 'timeseries', group = 'indicators'
 export const groupBy = (keyA, keyB, data, group) =>
   data.reduce((rData, ts) => {
-    const g = _find(group, { id: ts[`${keyB}_id`] }) || {
-      id: ts[`${keyB}_id`],
-      value: ts.value
-    };
+    // find in 'indicators' the current time series indicator
+    const g = _find(group, { id: ts[`${keyB}_id`] });
     if (_isUndefined(g)) return rData;
+    // find in existing result the year we are dealing with
     const foundTs = _find(rData, { [keyA]: ts[keyA] });
     const rest = _difference(rData, [foundTs]);
-    // data needs values for the graph and names for the legend
+    // we need values for the graph and names for the legend
     const item = {
       [_camelCase(g.name)]: {
         name: g.name,
@@ -24,6 +25,32 @@ export const groupBy = (keyA, keyB, data, group) =>
       ? rest.concat(Object.assign(foundTs, item))
       : rData.concat({
         [keyA]: ts[keyA],
+        ...item
+      });
+  }, []);
+
+export const groupBys = ([keyA, keyB], data, groupA, groupB) =>
+  data.reduce((rData, ts) => {
+    // find in 'indicators' the current time series indicator
+    const gA = _find(groupA, { id: ts[`${keyA}_id`] });
+    const gB = _find(groupB, { id: ts[`${keyB}_id`] });
+
+    if (_isUndefined(gA)) return rData;
+    if (_isUndefined(gB)) return rData;
+    // find in existing result the year we are dealing with
+    const foundTs = _find(rData, { [keyA]: _startCase(gA.name) });
+    const rest = _difference(rData, [foundTs]);
+    // we need values for the graph and names for the legend
+    const item = {
+      [_camelCase(gB.name)]: {
+        name: gB.name,
+        value: Number(ts.value)
+      }
+    };
+    return foundTs
+      ? rest.concat(Object.assign(foundTs, item))
+      : rData.concat({
+        [keyA]: _startCase(gA.name),
         ...item
       });
   }, []);
