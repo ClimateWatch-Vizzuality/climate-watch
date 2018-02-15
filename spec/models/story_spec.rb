7708afa38845a8503e75bb7ec79a368f8d403021
Javi Abia <javier.abia@vizzuality.com>
2018-02-15 10:37:51 +0100

    Added specs

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -6,8 +6,23 @@ RSpec.describe Story, type: :model do
     FactoryGirl.create(:story, tags: %w[NDC esp])
     FactoryGirl.create(:story, tags: %w[OP23])
     FactoryGirl.create(:story, tags: %w[OP])
+    FactoryGirl.create(:story, tags: %w[climatewatch-pinned])
+    FactoryGirl.create(:story, tags: %w[climatewatch-pinned esp])
     FactoryGirl.create(:story, tags: nil)
   end
+
+  describe '#pinned_stories' do
+    it 'should return one story' do
+      expect(Story.pinned_stories(1)).to have(1).items
+    end
+  end
+
+  describe '#pinned_stories' do
+    it 'should return one story' do
+      expect(Story.pinned_stories(2)).to have(2).items
+    end
+  end
+
   describe '#tagged_stories' do
     it 'should return two stories' do
       expect(Story.tagged_stories(['NDC', 'esp', 'climate watch'], 5)).to have(2).items
@@ -15,13 +30,13 @@ RSpec.describe Story, type: :model do
   end
 
   describe '#untagged_stories' do
-    it 'should return 2 stories' do
+    it 'should return three stories' do
       expect(Story.not_tagged_by(['NDC', 'esp', 'climate watch'], 5)).to have(3).items
     end
   end
 
   describe '#stories_filter' do
-    it 'should return 5 stories' do
+    it 'should return five stories' do
       expect(Story.stories_filter('NDC, esp, climate watch', 5)).to have(5).items
     end
   end
