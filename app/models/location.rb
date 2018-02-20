49a8382530f6c83a0538892ef27ca5c9b6f66eed
Simao Belchior <simaobelchior@gmail.com>
2018-02-20 20:32:02 +0000

    reduce size of method, move query to Socioeconomic::Indicator model

--- app/models/location.rb
+++ app/models/location.rb
@@ -38,43 +38,8 @@ class Location < ApplicationRecord
   end
 
   def latest_socioeconomics
-    sql = <<-SQL
-      SELECT
-        soci_pop.year AS population_year, soci_pop.population AS population,
-        soci_pop.population_rank AS population_rank,
-
-        soci_gdp.year AS gdp_year, soci_gdp.gdp AS gdp,
-        soci_gdp.gdp_rank AS gdp_rank,
-
-        soci_pop_growth.year AS population_growth_year,
-        soci_pop_growth.population_growth AS population_growth,
-        soci_pop_growth.population_growth_rank AS population_growth_rank,
-
-        soci_gdp_per_capita.year AS gdp_per_capita_year,
-        soci_gdp_per_capita.gdp_per_capita AS gdp_per_capita,
-        soci_gdp_per_capita.gdp_per_capita_rank AS gdp_per_capita_rank
-
-      FROM socioeconomic_indicators soci_pop,
-           socioeconomic_indicators soci_gdp,
-           socioeconomic_indicators soci_pop_growth,
-           socioeconomic_indicators soci_gdp_per_capita
-
-      WHERE soci_pop.location_id = soci_gdp.location_id
-        AND soci_gdp.location_id = soci_pop_growth.location_id
-        AND soci_pop_growth.location_id = soci_gdp_per_capita.location_id
-        AND soci_pop.population <> 0 AND soci_pop.population IS NOT NULL
-        AND soci_gdp.gdp <> 0 AND soci_gdp.gdp IS NOT NULL
-        AND soci_pop_growth.population_growth <> 0
-        AND soci_pop_growth.population_growth IS NOT NULL
-        AND soci_gdp_per_capita.gdp_per_capita <> 0
-        AND soci_gdp_per_capita.gdp_per_capita IS NOT NULL
-        AND soci_pop.location_id = #{self.id}
-      ORDER BY soci_pop.year DESC, soci_gdp.year DESC,
-      soci_pop_growth.year DESC, soci_gdp_per_capita.year DESC
-      LIMIT 1
-    SQL
-
-    result = ActiveRecord::Base.connection.execute(sql).first
+    result = ActiveRecord::Base.connection.
+      execute(Socioeconomic::Indicator.latest_available_data_query(id)).first
 
     if !result
       return {}
