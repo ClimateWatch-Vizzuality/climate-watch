12d54e0be6eb2b46d8ff7a4185fa30274294ceed
Javi Abia <javier.abia@vizzuality.com>
2018-02-20 20:09:03 +0100

    Extract logic to container on circular chart

--- app/javascript/app/components/circular-chart/circular-chart-component.jsx
+++ app/javascript/app/components/circular-chart/circular-chart-component.jsx
@@ -3,29 +3,30 @@ import PropTypes from 'prop-types';
 import { COUNTRY_COMPARE_COLORS } from 'data/constants';
 import styles from './circular-chart-styles.scss';
 
-const radius = index => (100 + index * 20) / (3.14159 * 2); // eslint-disable-line no-mixed-operators
-const diameter = index => radius(index) * 2;
-const normalizedCircunference = index => 3.14159 * diameter(index);
-const arcLength = (value, index) =>
-  360 * value / 100 / 360 * normalizedCircunference(index); // eslint-disable-line no-mixed-operators
-
-const CircularChart = ({ value, index }) => (
+const CircularChart = ({
+  value,
+  index,
+  setRadius,
+  setArcLength,
+  normalizeCircunference
+}) => (
   <div className={styles.circularChartWrapper}>
     <svg viewBox="0 0 50 50" className={styles.circularChart}>
       <circle
-        r={radius(index)}
+        r={setRadius(index)}
         cx="25"
         cy="25"
         className={styles.circleEmpty}
       />
       <circle
-        r={radius(index)}
+        r={setRadius(index)}
         cx="25"
         cy="25"
         className={styles.circleValue}
-        strokeDasharray={`${arcLength(value, index)}, ${normalizedCircunference(
+        strokeDasharray={`${setArcLength(
+          value,
           index
-        )}`}
+        )}, ${normalizeCircunference(index)}`}
         style={{ stroke: COUNTRY_COMPARE_COLORS[index] }}
       />
     </svg>
@@ -34,7 +35,10 @@ const CircularChart = ({ value, index }) => (
 
 CircularChart.propTypes = {
   value: PropTypes.number.isRequired,
-  index: PropTypes.number.isRequired
+  index: PropTypes.number.isRequired,
+  setRadius: PropTypes.func.isRequired,
+  setArcLength: PropTypes.func.isRequired,
+  normalizeCircunference: PropTypes.func.isRequired
 };
 
 export default CircularChart;
