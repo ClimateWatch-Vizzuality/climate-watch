b299c3a514fe999c4327887bd5eb4813130b7ced
Javi Abia <javier.abia@vizzuality.com>
2018-02-21 11:46:36 +0100

    Remove country selector on mobile and make it sticky

--- app/javascript/app/pages/country-compare/country-compare-component.jsx
+++ app/javascript/app/pages/country-compare/country-compare-component.jsx
@@ -9,32 +9,41 @@ import CountryCompareSelector from 'components/country-compare-selector';
 
 import layout from 'styles/layout.scss';
 import anchorNavRegularTheme from 'styles/themes/anchor-nav/anchor-nav-regular.scss';
+import { TabletLandscape } from 'components/responsive';
 import styles from './country-compare-styles.scss';
 
 const CountryCompare = ({ route, anchorLinks }) => (
-  <div>
-    <Header route={route}>
-      <div className={layout.content}>
-        <Intro title={'Country Comparison'} />
+  <TabletLandscape>
+    {isLandscape => (
+      <div>
+        <Header route={route}>
+          <div className={layout.content}>
+            <Intro title={'Country Comparison'} />
+          </div>
+          <Sticky activeClass="sticky -country-compare" top="#navBarMobile">
+            <AnchorNav
+              links={anchorLinks}
+              className={layout.content}
+              theme={anchorNavRegularTheme}
+              gradientColor={route.headerColor}
+            />
+          </Sticky>
+        </Header>
+        {isLandscape ? (
+          <Sticky activeClass="sticky" top={64}>
+            <CountryCompareSelector className={styles.countrySelectors} />
+          </Sticky>
+        ) : null}
+        {route.sections &&
+          route.sections.length > 0 &&
+          route.sections.map(section => (
+            <section key={section.hash} id={section.hash}>
+              {!!section.component && <section.component />}
+            </section>
+          ))}
       </div>
-      <Sticky activeClass="sticky -country-compare" top="#navBarMobile">
-        <AnchorNav
-          links={anchorLinks}
-          className={layout.content}
-          theme={anchorNavRegularTheme}
-          gradientColor={route.headerColor}
-        />
-      </Sticky>
-    </Header>
-    <CountryCompareSelector className={styles.countrySelectors} />
-    {route.sections &&
-      route.sections.length > 0 &&
-      route.sections.map(section => (
-        <section key={section.hash} id={section.hash}>
-          {!!section.component && <section.component />}
-        </section>
-      ))}
-  </div>
+    )}
+  </TabletLandscape>
 );
 
 CountryCompare.propTypes = {
