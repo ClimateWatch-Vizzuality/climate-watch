b795db2afbdc9c6bca2a4e96c03444f12d76a304
Javi Abia <javier.abia@vizzuality.com>
2018-02-28 14:45:45 +0100

    Hide logos on about page

--- app/javascript/app/components/footer/footer-component.jsx
+++ app/javascript/app/components/footer/footer-component.jsx
@@ -11,32 +11,34 @@ import styles from './footer-styles.scss';
 class Footer extends PureComponent {
   // eslint-disable-line react/prefer-stateless-function
   render() {
-    const { partners } = this.props;
+    const { partners, location } = this.props;
     const className = cx(styles.footer, styles.border);
     return (
       <footer className={className}>
-        <div className={cx(layout.content, styles.nav)}>
-          <div className={styles.partnersContainer}>
-            {partners.map(
-              partner =>
-                partner.img && (
-                  <div key={partner.img.alt} className={styles.logoContainer}>
-                    <a
-                      className={
-                        styles[partner.img.customClass] || styles.defaultLogo
-                      }
-                      href={partner.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img src={partner.img.src} alt={partner.img.alt} />
-                    </a>
-                  </div>
-                )
-            )}
+        {!location.pathname.includes('/about') && (
+          <div className={cx(layout.content, styles.nav)}>
+            <div className={styles.partnersContainer}>
+              {partners.map(
+                partner =>
+                  partner.img && (
+                    <div key={partner.img.alt} className={styles.logoContainer}>
+                      <a
+                        className={
+                          styles[partner.img.customClass] || styles.defaultLogo
+                        }
+                        href={partner.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img src={partner.img.src} alt={partner.img.alt} />
+                      </a>
+                    </div>
+                  )
+              )}
+            </div>
+            <Contact />
           </div>
-          <Contact />
-        </div>
+        )}
         <BottomBar className={layout.content} />
       </footer>
     );
@@ -44,7 +46,8 @@ class Footer extends PureComponent {
 }
 
 Footer.propTypes = {
-  partners: PropTypes.array.isRequired
+  partners: PropTypes.array.isRequired,
+  location: PropTypes.object
 };
 
 export default Footer;
