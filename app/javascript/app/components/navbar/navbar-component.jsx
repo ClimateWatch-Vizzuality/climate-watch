d91b7d3808f5b6efb506fc259d4f729298fcece9
Javi Abia <javier.abia@vizzuality.com>
2018-03-06 17:44:25 +0100

    Add grid to navbar component

--- app/javascript/app/components/navbar/navbar-component.jsx
+++ app/javascript/app/components/navbar/navbar-component.jsx
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
 import Nav from 'components/nav';
 import ToolsNav from 'components/tools-nav';
 
@@ -10,8 +9,10 @@ class NavBar extends PureComponent {
   render() {
     const { routes } = this.props;
     return (
-      <div className={cx(styles.row, styles.navbar)}>
-        <Nav routes={routes} isRendered />
+      <div className={styles.row}>
+        <div className={styles.navigationWrapper}>
+          <Nav routes={routes} className={styles.navigation} isRendered />
+        </div>
         <ToolsNav />
       </div>
     );
