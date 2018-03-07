4db41db6c3b03dafd8c12aed50c0ab7909029397
Javi Abia <javier.abia@vizzuality.com>
2018-03-07 19:04:46 +0100

    Update header background handling

--- app/javascript/app/components/header/header-component.jsx
+++ app/javascript/app/components/header/header-component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import { TabletLandscape } from 'components/responsive';
 
 import styles from './header-styles.scss';
 
@@ -10,26 +11,29 @@ const Header = props => {
     [styles.medium]: size === 'medium',
     [styles.large]: size === 'large'
   });
-
-  let style = { backgroundColor: color };
-  if (image) {
-    style = {
-      ...style,
-      backgroundImage: `url(${image})`
-    };
-  }
-
-  const gradientStyle = color
-    ? { backgroundImage: `linear-gradient(to top, ${color} 25%, transparent)` }
-    : null;
+  const getStyle = isLandscape => {
+    let style = { backgroundColor: color };
+    if (image) {
+      const gradient = `linear-gradient(to top, ${color} 25%, transparent), `;
+      style = {
+        ...style,
+        backgroundImage: `${isLandscape ? '' : gradient}url(${image})`
+      };
+    }
+    return style;
+  };
 
   return (
-    <div className={cx(className, styles.header, sizeClass)} style={style}>
-      {gradientStyle && (
-        <span className={styles.gradient} style={gradientStyle} />
+    <TabletLandscape>
+      {isLandscape => (
+        <div
+          className={cx(className, styles.header, sizeClass)}
+          style={getStyle(isLandscape)}
+        >
+          {children}
+        </div>
       )}
-      {children}
-    </div>
+    </TabletLandscape>
   );
 };
 
