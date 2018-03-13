e764b597b2f1f69561d88c0a110cb41b901f2aaa
Javi Abia <javier.abia@vizzuality.com>
2018-03-13 16:37:00 +0100

    Add hex to rgba function

--- app/javascript/app/utils/utils.js
+++ app/javascript/app/utils/utils.js
@@ -61,6 +61,14 @@ export function sanitize(data) {
   return data;
 }
 
+export function hexToRgba(hex, opacity) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return `rgba(${r} ,${g} ,${b},${opacity})`;
+}
+
 export default {
   compareIndexByKey,
   deburrUpper,
