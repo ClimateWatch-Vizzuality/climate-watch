9b34ab067eeefcefbc3e97448620d829a70073bb
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-03-21 11:19:02 +0100

    Fix vertical label and add label for unit

--- app/javascript/app/components/my-climate-watch/viz-creator/components/charts/line/line-component.jsx
+++ app/javascript/app/components/my-climate-watch/viz-creator/components/charts/line/line-component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {
+  Label,
   LineChart,
   Line,
   XAxis,
@@ -9,6 +10,57 @@ import {
   CartesianGrid,
   ResponsiveContainer
 } from 'recharts';
+
+const CustomizedYAxisTick = ({ payload, unit, x, y }) => (
+  <g transform={`translate(${x},${y})`}>
+    <text
+      x="0"
+      y="0"
+      dy="0"
+      textAnchor="end"
+      stroke="#b1b1c1"
+      strokeWidth="0.5"
+      fontSize="13px"
+    >
+      {`${payload.value}${unit ? '' : 't'}`}
+    </text>
+  </g>
+);
+
+CustomizedYAxisTick.propTypes = {
+  x: PropTypes.number,
+  y: PropTypes.number,
+  payload: PropTypes.object,
+  unit: PropTypes.oneOfType([PropTypes.string, PropTypes.bool])
+};
+
+const yAxisUnitLabel = unit => (
+  <Label
+    position="top"
+    content={() => (
+      <text x="22" y="20">
+        {unit}
+      </text>
+    )}
+  />
+);
+
+const yAxisIndicatorLabel = (label, height) => (
+  <Label
+    position="left"
+    angle={-90}
+    content={() => (
+      <text
+        x={height * -0.5}
+        y="15"
+        textAnchor="middle"
+        transform="rotate(-90)"
+      >
+        {label}
+      </text>
+    )}
+  />
+);
 
 const ChartLine = ({ width, height, className, config }) => (
   <ResponsiveContainer className={className} width={width} height={height}>
@@ -20,14 +72,10 @@ const ChartLine = ({ width, height, className, config }) => (
         ))}
       {config.xAxis && <XAxis {...config.xAxis} />}
       {config.yAxis && (
-        <YAxis
-          label={{
-            value: config.yAxis.label,
-            angle: -90,
-            position: 'left',
-            offset: -20
-          }}
-        />
+        <YAxis tick={<CustomizedYAxisTick unit={config.yAxis.unit} />}>
+          {yAxisUnitLabel(config.yAxis.unit)}
+          {yAxisIndicatorLabel(config.yAxis.label, height)}
+        </YAxis>
       )}
     </LineChart>
   </ResponsiveContainer>
