2c615a9106a056446f6332ce920e9592487ea50a
Javi Abia <javier.abia@vizzuality.com>
2018-03-21 16:07:22 +0100

    Add payload to render values

--- app/javascript/app/components/my-climate-watch/viz-creator/components/charts/tooltip/tooltip-component.jsx
+++ app/javascript/app/components/my-climate-watch/viz-creator/components/charts/tooltip/tooltip-component.jsx
@@ -5,13 +5,13 @@ import { themr } from 'react-css-themr';
 
 import theme from 'styles/themes/chart-tooltip/chart-tooltip.scss';
 
-const Tooltip = ({ label, tooltip }) => (
+const Tooltip = ({ label, tooltip, payload }) => (
   <div className={theme.tooltip}>
     <div className={theme.tooltipHeader}>
       <span className={cx(theme.labelName, theme.labelNameBold)}>{label}</span>
       <span className={theme.unit}>{tooltip[0].unit}</span>
     </div>
-    {tooltip.map(l => (
+    {tooltip.map((l, i) => (
       <div className={theme.label} key={l.label}>
         <div className={theme.legend}>
           <span
@@ -20,7 +20,17 @@ const Tooltip = ({ label, tooltip }) => (
           />
           <p className={theme.labelName}>{l.label}</p>
         </div>
-        <p className={theme.labelValue}>{l.value}</p>
+        {l.value ? <p className={theme.labelValue}>{l.value}</p> : null}
+        {!l.value &&
+        payload.length &&
+        payload.length > 1 && (
+            <p className={theme.labelValue}>{payload[i].value}</p>
+          )}
+        {!l.value &&
+        payload.length &&
+        payload.length === 1 && (
+            <p className={theme.labelValue}>{payload[0].value}</p>
+          )}
       </div>
     ))}
   </div>
@@ -28,7 +38,8 @@ const Tooltip = ({ label, tooltip }) => (
 
 Tooltip.propTypes = {
   label: PropTypes.any,
-  tooltip: PropTypes.array
+  tooltip: PropTypes.array,
+  payload: PropTypes.array
 };
 
 export default themr('Tooltip', theme)(Tooltip);
