ec6b625060739dca7d9b7745780a5b16ae9af192
Javi Abia <javier.abia@vizzuality.com>
2018-03-22 10:51:08 +0100

    Sort tooltip values

--- app/javascript/app/components/my-climate-watch/viz-creator/components/charts/tooltip/tooltip-component.jsx
+++ app/javascript/app/components/my-climate-watch/viz-creator/components/charts/tooltip/tooltip-component.jsx
@@ -5,40 +5,45 @@ import { themr } from 'react-css-themr';
 
 import theme from 'styles/themes/chart-tooltip/chart-tooltip.scss';
 
+const sortByValue = payload => {
+  const compare = (a, b) => b.value - a.value;
+  return payload.sort(compare);
+};
+
 const Tooltip = ({ label, tooltip, payload }) => (
   <div className={theme.tooltip}>
     <div className={theme.tooltipHeader}>
       <span className={cx(theme.labelName, theme.labelNameBold)}>{label}</span>
-      <span className={theme.unit}>{tooltip[0].unit}</span>
+      <span className={theme.unit}>{tooltip.unit}</span>
     </div>
-    {tooltip.map((l, i) => (
-      <div className={theme.label} key={l.label}>
-        <div className={theme.legend}>
-          <span
-            className={theme.labelDot}
-            style={{ backgroundColor: l.color }}
-          />
-          <p className={theme.labelName}>{l.label}</p>
+    {payload &&
+      payload.length &&
+      sortByValue(payload).map(v => (
+        <div className={theme.label} key={v.name}>
+          <div className={theme.legend}>
+            <span
+              className={theme.labelDot}
+              style={
+                v.color ? (
+                  { backgroundColor: v.color }
+                ) : (
+                  { backgroundColor: v.payload.fill }
+                )
+              }
+            />
+            <p className={theme.labelName}>
+              {tooltip.pie ? v.name : tooltip.names[0][v.name]}
+            </p>
+          </div>
+          <p className={theme.labelValue}>{v.value}</p>
         </div>
-        {l.value ? <p className={theme.labelValue}>{l.value}</p> : null}
-        {!l.value &&
-        payload.length &&
-        payload.length > 1 && (
-            <p className={theme.labelValue}>{payload[i].value}</p>
-          )}
-        {!l.value &&
-        payload.length &&
-        payload.length === 1 && (
-            <p className={theme.labelValue}>{payload[0].value}</p>
-          )}
-      </div>
-    ))}
+      ))}
   </div>
 );
 
 Tooltip.propTypes = {
   label: PropTypes.any,
-  tooltip: PropTypes.array,
+  tooltip: PropTypes.object,
   payload: PropTypes.array
 };
 
