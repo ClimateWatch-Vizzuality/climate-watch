c3d6ffc0d22495f5b2f1f929d8e408ec19a85581
Javi Abia <javier.abia@vizzuality.com>
2018-04-03 10:08:29 +0200

    Add prevent default click behavior to allow tag removal

--- app/javascript/app/components/tag/tag-component.jsx
+++ app/javascript/app/components/tag/tag-component.jsx
@@ -7,6 +7,11 @@ import { Link } from 'react-router-dom';
 import closeIcon from 'assets/icons/legend-close.svg';
 import styles from './tag-styles.scss';
 
+const handleClick = (e, data, onRemove) => {
+  e.preventDefault();
+  onRemove(data);
+};
+
 class Tag extends PureComponent {
   render() {
     const { data, onRemove, className, canRemove } = this.props;
@@ -15,7 +20,10 @@ class Tag extends PureComponent {
         <span className={styles.dot} style={{ backgroundColor: data.color }} />
         <p className={styles.label}>{data.label}</p>
         {canRemove && (
-          <button className={styles.closeButton} onClick={() => onRemove(data)}>
+          <button
+            className={styles.closeButton}
+            onClick={e => handleClick(e, data, onRemove)}
+          >
             <Icon icon={closeIcon} className={styles.icon} />
           </button>
         )}
