34c97af5f56205ff27ee2474ec133d57b2b5b3f2
Javi Abia <javier.abia@vizzuality.com>
2018-05-16 12:59:50 +0200

    Force context update on connected anchor nav

--- app/javascript/app/components/anchor-nav/anchor-nav.js
+++ app/javascript/app/components/anchor-nav/anchor-nav.js
@@ -12,4 +12,6 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(Component);
+// explanation to pure: false prop on connect could be found here
+// https://github.com/ReactTraining/react-router/issues/3536#issuecomment-225586661
+export default connect(mapStateToProps, null, null, { pure: false })(Component);
