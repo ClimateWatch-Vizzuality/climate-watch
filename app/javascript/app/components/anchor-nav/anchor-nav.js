9ce5daf20ec3df6ef86819ba1940f2d7b1af1b57
Javi Abia <javier.abia@vizzuality.com>
2018-05-16 11:35:41 +0200

    Consume active section state from anchor nav

--- app/javascript/app/components/anchor-nav/anchor-nav.js
+++ app/javascript/app/components/anchor-nav/anchor-nav.js
@@ -1,3 +1,15 @@
+import { connect } from 'react-redux';
 import Component from './anchor-nav-component';
+import actions from './anchor-nav-actions';
+import reducers, { initialState } from './anchor-nav-reducers';
 
-export default Component;
+export { actions, reducers, initialState };
+
+const mapStateToProps = state => {
+  const { anchorNav } = state;
+  return {
+    activeSection: anchorNav.activeSection
+  };
+};
+
+export default connect(mapStateToProps)(Component);
