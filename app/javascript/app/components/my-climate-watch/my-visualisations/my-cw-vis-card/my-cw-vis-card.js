f068d189368ca3cde6dc3ac0bb267a338038c75a
Javi Abia <javier.abia@vizzuality.com>
2018-05-17 16:03:46 +0200

    Connect vis-card with store, add actions and add delete button

--- app/javascript/app/components/my-climate-watch/my-visualisations/my-cw-vis-card/my-cw-vis-card.js
+++ app/javascript/app/components/my-climate-watch/my-visualisations/my-cw-vis-card/my-cw-vis-card.js
@@ -1,1 +1,5 @@
-export { default } from './my-cw-vis-card-component';
+import { connect } from 'react-redux';
+import * as actions from 'components/my-climate-watch/viz-creator/viz-creator-actions';
+import Component from './my-cw-vis-card-component';
+
+export default connect(null, actions)(Component);
