5c409851fa17cfde299bcaa7be6cf7e2106adf3d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-05-22 13:31:07 +0100

    fixup API

--- app/serializers/api/v1/data/historical_emissions/record_serializer.rb
+++ app/serializers/api/v1/data/historical_emissions/record_serializer.rb
@@ -3,14 +3,14 @@ module Api
     module Data
       module HistoricalEmissions
         class RecordSerializer < ActiveModel::Serializer
-          attribute :region { object['region'] }
-          attribute :iso_code3 { object['iso_code3'] }
-          attribute :gas { object['gas'] }
-          attribute :data_source { object['data_source'] }
-          attribute :sector { object['sector'] }
-          attribute :gwp { object['gwp'] }
-          attribute :unit { 'C02e' }
-          attribute :emissions
+          attribute(:region) { object['region'] }
+          attribute(:iso_code3) { object['iso_code3'] }
+          attribute(:gas) { object['gas'] }
+          attribute(:data_source) { object['data_source'] }
+          attribute(:sector) { object['sector'] }
+          attribute(:gwp) { object['gwp'] }
+          attribute(:unit) { 'C02e' }
+          attribute(:emissions)
         end
       end
     end
