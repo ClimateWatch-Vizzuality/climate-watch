7809ccadeb3af882e77c9778b6277e03e6e18b74
Jorge García <jorgegar@gmail.com>
2018-06-07 09:50:13 +0200

    allows to pass a className to the RenderChart component

--- app/javascript/app/components/my-climate-watch/viz-creator/components/render-chart/render-chart-component.jsx
+++ app/javascript/app/components/my-climate-watch/viz-creator/components/render-chart/render-chart-component.jsx
@@ -1,13 +1,14 @@
 import React, { PureComponent, createElement } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import charts from '../charts';
 import styles from './render-chart-styles';
 
 class RenderChart extends PureComponent {
   render() {
-    const { chart, ...props } = this.props;
+    const { chart, className, ...props } = this.props;
     return (
-      <div className={styles.container}>
+      <div className={cx(className, styles.container)}>
         {createElement(charts[chart], props)}
       </div>
     );
@@ -15,7 +16,8 @@ class RenderChart extends PureComponent {
 }
 
 RenderChart.propTypes = {
-  chart: PropTypes.string
+  chart: PropTypes.string,
+  className: PropTypes.string
 };
 
 export default RenderChart;
