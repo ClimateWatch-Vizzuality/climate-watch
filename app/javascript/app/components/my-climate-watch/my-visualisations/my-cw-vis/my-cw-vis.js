48eb0347d60b086bfce7e14c27bb49abc5101e9a
Jorge García <jorgegar@gmail.com>
2018-06-07 13:14:10 +0200

    remove unneeded component

--- app/javascript/app/components/my-climate-watch/my-visualisations/my-cw-vis/my-cw-vis.js
+++
@@ -1,5 +0,0 @@
-import { connect } from 'react-redux';
-import * as actions from 'components/my-climate-watch/viz-creator/viz-creator-actions';
-import Component from './my-cw-vis-component';
-
-export default connect(null, actions)(Component);
