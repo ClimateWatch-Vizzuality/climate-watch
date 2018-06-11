e08fc06bd07fb3316507793df197b25bc22aced8
Javi Abia <javier.abia@vizzuality.com>
2018-06-11 18:31:32 +0200

    Add temporal patch and props to my-account

--- app/javascript/app/components/my-climate-watch/my-account/my-account.js
+++ app/javascript/app/components/my-climate-watch/my-account/my-account.js
@@ -1,9 +1,17 @@
 import { connect } from 'react-redux';
 
+import { CWAPI } from 'services/api';
+
 import MyAccountComponent from './my-account-component';
 
+const updateUser = (id, payload) => {
+  CWAPI.patch(`my_cw/users/${id}`, payload);
+};
+
 const mapStateToProps = ({ login }) => ({
-  user: login.user
+  user: login.user,
+  id: login.user.id,
+  updateUser
 });
 
 export default connect(mapStateToProps, null)(MyAccountComponent);
