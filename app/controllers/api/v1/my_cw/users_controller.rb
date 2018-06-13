af4f820e91c1cd6a7979dee0bf523868953717a0
Javi Abia <javier.abia@vizzuality.com>
2018-06-13 15:51:07 +0200

    Change update method to update_attributes

--- app/controllers/api/v1/my_cw/users_controller.rb
+++ app/controllers/api/v1/my_cw/users_controller.rb
@@ -18,17 +18,19 @@ module Api
 
         def update
           user = ::MyCw::User.find params[:id]
-          if user.update(user_params)
+          if user.update_attributes!(user_params)
             render json: user, serializer: Api::V1::MyCw::UserSerializer
           else
             resource_error(user)
           end
         end
-
         private
 
         def user_params
-          params.require(:user).permit(:ct_id, :first_name, :last_name, :organization, :sector, :country, :tester, :data_usage)
+          params.require(:user).
+            permit(:ct_id, :first_name, :last_name,
+                   :organization, :sector, :country,
+                   :tester, :data_usage)
         end
       end
     end
