33f1855c38d522a5b71a652452fc70a4da3bebd0
Javi Abia <javier.abia@vizzuality.com>
2018-06-13 15:19:10 +0200

    Add selectors and new data to my account component

--- app/javascript/app/components/my-climate-watch/my-account/my-account.js
+++ app/javascript/app/components/my-climate-watch/my-account/my-account.js
@@ -1,13 +1,37 @@
 import { connect } from 'react-redux';
 import * as actions from 'providers/login-provider/login-provider-actions';
+import { USERS_PROFESIONAL_SECTORS as sectors } from 'data/constants';
 import Component from './my-account-component';
 
-const mapStateToProps = ({ login }) => ({
-  email: login.user.email,
-  id: login.user.user_id.id,
-  name: login.user.user_id.name,
-  organization: login.user.user_id.organization,
-  areaOfWork: login.user.user_id.areaOfWork
-});
+import {
+  countriesSelector,
+  sectorsSelector,
+  userSectorSelector,
+  userCountrySelector
+} from './my-account-selectors';
+
+const mapStateToProps = ({ login, countries }) => {
+  const myAccountState = {
+    countries,
+    sectors,
+    userSector: login.user.user_id.sector,
+    userCountry: login.user.user_id.country
+  };
+
+  return {
+    isProfileUpdated: login.profileUpdated,
+    email: login.user.email,
+    id: login.user.user_id.id,
+    tester: login.user.user_id.tester,
+    dataUsage: login.user.user_id.data_usage,
+    firstName: login.user.user_id.first_name,
+    lastName: login.user.user_id.last_name,
+    organization: login.user.user_id.organization,
+    sector: userSectorSelector(myAccountState),
+    country: userCountrySelector(myAccountState),
+    countries: countriesSelector(myAccountState),
+    sectors: sectorsSelector(myAccountState)
+  };
+};
 
 export default connect(mapStateToProps, actions)(Component);
