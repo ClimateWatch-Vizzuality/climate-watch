58024287040ed1cbcac4a5b38e1ce599bfc6f41a
Javi Abia <javier.abia@vizzuality.com>
2018-06-13 16:31:10 +0200

    Remove debounce to avoid input weird behaviour

--- app/javascript/app/components/text-input/text-input-component.jsx
+++ app/javascript/app/components/text-input/text-input-component.jsx
@@ -2,36 +2,10 @@ import React, { Component } from 'react';
 import { themr } from 'react-css-themr';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import debounce from 'lodash/debounce';
 
 import styles from './text-input-styles';
 
 class InputComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.value
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.value !== this.props.value) {
-      this.setState({ value: nextProps.value });
-    }
-  }
-
-  handleChange = value => {
-    this.setState({ value });
-    this.debouncedChange();
-  };
-
-  debouncedChange = debounce(() => {
-    const { onChange } = this.props;
-    if (onChange) {
-      this.props.onChange(this.state.value);
-    }
-  }, 0);
-
   render() {
     const {
       theme,
@@ -39,6 +13,7 @@ class InputComponent extends Component {
       failed,
       disabled,
       value,
+      onChange,
       placeholder,
       label,
       inputType
@@ -52,22 +27,24 @@ class InputComponent extends Component {
         [theme.disabled]: disabled,
         [theme.inputFailed]: failed
       }),
-      onChange: e => this.handleChange(e.target.value),
+      onChange: e => onChange(e.target.value),
       value: value || '',
       disabled,
       placeholder
     };
     const labelProp = label ? { id: label } : {};
 
-    const input = inputType === 'textarea' ? (
-      <textarea {...inputProps} {...labelProp} />
-    ) : (
-      <input {...inputProps} {...labelProp} />
-    );
+    const input =
+      inputType === 'textarea' ? (
+        <textarea {...inputProps} {...labelProp} />
+      ) : (
+        <input {...inputProps} {...labelProp} />
+      );
 
     return (
       <div>
         {label && (
+          // eslint-disable-next-line jsx-a11y/label-has-for
           <label htmlFor={label} className={theme.label}>
             {label}
           </label>
