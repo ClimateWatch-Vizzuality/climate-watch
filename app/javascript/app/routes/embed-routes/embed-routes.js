ea05b90ebed5eb42b0772398ff4d302cfc466ea3
Javi Abia <javier.abia@vizzuality.com>
2018-06-15 10:40:56 +0200

    Add embeded routes

--- app/javascript/app/routes/embed-routes/embed-routes.js
+++ app/javascript/app/routes/embed-routes/embed-routes.js
@@ -5,6 +5,8 @@ import NDCMap from 'components/ndcs/ndcs-map';
 import GhgEmissionsGraph from 'components/ghg-emissions';
 import CompareGhgChart from 'components/compare/compare-ghg-chart';
 import CountryGhg from 'components/country/country-ghg';
+import CountryNdcOverview from 'components/country/country-ndc-overview';
+import NDCSDGLinkages from 'components/country/country-ndc-sdg-linkages';
 import NdcSdgLinkagesContent from 'components/ndc-sdg/ndc-sdg-linkages-content';
 import EmissionPathwaysGraph from 'components/emission-pathways/emission-pathways-graph';
 import MyVisualisationsGraphComponent from 'components/my-climate-watch/my-visualisations/my-cw-vis-graph';
@@ -28,6 +30,16 @@ export default [
   {
     path: '/embed/countries/:iso/ghg-emissions',
     component: CountryGhg,
+    exact: true
+  },
+  {
+    path: '/embed/countries/:iso/ndc-content-overview',
+    component: CountryNdcOverview,
+    exact: true
+  },
+  {
+    path: '/embed/countries/:iso/ndc-sdg-linkages',
+    component: NDCSDGLinkages,
     exact: true
   },
   {
