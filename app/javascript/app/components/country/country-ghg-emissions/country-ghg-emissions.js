d2a3cfe87d9b3558fa75d1dff605d00d46fb952a
Javi Abia <javier.abia@vizzuality.com>
2018-06-15 11:05:23 +0200

    Update isEmbed to use navigation utils

--- app/javascript/app/components/country/country-ghg-emissions/country-ghg-emissions.js
+++ app/javascript/app/components/country/country-ghg-emissions/country-ghg-emissions.js
@@ -2,7 +2,11 @@ import { createElement, PureComponent } from 'react';
 import Proptypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { getLocationParamUpdated, isPageContained } from 'utils/navigation';
+import {
+  getLocationParamUpdated,
+  isPageContained,
+  isEmbededComponent
+} from 'utils/navigation';
 import qs from 'query-string';
 import ReactGA from 'react-ga';
 
@@ -33,7 +37,7 @@ const mapStateToProps = (state, { location, match }) => {
   const { data, quantifications } = state.countryGhgEmissions;
   const calculationData = state.wbCountryData.data;
   const { meta } = state.ghgEmissionsMeta;
-  const isEmbed = location.pathname.includes('/embed');
+  const isEmbed = isEmbededComponent(location);
   const search = qs.parse(location.search);
   const iso = match.params.iso;
   const countryGhg = {
