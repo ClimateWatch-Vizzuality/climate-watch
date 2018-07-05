d670ff76300c74282093e020caa2ec901e8872c5
Javi Abia <javier.abia@vizzuality.com>
2018-07-05 17:17:20 +0200

    Create check input component

---
+++ app/javascript/app/components/check-input/check-input-component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { themr } from 'react-css-themr';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import styles from './check-input-styles';
+
+const CheckInputComponent = props => {
+  const { className, label, checked, onChange, theme, toggleFirst } = props;
+  return (
+    <div className={className}>
+      <label
+        className={cx(theme.switch, { [styles.toggleFirst]: toggleFirst })}
+        htmlFor="checkbox"
+      >
+        <input
+          className={theme.checkbox}
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+          id="checkbox"
+        />
+        <span className={theme.label}>{label}</span>
+        <div className={cx(theme.slider, theme.round)} />
+      </label>
+    </div>
+  );
+};
+
+CheckInputComponent.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.string,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func,
+  toggleFirst: PropTypes.bool,
+  theme: PropTypes.object
+};
+
+export default themr('CheckInputComponent', styles)(CheckInputComponent);
