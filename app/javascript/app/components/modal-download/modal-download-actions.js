0a945db928c7c5bde63c62bd5f859e0f761c9c08
Javi Abia <javier.abia@vizzuality.com>
2018-07-07 00:16:14 +0200

    Create saveSurvey thunk

--- app/javascript/app/components/modal-download/modal-download-actions.js
+++ app/javascript/app/components/modal-download/modal-download-actions.js
@@ -1,9 +1,26 @@
 import { createAction } from 'redux-actions';
+import { createThunkAction } from 'utils/redux';
+import { USER_SURVEY_SPREADSHEET_URL } from 'data/constants';
 
 const setModalDownloadParams = createAction('setModalDownloadParams');
+const setRequiredFieldsError = createAction('setRequiredFieldsError');
 const toggleModalDownload = createAction('toggleModalDownload');
+
+const saveSurveyData = createThunkAction(
+  'saveSurveyData',
+  requestParams => (dispatch, getState) => {
+    const { modalDownload } = getState();
+    if (!modalDownload.requiredError) {
+      fetch(
+        `${USER_SURVEY_SPREADSHEET_URL}?${requestParams.join('&')}`
+      ).then(() => window.location.assign(modalDownload.downloadUrl));
+    }
+  }
+);
 
 export default {
   setModalDownloadParams,
-  toggleModalDownload
+  setRequiredFieldsError,
+  toggleModalDownload,
+  saveSurveyData
 };
