e24d0a66e8c1168ac7375256b26df5f19eb1c64b
Bluesmile82 <pursuance@gmail.com>
2018-07-10 11:21:03 +0200

    Remove tabindex attribute from modal

--- app/javascript/app/components/modal/modal.js
+++ app/javascript/app/components/modal/modal.js
@@ -1,6 +1,20 @@
+import { createElement, PureComponent } from 'react';
 import ModalHeader from './modal-header-component';
 import ModalComponent from './modal-component';
 
 export { ModalHeader, ModalComponent };
 
-export default ModalComponent;
+class ModalContainer extends PureComponent {
+  componentDidUpdate() {
+    const modal = document.getElementsByClassName('modal')[0];
+    if (modal && modal.hasAttribute('tabindex')) { modal.removeAttribute('tabindex'); }
+  }
+
+  render() {
+    return createElement(ModalComponent, {
+      ...this.props
+    });
+  }
+}
+
+export default ModalContainer;
