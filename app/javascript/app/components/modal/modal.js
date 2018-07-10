9d6584405a4074c36ee6d8966b717d0ec023f89c
Bluesmile82 <pursuance@gmail.com>
2018-07-10 12:09:29 +0200

    Added TODO comment

--- app/javascript/app/components/modal/modal.js
+++ app/javascript/app/components/modal/modal.js
@@ -5,9 +5,12 @@ import ModalComponent from './modal-component';
 export { ModalHeader, ModalComponent };
 
 class ModalContainer extends PureComponent {
+  // TODO: This is obviously not the best solution. We have to remove the tabindex from the modal as its preventing the scrollbar from the dropdowns inside it to work
   componentDidUpdate() {
     const modal = document.getElementsByClassName('modal')[0];
-    if (modal && modal.hasAttribute('tabindex')) { modal.removeAttribute('tabindex'); }
+    if (modal && modal.hasAttribute('tabindex')) {
+      modal.removeAttribute('tabindex');
+    }
   }
 
   render() {
