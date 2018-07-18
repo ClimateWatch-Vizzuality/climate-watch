33d75e2c7041d605756da4b734999cd5a47ea86f
Tiago Santos <santos.tiago@gmail.com>
2018-07-18 13:08:48 +0100

    First version of the the streamable concern.

---
+++ app/controllers/concerns/streamable.rb
@@ -0,0 +1,20 @@
+module Streamable
+  extend ActiveSupport::Concern
+
+  def create_headers(filename)
+    headers.delete('Content-Length')
+    headers['Cache-Control'] = 'no-cache'
+    headers['Content-Type'] = 'text/csv'
+    headers['Content-Disposition'] = "attachment; filename=\"#{filename}.csv\""
+    headers['X-Accel-Buffering'] = 'no'
+  end
+
+  def stream_file(filename)
+    create_headers filename
+    self.response_body =
+        Enumerator.new do |y|
+          yield (y)
+        end
+    response.status = 200
+  end
+end
