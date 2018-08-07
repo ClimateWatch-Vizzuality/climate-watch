9d78eef217f56c87871b8aa6f2cdc54d98d8de88
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-08-07 16:21:09 +0100

    Speed up migration which creates a materialized views for historical emissions

--- db/migrate/20180803123629_create_historical_emissions_searchable_records.rb
+++ db/migrate/20180803123629_create_historical_emissions_searchable_records.rb
@@ -1,17 +1,6 @@
 class CreateHistoricalEmissionsSearchableRecords < ActiveRecord::Migration[5.1]
-  def up
-    execute 'REFRESH MATERIALIZED VIEW historical_emissions_normalised_records'
-    create_view :historical_emissions_searchable_records, materialized: true
-    add_index :historical_emissions_searchable_records, [:data_source_id]
-    add_index :historical_emissions_searchable_records, [:gwp_id]
-    add_index :historical_emissions_searchable_records, [:location_id]
-    add_index :historical_emissions_searchable_records, [:sector_id]
-    add_index :historical_emissions_searchable_records, [:gas_id]
-    add_index :historical_emissions_searchable_records, [:id], unique: true
-    execute 'CREATE INDEX index_searchable_emissions_path_ops ON historical_emissions_searchable_records USING GIN (emissions_dict jsonb_path_ops)'
-  end
-
-  def down
-    drop_view :historical_emissions_searchable_records, materialized: true
+  def change
+    # this was taking too long, so it's now a noop
+    # file kept in order not to affect rails migrations tracking
   end
 end
