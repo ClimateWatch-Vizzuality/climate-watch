db3cfd82904eaae93a987fb86b75b836c04ed462
Javi Abia <javier.abia@vizzuality.com>
2018-08-22 11:40:54 +0200

    Add MODULES_TO_DATA_EXPLORER_PARAMS constant

--- app/javascript/app/data/data-explorer-constants.js
+++ app/javascript/app/data/data-explorer-constants.js
@@ -81,6 +81,14 @@ export const DATA_EXPLORER_DEPENDENCIES = {
 };
 
 export const DATA_EXPLORER_EXTERNAL_PREFIX = 'external';
+export const MODULES_TO_DATA_EXPLORER_PARAMS = {
+  'historical-emissions': {
+    filter: 'filter',
+    source: 'data-sources',
+    version: 'gwps',
+    breakBy: 'breakBy'
+  }
+};
 export const DATA_EXPLORER_TO_MODULES_PARAMS = {
   'historical-emissions': {
     data_sources: { key: 'source' },
