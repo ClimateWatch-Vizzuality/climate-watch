df4db2660e586dd1d2ff45a1dfcc70b13379324d
Simao Belchior <simaobelchior@gmail.com>
2018-09-07 14:29:06 +0200

    Enable ndc_die metadata source. requires rails wri_metadata:import

--- app/javascript/app/components/country/country-ndc-overview/country-ndc-overview.js
+++ app/javascript/app/components/country/country-ndc-overview/country-ndc-overview.js
@@ -37,7 +37,7 @@ class CountryNdcOverviewContainer extends PureComponent {
   handleInfoClick = () => {
     this.props.setModalMetadata({
       category: 'Country',
-      slugs: ['ndc_cait', 'ndc_wb'],
+      slugs: ['ndc_cait', 'ndc_wb', 'ndc_die'],
       customTitle: 'Nationally Determined Contribution (NDC) Overview',
       open: true
     });
