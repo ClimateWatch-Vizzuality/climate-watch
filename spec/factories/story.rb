703979789bc6af4e43e3345139ccf097ad612efe
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-09-12 14:28:22 +0100

    Fixed syntax in factories

--- spec/factories/story.rb
+++ spec/factories/story.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :story do
-    sequence :title { |n| "#{n} Stories about climate and love" }
+    sequence(:title) { |n| "#{n} Stories about climate and love" }
     link { 'http://my.dreams.com' }
     background_image_url { 'http://pictures.of.me/1.png' }
     published_at { Time.now }
