147824bc43b64cf312b6e0b913c40dd2dcb1fcb8
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-09-25 16:59:49 +0100

    Satisfy rubocop

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,6 @@ class Document < ApplicationRecord
   has_many_attached :uploads
 
   scope :global_country_platform, -> { where(platform_name: 'global_country_platform') }
-  scope :indonesia_country_platform, -> { where(platform_name: 'indonesia_country_platform' ) }
-  scope :india_country_platform, -> { where(platform_name: 'india_country_platform' ) }
+  scope :indonesia_country_platform, -> { where(platform_name: 'indonesia_country_platform') }
+  scope :india_country_platform, -> { where(platform_name: 'india_country_platform') }
 end
