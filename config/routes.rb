98f1ce96719d892678f75cff2a0e28eb816f9e8e
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-10-01 17:51:03 +0100

    Add dashboard for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
+  authenticate :admin_user do
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   namespace :api do
     namespace :v1 do
       namespace :locations do
