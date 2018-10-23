117fbd86c3fd281bc06045209909805cfbc5645f
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-10-23 14:59:33 +0100

    Abstract slide cards, remove selectors from slide cards, modify styles

--- app/javascript/app/components/slide-cards/slide-cards.js
+++ app/javascript/app/components/slide-cards/slide-cards.js
@@ -1,8 +1,3 @@
-import { connect } from 'react-redux';
-
 import Component from './slide-cards-component';
-import { getCards } from './slide-cards-selectors';
 
-const mapStateToProps = getCards;
-
-export default connect(mapStateToProps, null)(Component);
+export default Component;
