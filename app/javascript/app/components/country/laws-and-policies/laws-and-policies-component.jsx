e49f12312bc18864268e7de41cbe6b6f5ead1932
Simao Belchior <simaobelchior@gmail.com>
2018-10-26 16:48:32 +0100

    Update description of laws and policies module

--- app/javascript/app/components/country/laws-and-policies/laws-and-policies-component.jsx
+++ app/javascript/app/components/country/laws-and-policies/laws-and-policies-component.jsx
@@ -55,14 +55,9 @@ class LawsAndPolicies extends PureComponent {
         <h3 className={styles.title}>Laws and Policies</h3>
         <div className={styles.descriptionContainer}>
           <div className="grid-column-item">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus
-            hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque,
-            aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula
-            sollicitudin laoreet viverra, tortor libero sodales leo, eget
-            blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse
-            potenti. Sed egestas, ante et vulputate volutpat, eros pede semper
-            est, vitae luctus metus libero eu augue. Morbi purus libero,
-            faucibus adipiscing, commodo quis, gravida id.
+            This table compares quantified targets in countries’ NDCs with those
+            in their national laws and policies. The purpose is to indicate the
+            level of alignment.
           </div>
           <div className={styles.logoContainer}>
             {
