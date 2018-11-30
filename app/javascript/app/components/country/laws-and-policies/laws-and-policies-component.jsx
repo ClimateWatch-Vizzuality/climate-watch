cb34ff2a9c963cf21cb6857f68e16dd615f13367
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-11-30 12:37:50 +0000

    Improve laws and policies design

--- app/javascript/app/components/country/laws-and-policies/laws-and-policies-component.jsx
+++ app/javascript/app/components/country/laws-and-policies/laws-and-policies-component.jsx
@@ -11,6 +11,7 @@ import Card from 'components/card';
 import CardRow from 'components/card/card-row';
 import LawsAndPoliciesProvider from 'providers/laws-and-policies-provider';
 import cx from 'classnames';
+import { Desktop, TabletLandscapeOnly } from 'components/responsive';
 
 import styles from './laws-and-policies-styles.scss';
 
@@ -59,12 +60,42 @@ class LawsAndPolicies extends PureComponent {
 
     return (
       <div className={layout.content}>
-        <h3 className={styles.title}>Targets in Laws and Policies</h3>
         <div className={styles.descriptionContainer}>
           <div className="grid-column-item">
-            This table compares quantified targets in countries’ submitted NDCs
-            with targets in relevant national laws and policies. The purpose is
-            to indicate the level of alignment.
+            <h3 className={styles.title}>Targets in Laws and Policies</h3>
+            <div>
+              This table compares quantified targets in countries’ submitted
+              NDCs with targets in relevant national laws and policies. The
+              purpose is to indicate the level of alignment.
+            </div>
+            <Desktop>
+              <div className={styles.actions}>
+                <Dropdown
+                  wrapperClassName={styles.dropdownWrapper}
+                  className={styles.dropdown}
+                  label="Filter by sector"
+                  options={sectors}
+                  onValueChange={this.handleSourceChange}
+                  value={currentSector}
+                  disclaimer={`${nationalPoliciesCount} ${nationalPoliciesCount ===
+                  1
+                    ? 'national law and policy'
+                    : 'national laws and policies'} with targets available for ${countryName} for the selected sector`}
+                  hideResetButton
+                />
+                <div className={styles.buttonContainer}>
+                  <ButtonGroup
+                    className={styles.buttonGroup}
+                    buttonsConfig={[
+                      {
+                        type: 'info',
+                        onClick: this.handleInfoOnClick
+                      }
+                    ]}
+                  />
+                </div>
+              </div>
+            </Desktop>
           </div>
           <div className={styles.logoContainer}>
             {
@@ -81,30 +112,34 @@ class LawsAndPolicies extends PureComponent {
               >{`See all ${lawsAndPoliciesCount} national laws and policies in ${countryName} on Climate Change Laws of the World.`}</span>
             </a>
           </div>
-        </div>
-        <div className={styles.actions}>
-          <Dropdown
-            className={styles.dropdown}
-            label="Filter by sector"
-            options={sectors}
-            onValueChange={this.handleSourceChange}
-            value={currentSector}
-            disclaimer={`${nationalPoliciesCount} ${nationalPoliciesCount === 1
-              ? 'national law and policy'
-              : 'national laws and policies'} with targets available for ${countryName} for the selected sector`}
-            hideResetButton
-          />
-          <div className={styles.buttonContainer}>
-            <ButtonGroup
-              className={styles.buttonGroup}
-              buttonsConfig={[
-                {
-                  type: 'info',
-                  onClick: this.handleInfoOnClick
-                }
-              ]}
-            />
-          </div>
+          <TabletLandscapeOnly>
+            <div className={styles.actions}>
+              <Dropdown
+                wrapperClassName={styles.dropdownWrapper}
+                className={styles.dropdown}
+                label="Filter by sector"
+                options={sectors}
+                onValueChange={this.handleSourceChange}
+                value={currentSector}
+                disclaimer={`${nationalPoliciesCount} ${nationalPoliciesCount ===
+                1
+                  ? 'national law and policy'
+                  : 'national laws and policies'} with targets available for ${countryName} for the selected sector`}
+                hideResetButton
+              />
+              <div className={styles.buttonContainer}>
+                <ButtonGroup
+                  className={styles.buttonGroup}
+                  buttonsConfig={[
+                    {
+                      type: 'info',
+                      onClick: this.handleInfoOnClick
+                    }
+                  ]}
+                />
+              </div>
+            </div>
+          </TabletLandscapeOnly>
         </div>
         <div className={styles.cardsContainer}>
           {ndcContentPresent ? (
