4d09bb67e15931a351381338fa179087babd6a8c
Javi Abia <javier.abia@vizzuality.com>
2018-12-13 18:41:58 +0100

    Add carousel component

--- app/javascript/app/pages/home/home-component.jsx
+++ app/javascript/app/pages/home/home-component.jsx
@@ -13,18 +13,10 @@ import cx from 'classnames';
 import GeoLocationProvider from 'providers/geolocation-provider';
 import { MobileOnly, TabletLandscape } from 'components/responsive';
 
+import CarouselSection from './carousel-section';
+
 import cwLogo from 'assets/icons/cw-logo-white.svg';
 import background from 'assets/headers/home.jpg';
-import countryBgScreenshot from 'assets/screenshots/country-with-quantifications@1x.png';
-import countrySmScreenshot from 'assets/screenshots/country-mobile-screenshot@2x.png';
-import ndcBgScreenshot from 'assets/screenshots/ndc-explore-bg-screenshot';
-import ndcSmScreenshot from 'assets/screenshots/ndc-explore-sm-screenshot@2x.png';
-import ndcSdgBgScreenshot from 'assets/screenshots/ndc-sdg-bg-screenshot';
-import ndcSdgSmScreenshot from 'assets/screenshots/ndc-sdg-sm-screenshot@2x.png';
-import ghgBgScreenshot from 'assets/screenshots/ghg-screenshot@1x.png';
-import ghgMobileScreenshot from 'assets/screenshots/ghg-mobile-screenshot@2x.png';
-import pathwaysBgScreenshot from 'assets/screenshots/pathways-screenshot@2x.png';
-import pathwaysMobileScreenshot from 'assets/screenshots/pathways-mobile-screenshot@2x.png';
 import theme from 'styles/themes/dropdown/dropdown-links.scss';
 import screenfull from 'screenfull';
 
@@ -58,10 +50,10 @@ class Home extends PureComponent {
             />
           </div>
         </Section>
-        <div className={cx(layout.content, styles.section)}>
-          <Stories />
+        <div >
+          <CarouselSection />
         </div>
-        <Section className={cx(styles.section, styles.countries)}>
+        {/* <Section className={cx(styles.section, styles.countries)}>
           <MobileOnly>
             {matches => (
               <div
@@ -118,162 +110,7 @@ class Home extends PureComponent {
               />
             </div>
           </div>
-        </Section>
-        <Section
-          className={cx(styles.section, styles.extraPadding, styles.ndcs)}
-        >
-          <div className={styles.column}>
-            <Intro
-              theme={introDark}
-              title="Explore and Compare Nationally Determined Contributions"
-              description="169 nationally determined contributions (NDCs) representing 192 countries and the European Union have been submitted to the UN Framework Convention on Climate Change.<br />Analyze and compare NDCs of all countries using 150 structured indicators and search the full text for key terms."
-            />
-            <div className={styles.doubleFold}>
-              <Button color="yellow" link="/ndcs-content">
-                Explore NDC content
-              </Button>
-              <TabletLandscape>
-                <Button color="plain" link="/ndcs/compare">
-                  Compare NDCs
-                </Button>
-              </TabletLandscape>
-            </div>
-          </div>
-          <MobileOnly>
-            {matches => (
-              <div className={matches ? styles.ndcImageMobile : styles.column}>
-                <div className={styles.imgContainer}>
-                  <img
-                    className={matches ? '' : styles.imageRight}
-                    src={matches ? ndcSmScreenshot : ndcBgScreenshot}
-                    alt="Ndcs section screenshot"
-                  />
-                </div>
-              </div>
-            )}
-          </MobileOnly>
-        </Section>
-        <Section
-          className={cx(
-            styles.section,
-            styles.extraPadding,
-            styles.sdgLinkages
-          )}
-        >
-          <MobileOnly>
-            {matches => (
-              <div
-                className={cx(
-                  styles.column,
-                  styles.invertOrder,
-                  layout.screenshotMobileLayout
-                )}
-              >
-                <div className={styles.imgLayout}>
-                  <div className={styles.imgContainer}>
-                    <img
-                      className={matches ? '' : styles.imageRight}
-                      src={matches ? ndcSdgSmScreenshot : ndcSdgBgScreenshot}
-                      alt="NDC SDGs screenshot"
-                    />
-                  </div>
-                </div>
-              </div>
-            )}
-          </MobileOnly>
-          <div className={styles.column}>
-            <Intro
-              theme={introDark}
-              title="Examine Links Between Sustainable Development and Climate Goals"
-              description="Our research shows that climate actions in NDCs align with at least 154 of the 169 targets of the Sustainable Development Goals (SDGs), demonstrating the enormous potential for linking climate and sustainable development efforts.<br />Identify potential alignment between the targets, actions, policy measures and needs in countries’ NDCs and the targets of the SDGs."
-            />
-            <div className={styles.doubleFold}>
-              <Button color="yellow" link={'/ndcs-sdg'}>
-                Explore NDC-SDG Linkages
-              </Button>
-            </div>
-          </div>
-        </Section>
-        <Section
-          className={cx(styles.section, styles.extraPadding, styles.ghg)}
-        >
-          <div className={styles.column}>
-            <Intro
-              theme={introDark}
-              title="Explore Historical Greenhouse Gas Emissions"
-              description="The 10 countries that emit the most greenhouse gases account for over 60% of global emissions, while the 100 least emitters contributed less than 3%.<br />Explore and download over 160 years of greenhouse gas emissions data from 196 countries and the EU, across seven sectors. Our inventory includes datasets sourced from independent research organizations and country self-reported inventories to the UNFCCC."
-            />
-            <div className={styles.doubleFold}>
-              <Button color="yellow" link="/ghg-emissions">
-                Explore Historical GHG Emissions
-              </Button>
-            </div>
-          </div>
-          <MobileOnly>
-            {matches => (
-              <div
-                className={cx(
-                  styles.column,
-                  styles.invertOrder,
-                  layout.screenshotMobileLayout
-                )}
-              >
-                <div className={styles.imgLayout}>
-                  <div className={styles.imgContainer}>
-                    <img
-                      className={matches ? '' : styles.imageRight}
-                      src={matches ? ghgMobileScreenshot : ghgBgScreenshot}
-                      alt="GHG Emissions section screenshot"
-                    />
-                  </div>
-                </div>
-              </div>
-            )}
-          </MobileOnly>
-        </Section>
-        <Section
-          className={cx(styles.section, styles.extraPadding, styles.pathways)}
-        >
-          <MobileOnly>
-            {matches => (
-              <div
-                className={cx(
-                  styles.column,
-                  styles.invertOrder,
-                  layout.screenshotMobileLayout
-                )}
-              >
-                <div className={styles.imgLayout}>
-                  <div className={styles.imgContainer}>
-                    <img
-                      className={matches ? '' : styles.imageRight}
-                      src={
-                        matches ? (
-                          pathwaysMobileScreenshot
-                        ) : (
-                          pathwaysBgScreenshot
-                        )
-                      }
-                      alt="Pathways section screenshot"
-                    />
-                  </div>
-                </div>
-              </div>
-            )}
-          </MobileOnly>
-          <div className={styles.column}>
-            <Intro
-              theme={introDark}
-              title="Chart and Visualize Decarbonization Pathways"
-              description="Where are global emissions and global temperatures headed? That all depends on the paths we take.<br />Draw insights from 30 potential future pathways developed by modeling teams from around the world. Pathways helps you navigate through multiple models and visualize scenarios for countries, regions and sectors."
-            />
-            <div className={styles.doubleFold}>
-              <Button color="yellow" link={'/pathways'}>
-                Explore Pathways
-              </Button>
-            </div>
-          </div>
-        </Section>
+        </Section> */}
       </div>
     );
   }
