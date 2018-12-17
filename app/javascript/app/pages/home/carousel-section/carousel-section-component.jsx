4ff50eefad1bf4f19af59d362cd6d34ece264b63
Javi Abia <javier.abia@vizzuality.com>
2018-12-17 18:53:16 +0100

    Update carousel section with geolocation

--- app/javascript/app/pages/home/carousel-section/carousel-section-component.jsx
+++ app/javascript/app/pages/home/carousel-section/carousel-section-component.jsx
@@ -1,41 +1,68 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import { Carousel } from 'cw-components';
 import Dropdown from 'components/dropdown';
 import Button from 'components/button';
 import { handleAnalytics } from 'utils/analytics';
+import GeoLocationProvider from 'providers/geolocation-provider';
 import { slidesData } from './carousel-section-data';
 
 import styles from './carousel-section-styles.scss';
 
-const BottomSlide = ({ title, text, buttons, countriesOptions, handleDropDownChange }) => (
-  <div className={styles.slideWrapper} key={title}>
-    <h3 className={styles.slideTitle}>{title}</h3>
-    {text.map(paragraph => (
-      <p key={paragraph} className={styles.slideParagraph}>
-        {paragraph}
-      </p>
-    ))}
-    <div className={styles.buttonsContainer}>
-      {
-        buttons.map(b => (b.link ?
-          (<Button className={styles.button} key={b.text} link={b.link} color={b.color}>{b.text}</Button>) :
-          (
-            <div key={b.text} className={styles.button}>
-              <Dropdown
-                placeholder="Select another country"
-                options={countriesOptions}
-                onValueChange={handleDropDownChange}
-                plain
-                hideResetButton
-              />
-            </div>
-          )
-        )
-      )}
-    </div>
+const renderExploreCountryButton = geolocation => (
+  <div key="geolocator">
+    <span
+      className={cx(styles.geoLocation, {
+        [styles.geoLocationHide]: !geolocation.country
+      })}
+    >
+      Connecting from {geolocation.country}?
+    </span>
+    <Button
+      className={styles.button}
+      color="yellow"
+      link={`/countries/${geolocation.iso ? geolocation.iso : ''}`}
+    >
+      Explore your country
+    </Button>
   </div>
 );
+
+const renderButton = (
+  b,
+  countriesOptions,
+  handleDropDownChange,
+  geolocation
+) => {
+  const { type } = b;
+  if (type === 'geolocator') {
+    return renderExploreCountryButton(geolocation);
+  }
+  if (type === 'button') {
+    return (
+      <Button
+        className={styles.button}
+        key={b.text}
+        link={b.link}
+        color={b.color}
+      >
+        {b.text}
+      </Button>
+    );
+  }
+  return (
+    <div key={b.text} className={styles.button}>
+      <Dropdown
+        placeholder="Select another country"
+        options={countriesOptions}
+        onValueChange={handleDropDownChange}
+        plain
+        hideResetButton
+      />
+    </div>
+  );
+};
 
 const TopSlide = ({ smImage, bgImage, altText }) => (
   <div className={styles.bottomSlideContainer} key={altText}>
@@ -48,6 +75,29 @@ const TopSlide = ({ smImage, bgImage, altText }) => (
   </div>
 );
 
+const BottomSlide = ({
+  title,
+  text,
+  buttons,
+  countriesOptions,
+  handleDropDownChange,
+  geolocation
+}) => (
+  <div className={styles.slideWrapper} key={title}>
+    <h3 className={styles.slideTitle}>{title}</h3>
+    {text.map(paragraph => (
+      <p key={paragraph} className={styles.slideParagraph}>
+        {paragraph}
+      </p>
+    ))}
+    <div className={styles.buttonsContainer}>
+      {buttons.map(b =>
+        renderButton(b, countriesOptions, handleDropDownChange, geolocation)
+      )}
+    </div>
+  </div>
+);
+
 class CarouselSectionComponent extends Component {
   pagingTitles = () => slidesData.map(s => s.pagingTitle);
 
@@ -57,29 +107,9 @@ class CarouselSectionComponent extends Component {
     handleAnalytics('Home', 'Search for a country', selected.value);
   };
 
-  // renderExploreCountryButton = () => (
-  //   <div>
-  //     <span
-  //       className={cx(styles.geoLocation, {
-  //         [styles.geoLocationHide]: !geolocation.country
-  //       })}
-  //     >
-  //       Connected from {geolocation.country}?
-  //     </span>
-  //     <div className={cx(styles.doubleFold, styles.mobileDoubleAction)}>
-  //       <Button
-  //         color="yellow"
-  //         link={`/countries/${geolocation.iso ? geolocation.iso : ''}`}
-  //       >
-  //         Explore your country
-  //       </Button>
-        
-  //     </div>
-  //   </div>
-  // )
-
   renderSlides = ({ geolocation, countriesOptions }) => {
-    const tops = slidesData &&
+    const tops =
+      slidesData &&
       slidesData.map(slide => (
         <TopSlide
           key={slide.smImage}
@@ -90,7 +120,8 @@ class CarouselSectionComponent extends Component {
         />
       ));
 
-    const bottoms = slidesData &&
+    const bottoms =
+      slidesData &&
       slidesData.map(slide => (
         <BottomSlide
           key={slide.title}
@@ -110,9 +141,14 @@ class CarouselSectionComponent extends Component {
   render() {
     return (
       <section className={styles.container}>
-        <Carousel pagingTitles={this.pagingTitles(slidesData)} primarySlider="bottom" autoplay={false}>
+        <Carousel
+          pagingTitles={this.pagingTitles(slidesData)}
+          primarySlider="bottom"
+          autoplay={false}
+        >
           {this.renderSlides(this.props)}
         </Carousel>
+        <GeoLocationProvider />
       </section>
     );
   }
@@ -123,7 +159,8 @@ BottomSlide.propTypes = {
   text: PropTypes.arrayOf(PropTypes.string),
   buttons: PropTypes.arrayOf(PropTypes.shape({})),
   countriesOptions: PropTypes.arrayOf(PropTypes.shape({})),
-  handleDropDownChange: PropTypes.func.isRequired
+  handleDropDownChange: PropTypes.func.isRequired,
+  geolocation: PropTypes.shape({})
 };
 
 BottomSlide.defaultProps = {
@@ -137,6 +174,10 @@ TopSlide.propTypes = {
   smImage: PropTypes.string.isRequired,
   bgImage: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired
+};
+
+CarouselSectionComponent.propTypes = {
+  history: PropTypes.shape({})
 };
 
 export default CarouselSectionComponent;
