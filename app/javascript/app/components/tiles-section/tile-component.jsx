fb0505b23370473b1815b17a9701a8f9e2136e50
Kamil Filip <kamil.filip94@gmail.com>
2018-12-18 16:26:08 +0000

    Add animation and layout features to tile-components

--- app/javascript/app/components/tiles-section/tile-component.jsx
+++ app/javascript/app/components/tiles-section/tile-component.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Truncate from 'react-truncate';
+import Icon from 'components/icon';
+import arrowTailRight from 'assets/icons/arrow-tail-right.svg';
 import styles from './tile-styles.scss';
 
-const Tile = () => (
+const Tile = ({ type, date, description, link }) => (
   <div className={styles.tile}>
-    <div className={styles.details}>
-      <span>Data</span>
-      <span>13 Jun 2018</span>
-    </div>
-    <div className={styles.content}>
-      <p className={styles.title}>Sample title</p>
-      <a href="#" className={styles.link}>
-        Explore here
-      </a>
+    <div className={styles.container}>
+      <div className={styles.details}>
+        <span>{type}</span>
+        <span>{date}</span>
+      </div>
+      <div className={styles.content}>
+        <Truncate
+          className={styles.description}
+          lines={4}
+          ellipsis={<span>...</span>}
+          width={320}
+        >
+          {description}
+        </Truncate>
+        <a href={link} className={styles.link}>
+          Explore here
+          <Icon icon={arrowTailRight} />
+        </a>
+      </div>
     </div>
   </div>
 );
+
+Tile.propTypes = {
+  type: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired
+};
 
 export default Tile;
