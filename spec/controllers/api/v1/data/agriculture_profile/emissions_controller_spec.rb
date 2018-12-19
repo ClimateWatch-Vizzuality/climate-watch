8cca7b8f67081770a3d839f79485e14319b62b71
Tiago Santos <santos.tiago@gmail.com>
2018-12-19 15:51:23 +0000

    Fixed the test for the emissions controller

--- spec/controllers/api/v1/data/agriculture_profile/emissions_controller_spec.rb
+++ spec/controllers/api/v1/data/agriculture_profile/emissions_controller_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Data::AgricultureProfile::EmissionsController, type: :controller do
-  include_context 'historical emissions records'
-
-  describe 'GET index' do
-    it 'renders data sources' do
-      get :index
-      expect(@response).to match_response_schema('historical_emissions_data_sources')
+  context do
+    let!(:some_emissions) {
+      FactoryBot.create_list(:agriculture_profile_emission_complete, 3)
+    }
+    describe 'GET index' do
+      it 'renders data sources' do
+        get :index
+        expect(@response).to be_successful
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body['data'].length).to eq(3)
+      end
     end
   end
 end
