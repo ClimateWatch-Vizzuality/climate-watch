dca191bbe93ef001d7006c813f092d7d6643fe59
Simao Belchior <simaobelchior@gmail.com>
2018-12-21 18:39:54 +0000

    removes unnecessary divs

--- app/javascript/app/pages/home/home-component.jsx
+++ app/javascript/app/pages/home/home-component.jsx
@@ -46,12 +46,8 @@ const Home = () => (
         }}
       />
     </div>
-    <div>
-      <HighlightedStories />
-    </div>
-    <div>
-      <SubscribeSection />
-    </div>
+    <HighlightedStories />
+    <SubscribeSection />
   </div>
 );
 
