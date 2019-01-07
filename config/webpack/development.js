1a00ce51d4c255692c38a0c06d49182ad33f8843
Javi Abia <javier.abia@vizzuality.com>
2019-01-07 16:27:58 +0100

    Update webpack configs

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,21 +1,16 @@
 // Note: You must restart bin/webpack-dev-server for changes to take effect
 
-const dotenv = require('dotenv').config(); // eslint-disable-line
+// eslint-disable-next-line
+const dotenv = require('dotenv').config();
 const merge = require('webpack-merge');
 const sharedConfig = require('./shared.js');
 const { settings, output } = require('./configuration.js');
 
 module.exports = merge(sharedConfig, {
   devtool: '#eval-source-map',
-
-  stats: {
-    errorDetails: true
-  },
-
-  output: {
-    pathinfo: true
-  },
-
+  mode: 'development',
+  stats: { errorDetails: true },
+  output: { pathinfo: true },
   devServer: {
     clientLogLevel: 'none',
     https: settings.dev_server.https,
@@ -26,8 +21,6 @@ module.exports = merge(sharedConfig, {
     compress: true,
     headers: { 'Access-Control-Allow-Origin': '*' },
     historyApiFallback: true,
-    watchOptions: {
-      ignored: /node_modules/
-    }
+    watchOptions: { ignored: /node_modules/ }
   }
 });
