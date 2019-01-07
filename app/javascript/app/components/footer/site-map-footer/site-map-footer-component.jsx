f47f4719d0b55cdde3c275e0b5664dc55b45314c
Kamil Filip <kamil.filip94@gmail.com>
2019-01-07 12:33:51 +0000

    Open sitemap links that refer to internal page in this same tab

--- app/javascript/app/components/footer/site-map-footer/site-map-footer-component.jsx
+++ app/javascript/app/components/footer/site-map-footer/site-map-footer-component.jsx
@@ -6,37 +6,45 @@ import { siteMapData } from './site-map-footer-data';
 
 import styles from './site-map-footer-styles';
 
-const Component = () => (
-  <section className={styles.siteMapContainer}>
-    <div className={styles.contentWrapper}>
-      <div className={styles.link}>
-        <Icon theme={{ icon: styles.logo }} icon={cwLogo} />
-      </div>
-      <div>
-        <div className={styles.sectionsContainer}>
-          {siteMapData.map(section => (
-            <div className={styles.sectionWrapper}>
-              <h4 className={styles.sectionHeader}>{section.title}</h4>
-              <ul className={styles.linksList}>
-                {section.links.map(l => (
-                  <li>
-                    <a
-                      href={l.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={styles.link}
-                    >
-                      {l.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+const Component = () => {
+  // eslint-disable-next-line react/prop-types
+  const siteMapLink = ({ title, href }) =>
+    (href.startsWith('/') ? (
+      <a href={href} className={styles.link}>
+        {title}
+      </a>
+    ) : (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.link}
+      >
+        {title}
+      </a>
+    ));
+
+  return (
+    <section className={styles.siteMapContainer}>
+      <div className={styles.contentWrapper}>
+        <div className={styles.link}>
+          <Icon theme={{ icon: styles.logo }} icon={cwLogo} />
+        </div>
+        <div>
+          <div className={styles.sectionsContainer}>
+            {siteMapData.map(section => (
+              <div className={styles.sectionWrapper}>
+                <h4 className={styles.sectionHeader}>{section.title}</h4>
+                <ul className={styles.linksList}>
+                  {section.links.map(l => <li>{siteMapLink(l)}</li>)}
+                </ul>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Component;
