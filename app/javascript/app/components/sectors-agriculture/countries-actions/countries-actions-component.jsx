1bac0b4c039c6deb22fe94afe4cbe27ce20fbbb7
Kamil Filip <kamil.filip94@gmail.com>
2019-01-08 18:22:37 +0000

    Implement basic routing for agriculture sector

---
+++ app/javascript/app/components/sectors-agriculture/countries-actions/countries-actions-component.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class CountriesActions extends PureComponent {
+  render() {
+    return (
+      <div style={{ height: '700px' }}>Countries’ Actions in their NDCs</div>
+    );
+  }
+}
+
+export default CountriesActions;
---
+++ app/javascript/app/components/sectors-agriculture/drivers-of-emissions/drivers-of-emissions-component.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+class DriversOfEmissions extends PureComponent {
+  render() {
+    return <div style={{ height: '700px' }}>Drivers of emissions</div>;
+  }
+}
+
+export default DriversOfEmissions;
