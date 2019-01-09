d721975341692221700ae690e988f3322b19e08b
Kamil Filip <kamil.filip94@gmail.com>
2019-01-09 10:45:24 +0000

    Add new background image for agriculture sector

--- app/javascript/app/routes/app-routes/app-routes.js
+++ app/javascript/app/routes/app-routes/app-routes.js
@@ -80,7 +80,7 @@ export default [
     component: SectorsAgriculture,
     label: 'SECTORS AGRICULTURE',
     sections: agricultureSections,
-    headerImage: 'home'
+    headerImage: 'sectors-agriculture'
   },
   {
     path: '/ndcs/country/:iso/full',
