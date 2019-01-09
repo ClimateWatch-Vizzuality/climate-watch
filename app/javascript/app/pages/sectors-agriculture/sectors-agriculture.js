7095ecafc43cdbb74f9762337c8deeab25d96f4e
Kamil Filip <kamil.filip94@gmail.com>
2019-01-09 11:31:41 +0000

    Remove comments

--- app/javascript/app/pages/sectors-agriculture/sectors-agriculture.js
+++ app/javascript/app/pages/sectors-agriculture/sectors-agriculture.js
@@ -7,9 +7,7 @@ import Component from './sectors-agriculture-component';
 const mapStateToProps = (state, { route, location }) => {
   const routeData = { route, location, hash: location.hash };
   return {
-    // query: location.search
     anchorLinks: getAnchorLinks(routeData)
-    // routeLinks: getRouteLinks(routeData)
   };
 };
 
