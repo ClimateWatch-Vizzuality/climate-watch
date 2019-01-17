faccd02a308e37be2eb9f30f499e3474af18cd4d
Simao Belchior <simaobelchior@gmail.com>
2019-01-17 17:22:14 +0000

    Include iso_code3 in the response, and allow filtering by iso_code3

--- app/serializers/api/v1/data/agriculture_profile/country_context_serializer.rb
+++ app/serializers/api/v1/data/agriculture_profile/country_context_serializer.rb
@@ -5,7 +5,12 @@ module Api
         class CountryContextSerializer < ActiveModel::Serializer
           attributes :location_id, :year, :employment_agri_female, :employment_agri_male,
                      :employment_agri_total, :total_pesticides_use, :total_fertilizers,
-                     :water_withdrawal, :water_withdrawal_rank, :value_added_agr
+                     :water_withdrawal, :water_withdrawal_rank, :value_added_agr,
+                     :iso_code3
+
+          def iso_code3
+            object.location&.iso_code3
+          end
         end
       end
     end
