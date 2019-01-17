7b673531275302baec75b7cf756867582ab91496
Simao Belchior <simaobelchior@gmail.com>
2019-01-17 17:32:12 +0000

    Fixes bug with filtering by iso_code3

--- app/models/agriculture_profile/country_context.rb
+++ app/models/agriculture_profile/country_context.rb
@@ -5,13 +5,14 @@ module AgricultureProfile
     validates_presence_of :year
 
     scope :by_location, ->(location_id) { where(location_id: location_id) }
+    scope :by_location_iso, ->(iso_code3) { joins(:location).where(locations: { iso_code3: iso_code3}) }
     scope :by_year, ->(year) { where(year: year) }
 
     def self.filter(params)
       context = CountryContext.all
       context = context.by_year(params[:year]) if params[:year]
       context = context.by_location(params[:location_id]) if params[:location_id]
-      context = context.by_location(params[:iso_code3]) if params[:iso_code3]
+      context = context.by_location_iso(params[:iso_code3]) if params[:iso_code3]
       context
     end
   end
