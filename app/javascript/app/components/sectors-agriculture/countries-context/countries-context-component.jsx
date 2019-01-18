bb6a70468a3b353476a3fa904752da8722530e3b
Javi Abia <javier.abia@vizzuality.com>
2019-01-18 23:36:15 +0100

    Add countries contexts section

--- app/javascript/app/components/sectors-agriculture/countries-context/countries-context-component.jsx
+++ app/javascript/app/components/sectors-agriculture/countries-context/countries-context-component.jsx
@@ -1,11 +1,13 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { Switch, Dropdown, Card } from 'cw-components';
+import { Switch, Dropdown, Card, Icon, PieChart } from 'cw-components';
 import CountriesProvider from 'providers/countries-provider';
+import WbCountryDataProvider from 'providers/wb-country-data-provider';
 import AgricultureCountriesContextProvider from 'providers/agriculture-countries-context-provider';
 import ButtonGroup from 'components/button-group';
+import infoIcon from 'assets/icons/info';
 import styles from './countries-context-styles.scss';
-
 
 const emissionTabs = [
   {
@@ -39,126 +41,146 @@ const buttonGroupConfig = [
   }
 ];
 
-const charts = [
-  {
-    text: 'Agriculture makes up a significant proportion of the total economic output of many countries, especially developing ones. ',
-    title: 'GDP indicators'
-  },
-  {
-    text: 'There were 21 million people (10.5% of the population) employed in Brazil\'s Agriculture sector in 2011, of which 10.7% were female.',
-    title: 'Socio-economic indicators'
-  },
-  {
-    text: 'In Brazil in 2011, 73% of total water withdrawn was employed in agricultural activities.',
-    title: 'Water withdrawal and water stress'
-  },
-  {
-    text: 'The use of synthetic nitrogen fertilizer is a large contributor to emissions in agriculture, owing to the potent greenhouse gas N2O. Heavy pesticide use can lead to harmful impacts on the environment.',
-    title: 'Fertilizer and pesticide use'
-  },
-  {
-    text: 'Share of different types of agricultural land as a percentage of total land area in a country. Depending on agricultural intensity, countries devote a different amount of land to agriculture. ',
-    title: 'Share in Land Area'
-  }
-];
+const cardTheme = {
+  card: styles.card,
+  data: styles.cardData,
+  contentContainer: styles.titleContainer
+};
 
-const Timeline = ({ years, yearSelected }) => (
-  <div className={styles.timelineWrapper}>
-    <p className={styles.label}>Year</p>
-    <div className={styles.dataContainer}>
-      <div className={styles.timelineContainer}>
-        <div className={styles.track}>
-          <div className={styles.marker} />
+const CountriesContext = ({
+  countries,
+  selectedCountry,
+  years,
+  selectedYear,
+  cards,
+  updateYearFilter,
+  updateCountryFilter
+}) => (
+  <div className={styles.container}>
+    <div>
+      <h2 className={styles.header}>CountriesContext</h2>
+      <div className={styles.intro}>
+        <p className={styles.introText}>
+          The agricultural sector differs vastly among countries and affects
+          jobs, economy, land-use, water and food security. Explore key
+          indicators of progress over time below.
+        </p>
+        <div className={styles.switchWrapper}>
+          <Switch
+            options={emissionTabs}
+            selectedOption={emissionTabs[1].value}
+            onClick={() => {}}
+            theme={{
+              wrapper: styles.switch,
+              option: styles.switchOption,
+              checkedOption: styles.switchSelected
+            }}
+          />
         </div>
       </div>
-      <div className={styles.yearsContainer}>
-        {
-          years.map(year => (
-            <span
-              className={cx(
-                styles.year,
-                { [styles.yearSelected]: year === yearSelected }
-              )}
-            >
-              {year}
-            </span>
-          ))
-        }
+      <div className={styles.actionsContainer}>
+        <div className={styles.selectorWrapper}>
+          {countries && (
+            <Dropdown
+              label={'Country'}
+              value={selectedCountry}
+              options={countries}
+              onValueChange={updateCountryFilter}
+            />
+          )}
+        </div>
+        <div className={styles.timelineWrapper}>
+          {selectedYear && (
+            <Dropdown
+              label={'Year'}
+              value={selectedYear}
+              options={years}
+              onValueChange={updateYearFilter}
+            />
+          )}
+        </div>
+        <div className={styles.buttonGroupWrapper}>
+          <ButtonGroup
+            className={styles.btnGroup}
+            buttonsConfig={buttonGroupConfig}
+          />
+        </div>
       </div>
     </div>
+    <div>
+      {selectedCountry ? (
+        <div className={styles.cardsContainer}>
+          {cards &&
+            cards.map(c => (
+              <Card key={c.title} title={c.title} theme={cardTheme}>
+                <div
+                  className={cx(styles.textHtmlWrapper, styles.introText)}
+                  dangerouslySetInnerHTML={{ __html: c.text }}
+                />
+                <div className={styles.cardContent}>
+                  <div className={cx(styles.textHtmlWrapper, styles.legend)}>
+                    {c.legend &&
+                      c.legend.map(i => (
+                        <div
+                          key={i.title}
+                          dangerouslySetInnerHTML={{ __html: i.text }}
+                          className={styles.legendItem}
+                        />
+                      ))}
+                  </div>
+                  <div className={styles.chart}>
+                    {c.chartConfig && (
+                      <PieChart
+                        data={c.chartData}
+                        width={150}
+                        config={c.chartConfig}
+                      />
+                    )}
+                  </div>
+                  {c.rank && (
+                    <div
+                      className={cx(styles.textHtmlWrapper, styles.rank)}
+                      dangerouslySetInnerHTML={{ __html: c.rank }}
+                    />
+                  )}
+                </div>
+                <Icon
+                  icon={infoIcon}
+                  theme={{ icon: styles.cardInfoIcon }}
+                  onClick={() => {}}
+                />
+                <div className={styles.yearData}>
+                  <span>{selectedYear && selectedYear.value}</span> data
+                </div>
+              </Card>
+            ))}
+        </div>
+      ) : (
+        <div>SELECT A COUNTRY</div>
+      )}
+    </div>
+    <CountriesProvider />
+    <WbCountryDataProvider />
+    <AgricultureCountriesContextProvider
+      country={selectedCountry && selectedCountry.value}
+    />
   </div>
 );
 
-class CountriesContext extends PureComponent {
-  render() {
-    const {
-      countries
-    } = this.props;
-    return (
-      <div className={styles.container}>
-        <div>
-          <h2 className={styles.header}>CountriesContext</h2>
-          <div className={styles.intro}>
-            <p className={styles.introText}>
-              The agricultural sector differs vastly among countries and affects jobs, economy, land-use, water and food security.
-              Explore key indicators of progress over time below.
-            </p>
-            <div className={styles.switchWrapper}>
-              <Switch
-                options={emissionTabs}
-                selectedOption={emissionTabs[1].value}
-                onClick={() => {}}
-                theme={{
-                  wrapper: styles.switch,
-                  option: styles.switchOption,
-                  checkedOption: styles.switchSelected
-                }}
-              />
-            </div>
-          </div>
-          <div className={styles.actionsContainer}>
-            <div className={styles.selectorWrapper}>
-              <Dropdown
-                label={'Country'}
-                value={{ label: 'galicia', value: 'ga' }}
-                options={[{ label: 'galicia', value: 'ga' }]}
-                onValueChange={() => {}}
-              />
-            </div>
-            <div className={styles.timelineWrapper}>
-              <Timeline
-                years={['2009', '2010', '2011', '2012', '2013', '2014']}
-                yearSelected={'2012'}
-              />
-            </div>
-            <div className={styles.buttonGroupWrapper}>
-              <ButtonGroup
-                className={styles.btnGroup}
-                buttonsConfig={buttonGroupConfig}
-              />
-            </div>
-          </div>
-        </div>
-        <div>
-          {
-            charts.map(c => (
-              <Card key={c.title} title={c.title} >
-                {c.text}
-              </Card>
-            ))
-          }
-        </div>
-        <CountriesProvider />
-        <AgricultureCountriesContextProvider country={70} year={2012} />
-      </div>
-    );
-  }
-}
-
-// CountriesContext.propTypes = {
-//   handleTabChange: PropTypes.func.isRequired,
-//   emissionTabs: PropTypes.array.isRequired,
-//   activeTab: PropTypes.string.isRequired
-// };
+CountriesContext.propTypes = {
+  countries: PropTypes.arrayOf(PropTypes.shape({})),
+  selectedCountry: PropTypes.shape({
+    value: PropTypes.string,
+    label: PropTypes.string
+  }),
+  years: PropTypes.arrayOf(PropTypes.shape({})),
+  selectedYear: PropTypes.shape({
+    value: PropTypes.string,
+    label: PropTypes.string
+  }),
+  cards: PropTypes.arrayOf(PropTypes.shape({})),
+  updateYearFilter: PropTypes.func.isRequired,
+  updateCountryFilter: PropTypes.func.isRequired
+};
 
 export default CountriesContext;
--- app/javascript/app/components/sectors-agriculture/countries-context/countries-contexts-selectors.js
+++ app/javascript/app/components/sectors-agriculture/countries-context/countries-contexts-selectors.js
@@ -1,3 +1,209 @@
-import { createSelector } from 'reselect';
+import { createSelector, createStructuredSelector } from 'reselect';
+import { isEmpty, sortBy, some } from 'lodash';
 
-const getCountriesContextsData = ({ countriesContexts }) => countriesContexts.data;
+const getCountriesContextsData = ({ agricultureCountriesContexts }) =>
+  agricultureCountriesContexts && agricultureCountriesContexts.data;
+const getWBCountriesData = ({ wbCountryData }) =>
+  wbCountryData && wbCountryData.data;
+const getLocationsData = ({ countries }) => countries && countries.data;
+const getSearch = ({ search }) => search || null;
+
+const legendHtmlDot = (text, color, value, unit) =>
+  `<p><span style="background-color: ${color}; width: 10px; height: 10px; display: inline-block; border-radius: 10px; margin-right: 10px;" ></span>${text}</p><p style="color: ${color};">${value ||
+    '---'}${unit}.<p/>`;
+
+const getChartConfig = (labels, year, unit, colors) => ({
+  outerRadius: 55,
+  innerRadius: 25,
+  hideLegend: true,
+  hideLabel: true,
+  tooltip: {
+    [labels[0].slug]: { label: `${labels[0].label}` },
+    [labels[1].slug]: { label: `${labels[1].label}` }
+  },
+  animation: true,
+  axes: {
+    yLeft: {
+      unit,
+      label: year
+    }
+  },
+  theme: {
+    [labels[0].slug]: { label: labels[0].label, stroke: `${colors[0]}` },
+    [labels[1].slug]: { label: labels[1].label, stroke: `${colors[1]}` }
+  }
+});
+
+const getCountries = createSelector(getLocationsData, locations => {
+  if (!locations) return [];
+  return locations.map(l => ({
+    label: l.wri_standard_name,
+    value: l.iso_code3
+  }));
+});
+
+const getSelectedCountry = createSelector(
+  [getSearch, getCountries],
+  (search, countries) => {
+    if (!search && !search.country && isEmpty(countries)) return null;
+    const selectedCountry = countries.find(c => c.value === search.country);
+    return selectedCountry;
+  }
+);
+
+const getYears = createSelector(getCountriesContextsData, data => {
+  if (isEmpty(data)) return null;
+  return sortBy(data, 'year').map(r => ({
+    label: r.year.toString(),
+    value: r.year.toString()
+  }));
+});
+
+const getSelectedYear = createSelector(
+  [getSearch, getYears],
+  (search, years) => {
+    if (!search && !search.year && !years) return null;
+    if (
+      (!search || !search.year || !some(years, ['value', search.year])) &&
+      years
+    ) {
+      return years[0];
+    }
+    return { label: search.year, value: search.year };
+  }
+);
+
+const getCardsData = createSelector(
+  [
+    getCountriesContextsData,
+    getWBCountriesData,
+    getSelectedCountry,
+    getSelectedYear,
+    getCountries,
+    getYears
+  ],
+  (contextsData, wbData, country, year, countries, years) => {
+    if (isEmpty(contextsData) || isEmpty(wbData)) return null;
+    const c = country || countries[0];
+    const y = year || years[0];
+    const yearData = contextsData.find(d => d.year === parseInt(y.value, 10));
+    const countryCode = c.value;
+    const wbCountryData = wbData[countryCode].find(
+      d => d.year === parseInt(y.value, 10)
+    );
+
+    const socioeconomic = {
+      title: 'Socio-economic indicators',
+      text: `<p>There were <span style="color: red;">21 million</span> people (<span style="color: red;">10.5%</span> of the population) employed in <span>${c.label}'s</span> Agriculture sector in <span>${y.value}</span>, of which <span>${Math.round(
+        yearData.employment_agri_female * 100
+      ) / 100}%</span> were female.</p>`
+    };
+
+    const GDP = {
+      chartConfig: getChartConfig(
+        [
+          { label: 'Agriculture production', slug: 'agricultureProduction' },
+          { label: 'Total GDP', slug: 'totalGDP' }
+        ],
+        y.label,
+        'USD',
+        ['#0677B3', '#CACCD0']
+      ),
+      chartData: [
+        {
+          name: 'agricultureProduction',
+          value: yearData.value_added_agr,
+          fill: '#0677B3'
+        },
+        {
+          name: 'totalGDP',
+          value: wbCountryData && wbCountryData.gdp ? wbCountryData.gdp : 0,
+          fill: '#CACCD0'
+        }
+      ],
+      title: 'GDP indicators',
+      legend: [
+        {
+          text: legendHtmlDot(
+            'Agriculture production',
+            '#0677B3',
+            yearData.value_added_agr,
+            '$USD'
+          )
+        },
+        {
+          text: legendHtmlDot(
+            'Total GDP',
+            '#CACCD0',
+            wbCountryData ? wbCountryData.gdp : 0,
+            '$USD'
+          )
+        }
+      ],
+      text:
+        '<p><span>Agriculture</span> makes up a significant proportion of the total economic output of many countries, especially developing ones. </p>'
+    };
+    const water = {
+      title: 'Water withdrawal and water stress',
+      legend: [
+        {
+          text: legendHtmlDot(
+            'Agricultural activities',
+            '#0677B3',
+            yearData.water_withdrawal,
+            '%'
+          )
+        }
+      ],
+      rank: `<p>Water stress country ranking <span>${yearData.water_withdrawal_rank ||
+        '---'}</span> of 156</p>`,
+      text: `<p>In <span>${c.label}</span> in <span>${y.label}</span>, <span>${contextsData.water_withdrawal}%</span> of total water withdrawn was employed in agricultural activities.</p>`
+    };
+    const fertilizer = {
+      chartConfig: getChartConfig(
+        [
+          { label: 'Fertilizer use', slug: 'fertilizerUse' },
+          { label: 'Pesticides use', slug: 'pesticidesUse' }
+        ],
+        y.label,
+        'ton',
+        ['#0677B3', '#1ECDB0']
+      ),
+      chartData: [
+        { name: 'fertilizerUse', value: yearData.total_fertilizers },
+        { name: 'pesticidesUse', value: yearData.total_pesticides_use }
+      ],
+      legend: [
+        {
+          text: legendHtmlDot(
+            'Fertilizer use',
+            '#0677B3',
+            yearData.total_fertilizers,
+            'ton'
+          )
+        },
+        {
+          text: legendHtmlDot(
+            'Pesticides use',
+            '#1ECDB0',
+            yearData.total_pesticides_use,
+            'ton'
+          )
+        }
+      ],
+      title: 'Fertilizer and pesticide use',
+      text:
+        '<p>The use of synthetic nitrogen fertilizer is a large contributor to emissions in agriculture, owing to the potent greenhouse gas N2O. Heavy pesticide use can lead to harmful impacts on the environment.</p>'
+    };
+
+    return [GDP, socioeconomic, water, fertilizer];
+  }
+);
+
+export const countriesContexts = createStructuredSelector({
+  years: getYears,
+  selectedYear: getSelectedYear,
+  countries: getCountries,
+  selectedCountry: getSelectedCountry,
+  cards: getCardsData
+});
