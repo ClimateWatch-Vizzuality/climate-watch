8937ced1deaba5784a06de88a8e9b58e411321e7
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-01-30 14:42:34 +0000

    Improve tooltip for meat data bar chart

--- app/javascript/app/components/sectors-agriculture/countries-context/meat-data/tooltip/tooltip-component.jsx
+++ app/javascript/app/components/sectors-agriculture/countries-context/meat-data/tooltip/tooltip-component.jsx
@@ -10,11 +10,9 @@ import styles from './tooltip-styles.scss';
 const Tooltip = ({ content, config }) => {
   const payload = get(content, 'payload[0].payload');
   const category = (payload && payload.x) || '';
-  const isCountrySelected = payload && payload.yCountry;
-  const isOtherCountriesSelected = payload && payload.yOthers;
 
-  const countryValue = (payload && payload.yCountry) || 'n/a';
-  const otherCountriesValue = (payload && payload.yOthers) || 'n/a';
+  const countryValue = payload && payload.yCountry;
+  const otherCountriesValue = payload && payload.yOthers;
 
   const countryName = get(config, 'tooltip.yCountry.label');
   const othersName = get(config, 'tooltip.yOthers.label');
@@ -27,42 +25,38 @@ const Tooltip = ({ content, config }) => {
     <div className={styles.tooltip}>
       <div className={styles.category}>{category}</div>
       <p className={styles.info}>
-        {isCountrySelected && (
-          <React.Fragment>
-            <Tag
-              key="countryAgr"
-              label={countryName}
-              theme={{
-                tag: styles.tag,
-                label: styles.label
-              }}
-              canRemove={false}
-              color={countryColor}
-            />
-            <div className={styles.value}>
-              {format('.2s')(countryValue)} {unit}
-            </div>
-          </React.Fragment>
-        )}
+        <Tag
+          key="countryAgr"
+          label={countryName}
+          theme={{
+            tag: styles.tag,
+            label: styles.label
+          }}
+          canRemove={false}
+          color={countryColor}
+        />
+        <div className={styles.value}>
+          {countryValue ? `${format('.2s')(countryValue)} ${unit}` : 'n/a'}
+        </div>
       </p>
       <p className={styles.info}>
-        {isOtherCountriesSelected && (
-          <React.Fragment>
-            <Tag
-              key="othersAgr"
-              label={othersName}
-              theme={{
-                tag: styles.tag,
-                label: styles.label
-              }}
-              canRemove={false}
-              color={othersColor}
-            />
-            <div className={styles.value}>
-              {format('.2s')(otherCountriesValue)} {unit}
-            </div>
-          </React.Fragment>
-        )}
+        <Tag
+          key="othersAgr"
+          label={othersName}
+          theme={{
+            tag: styles.tag,
+            label: styles.label
+          }}
+          canRemove={false}
+          color={othersColor}
+        />
+        <div className={styles.value}>
+          {otherCountriesValue ? (
+            `${format('.2s')(otherCountriesValue)} ${unit}`
+          ) : (
+            'n/a'
+          )}
+        </div>
       </p>
     </div>
   );
