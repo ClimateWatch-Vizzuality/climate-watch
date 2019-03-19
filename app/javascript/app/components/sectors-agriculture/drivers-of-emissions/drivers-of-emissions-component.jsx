61c89cacc8400bcb7343d68b1b56f208ff440cc2
kfilip <kamil.filip94@gmail.com>
2019-03-19 14:23:16 +0000

    Implement text reviews for DoE section

--- app/javascript/app/components/sectors-agriculture/drivers-of-emissions/drivers-of-emissions-component.jsx
+++ app/javascript/app/components/sectors-agriculture/drivers-of-emissions/drivers-of-emissions-component.jsx
@@ -6,49 +6,30 @@ import { emissionTabs } from './drivers-of-emissions-data';
 import HistoricalEmissionsGraph from './historical-emissions-graph/historical-emissions-graph';
 import EmissionPathwaysGraph from './emission-pathways-graph/emission-pathways-graph';
 
-// eslint-disable-next-line react/prop-types
-const emissionDescription = ({ agriculturePercentage }) => (
-  <p>
-    Many processes result in agricultural emissions, with fermentation, manure
-    and rice cultivation contributing the major share{agriculturePercentage &&
-      ` (${agriculturePercentage})`}. This graph contains all the emissions
-    produced in the different agricultural emissions sub-domains, providing a
-    picture of the contribution to the total amount of GHG emissions from
-    agriculture. GHG emissions from agriculture consist of non-CO<sub>2</sub>{' '}
-    gases produced by crop and livestock production and management activities.
-    Explore the history and potential future pathways of those drivers.
-  </p>
-);
-
-const pathwaysDescription = () => (
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-);
-
 const switchOptions = {
-  HISTORICAL_EMISSIONS: {
-    Component: HistoricalEmissionsGraph,
-    Description: emissionDescription
-  },
-  FUTURE_PATHWAYS: {
-    Component: EmissionPathwaysGraph,
-    Description: pathwaysDescription
-  }
+  HISTORICAL_EMISSIONS: HistoricalEmissionsGraph,
+  FUTURE_PATHWAYS: EmissionPathwaysGraph
 };
 
 class DriversOfEmissions extends PureComponent {
   render() {
-    const { handleTabChange, activeTab, pieChartData } = this.props;
-    const agriculturePercentage =
-      pieChartData && pieChartData.emissionPercentage;
+    const { handleTabChange, activeTab } = this.props;
     const selectedOption = activeTab.value || emissionTabs[0].value;
-    const { Component, Description } = switchOptions[selectedOption];
+    const Component = switchOptions[selectedOption];
+    const emissionDescription = (
+      <div className={styles.description}>
+        <p>
+          Agriculture is one of the most rapidly growing sectors and accounts
+          for a significant portion of emissions in developing countries. GHG
+          emissions from agriculture consist of non-CO<sub>2</sub> gases
+          produced by crop and livestock production and management activities.
+        </p>
+        <p>
+          Explore what activities are driving agricultural emissions in the
+          chart below and see future pathways of those drivers.
+        </p>
+      </div>
+    );
 
     return (
       <div>
@@ -56,7 +37,7 @@ class DriversOfEmissions extends PureComponent {
           <div className={styles.header}>
             <h2>Drivers of Emissions</h2>
             <div className={styles.intro}>
-              <Description agriculturePercentage={agriculturePercentage} />
+              {emissionDescription}
               <Switch
                 options={emissionTabs}
                 selectedOption={activeTab.value}
@@ -77,14 +58,12 @@ class DriversOfEmissions extends PureComponent {
 
 DriversOfEmissions.propTypes = {
   handleTabChange: PropTypes.func,
-  activeTab: PropTypes.object,
-  pieChartData: PropTypes.object
+  activeTab: PropTypes.object
 };
 
 DriversOfEmissions.defaultProps = {
   handleTabChange: () => {},
-  activeTab: emissionTabs[0],
-  pieChartData: null
+  activeTab: emissionTabs[0]
 };
 
 export default DriversOfEmissions;
