6933eb7841352dd4c23933c9dffa57b6f4a4db1a
Simao Belchior <simaobelchior@gmail.com>
2019-04-01 21:13:10 +0100

    Enable platforms in the menu

--- app/javascript/app/components/countries-select/countries-select.js
+++ app/javascript/app/components/countries-select/countries-select.js
@@ -76,25 +76,26 @@ class CountrySelectContainer extends PureComponent {
       </svg>
     );
 
-    const markers = [];
-//      {
-//        coordinates: [28.034, -26.1952],
-//        link: 'http://southafricaclimateexplorer.org',
-//        name: 'South Africa platform',
-//        pin
-//      },
-//      {
-//        coordinates: [79.2167, 20.6448],
-//        link: 'http://india-platform-staging.vizzuality.com',
-//        name: 'India platform',
-//        pin
-//      },
-//      {
-//        coordinates: [112.0149, -3.54],
-//        link: 'http://indonesia-platform-staging.vizzuality.com',
-//        name: 'Indonesia platform',
-//        pin
-//      }
+    const markers = [
+      {
+        coordinates: [28.034, -26.1952],
+        link: 'http://southafricaclimateexplorer.org',
+        name: 'South Africa Climate Explorer',
+        pin
+      },
+      //      {
+      //        coordinates: [79.2167, 20.6448],
+      //        link: 'http://india-platform-staging.vizzuality.com',
+      //        name: 'India platform',
+      //        pin
+      //      },
+      {
+        coordinates: [112.0149, -3.54],
+        link: 'https://indonesia.climatewatchdata.org',
+        name: 'Climate Watch Indonesia',
+        pin
+      }
+    ];
 
     return createElement(CountrySelectComponent, {
       ...this.props,
