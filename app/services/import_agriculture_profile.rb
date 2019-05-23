ad1d95d5eb2eddb513f052d7d3d6b82d63325fb9
Simao Belchior <simaobelchior@gmail.com>
2019-05-23 14:32:01 +0100

    Enable upsert on importing

--- app/services/import_agriculture_profile.rb
+++ app/services/import_agriculture_profile.rb
@@ -45,12 +45,23 @@ class ImportAgricultureProfile
         next if value.second.blank?
         year = value.first.to_s.to_i
         area = areas.select{|m| m.location_id == location_id && m.year == year }.first ||
-          AgricultureProfile::Area.new(location_id: location_id, year: year)
+          AgricultureProfile::Area.find_or_initialize_by(location_id: location_id, year: year)
         area.send(:"#{indicator.downcase}=", value.second)
         areas << area
       end
+      puts "#{areas.size} areas to add!"
+      AgricultureProfile::Area.import!(areas,
+                                       on_duplicate_key_update: { conflict_target: [:id],
+                                                                  columns: [:share_in_land_area_1,
+                                                                            :share_in_land_area_2,
+                                                                            :share_in_land_area_3,
+                                                                            :share_in_land_area_4,
+                                                                            :share_in_agricultural_area_1,
+                                                                            :share_in_agricultural_area_2,
+                                                                            :share_in_agricultural_area_3]
+      })
+      areas = []
     end
-    AgricultureProfile::Area.import!(areas)
   end
 
   def import_meat_consumptions(content, filepath)
@@ -70,13 +81,25 @@ class ImportAgricultureProfile
         next if value.second.blank?
         year = value.first.to_s.to_i
         meat = meats.select{|m| m.location_id == location_id && m.year == year }.first ||
-          AgricultureProfile::MeatConsumption.new(location_id: location_id,
+          AgricultureProfile::MeatConsumption.find_or_initialize_by(location_id: location_id,
             year: year)
         meat.send(:"#{indicator.downcase}=", value.second)
         meats << meat
       end
+      puts "#{meats.size} records to add of meat consumption"
+      AgricultureProfile::MeatConsumption.import!(meats,
+                                       on_duplicate_key_update: { conflict_target: [:id],
+                                                                  columns: [:meat_consumption_1,
+                                                                            :meat_consumption_2,
+                                                                            :meat_consumption_3,
+                                                                            :meat_consumption_4,
+                                                                            :meat_consumption_per_capita_1,
+                                                                            :meat_consumption_per_capita_2,
+                                                                            :meat_consumption_per_capita_3,
+                                                                            :meat_consumption_per_capita_4]
+      })
+      meats = []
     end
-    AgricultureProfile::MeatConsumption.import!(meats)
   end
 
   def import_meat_trades(content, filepath)
@@ -100,7 +123,7 @@ class ImportAgricultureProfile
           meat_production = meat_productions.select do |mp|
             mp.location_id == location_id && mp.year == year
           end.first ||
-            AgricultureProfile::MeatProduction.new(location_id: location_id,
+            AgricultureProfile::MeatProduction.find_or_initialize_by(location_id: location_id,
                                                    year: year)
           meat_production.send(:"#{indicator.downcase}=", value.second)
           meat_productions << meat_production
@@ -108,14 +131,48 @@ class ImportAgricultureProfile
           meat_trade = meat_trades.select do |mt|
             mt.location_id == location_id && mt.year == year
           end.first ||
-          AgricultureProfile::MeatTrade.new(location_id: location_id,
+          AgricultureProfile::MeatTrade.find_or_initialize_by(location_id: location_id,
                                             year: year)
           meat_trade.send(:"#{indicator.downcase}=", value.second)
           meat_trades << meat_trade
         end
       end
+      puts "#{meat_productions.size} meat productions to add"
+      puts "#{meat_trades.size} meat trades to add"
+      AgricultureProfile::MeatProduction.import!(meat_productions,
+                                       on_duplicate_key_update: { conflict_target: [:id],
+                                                                  columns: [:production_agr_1,
+                                                                            :production_agr_2,
+                                                                            :production_agr_3,
+                                                                            :production_agr_4,
+                                                                            :production_agr_5,
+                                                                            :production_agr_6,
+                                                                            :production_agr_7,
+                                                                            :production_agr_8,
+                                                                            :production_agr_9,
+                                                                            :production_agr_10]
+      })
+      AgricultureProfile::MeatTrade.import!(meat_trades,
+                                       on_duplicate_key_update: { conflict_target: [:id],
+                                                                  columns: [:trade_import_1,
+                                                                            :trade_import_2,
+                                                                            :trade_import_3,
+                                                                            :trade_import_4,
+                                                                            :trade_import_5,
+                                                                            :trade_import_6,
+                                                                            :trade_import_7,
+                                                                            :trade_import_8,
+                                                                            :trade_export_1,
+                                                                            :trade_export_2,
+                                                                            :trade_export_3,
+                                                                            :trade_export_4,
+                                                                            :trade_export_5,
+                                                                            :trade_export_6,
+                                                                            :trade_export_7,
+                                                                            :trade_export_8]
+      })
+      meat_productions = []
+      meat_trades = []
     end
-    AgricultureProfile::MeatProduction.import!(meat_productions)
-    AgricultureProfile::MeatTrade.import!(meat_trades)
   end
 end
