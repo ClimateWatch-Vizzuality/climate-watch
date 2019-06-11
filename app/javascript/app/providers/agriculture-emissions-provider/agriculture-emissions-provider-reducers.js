6a8aa3fbfcea671e3ee067511e6255497858278c
Tomasz Subik <tsubik@gmail.com>
2019-06-11 11:17:30 +0200

    move agriculture total emission to backend, import emissions once again

--- app/javascript/app/providers/agriculture-emissions-provider/agriculture-emissions-provider-reducers.js
+++ app/javascript/app/providers/agriculture-emissions-provider/agriculture-emissions-provider-reducers.js
@@ -1,24 +1,9 @@
-import { AGRICULTURE_TOTAL_EMISSIONS } from 'data/constants';
-
 export const initialState = {
   loading: false,
   loaded: false,
   data: [],
   error: false
 };
-
-const appendMissingTotalEmissionsProps = d => {
-  if (d.emission_subcategory.short_name === AGRICULTURE_TOTAL_EMISSIONS.short_name) {
-    const emission_subcategory = {
-      ...d.emission_subcategory,
-      ...AGRICULTURE_TOTAL_EMISSIONS
-    };
-    return { ...d, emission_subcategory };
-  }
-  return d;
-};
-
-const transformData = data => data.map(appendMissingTotalEmissionsProps);
 
 const setLoading = (loading, state) => ({ ...state, loading });
 const setLoaded = (loaded, state) => ({ ...state, loaded });
@@ -27,6 +12,6 @@ const setError = (state, error) => ({ ...state, error });
 export default {
   fetchAgricultureEmissionsInit: state => setLoading(true, state),
   fetchAgricultureEmissionsReady: (state, { payload: { data } }) =>
-    setLoaded(true, setLoading(false, { ...state, data: transformData(data) })),
+    setLoaded(true, setLoading(false, { ...state, data })),
   fetchAgricultureEmissionsFail: state => setLoading(setError(state, true), false)
 };
