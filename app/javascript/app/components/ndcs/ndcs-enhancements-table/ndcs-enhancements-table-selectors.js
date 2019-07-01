9beceb9c5ab8fce6b55231e4e40e0b1d03fea441
kevinlustig <kevin.lustig@gmail.com>
2019-07-01 11:10:09 -0400

    Adding cw-components table and finalizing table data display

--- app/javascript/app/components/ndcs/ndcs-enhancements-table/ndcs-enhancements-table-selectors.js
+++ app/javascript/app/components/ndcs/ndcs-enhancements-table/ndcs-enhancements-table-selectors.js
@@ -89,13 +89,17 @@ export const tableRemoveIsoFromData = createSelector(
       try {
         date = new Date(d['Statement Date']);
         date = {
-          name:date.toLocaleDateString("en-US"),
-          value:date.getTime()
-        }
-      } catch (e) {};
+          name: date.toLocaleDateString('en-US'),
+          value: date.getTime()
+        };
+      } catch (e) {}
       d['Statement Date'] = date;
-      d['Source Link'] = d['Source Link'].replace("href=","target='_blank' href=");
-      d.country = "<a href='"+`/ndcs/country/${d.iso}`+"'>"+d.country+"</a>";
+      d['Source Link'] = d['Source Link'].replace(
+        'href=',
+        "target='_blank' href="
+      );
+      d.country =
+        "<a href='" + `/ndcs/country/${d.iso}` + "'>" + d.country + '</a>';
       delete d.iso;
       return d;
     });
