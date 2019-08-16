440606786d76cf3ad852e1eb27423827572269a8
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-08-16 11:43:55 +0100

    Fix Indc::ValueSerializer label_slug attribute

--- app/serializers/api/v1/indc/value_serializer.rb
+++ app/serializers/api/v1/indc/value_serializer.rb
@@ -6,6 +6,10 @@ module Api
         attribute :label_id, if: -> { object.label_id }
         attribute :label_slug, if: -> { object.label&.slug }
         attribute :sector_id, if: -> { object.sector_id }
+
+        def label_slug
+          object.label&.slug
+        end
       end
     end
   end
