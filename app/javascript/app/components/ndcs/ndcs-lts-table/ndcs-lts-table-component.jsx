0ac99599c4fc58a758d2978d7d8f50c1aab47e54
kevinlustig <kevin.lustig@gmail.com>
2019-08-19 12:10:17 -0700

    Duplicating NDCS Enhancements page and components to create NDCS LTS page and components

---
+++ app/javascript/app/components/ndcs/ndcs-lts-table/ndcs-lts-table-component.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactTooltip from 'react-tooltip';
+import { Link } from 'react-router-dom';
+import Search from 'components/search';
+import { Table } from 'cw-components';
+import NoContent from 'components/no-content';
+import Loading from 'components/loading';
+import darkSearch from 'styles/themes/search/search-dark.scss';
+import styles from './ndcs-lts-table-styles.scss';
+
+const renderSearch = (searchHandler, query) => (
+  <Search
+    value={query}
+    theme={darkSearch}
+    onChange={searchHandler}
+    className={styles.searchBox}
+    placeholder="Search table data"
+    plain
+  />
+);
+
+const NDCSLTSTable = ({
+  loading,
+  tableData,
+  tableHeaders,
+  query,
+  handleSearchChange,
+  noContentMsg
+}) => (
+  <div>
+    <div className={styles.wrapper}>
+      {loading && <Loading light className={styles.loader} />}
+      {!loading && (
+        <div className={styles.filtersLayout}>
+          {renderSearch(handleSearchChange, query)}
+        </div>
+      )}
+      {!loading &&
+      tableData &&
+      tableData.length > 0 && (
+        <div className={styles.tableWrapper}>
+          <Table
+            data={tableData}
+            horizontalScroll
+            parseHtml
+            dynamicRowsHeight={true}
+            setColumnWidth={column => 1100 / 6}
+            defaultColumns={[
+              'country',
+              'Share of GHG Emissions',
+              '2020 NDC Status',
+              'Statement',
+              'Source Link',
+              'Statement Date'
+            ]}
+          />
+        </div>
+      )}
+      {!loading &&
+      (!tableData || tableData.length <= 0) && (
+        <NoContent className={styles.noContent} message={noContentMsg} />
+      )}
+    </div>
+  </div>
+);
+
+NDCSLTSTable.propTypes = {
+  loading: PropTypes.bool,
+  noContentMsg: PropTypes.string,
+  query: PropTypes.string,
+  tableData: PropTypes.array,
+  tableHeaders: PropTypes.array,
+  handleSearchChange: PropTypes.func.isRequired
+};
+
+export default NDCSLTSTable;
---
+++ app/javascript/app/pages/ndcs-lts/ndcs-lts-component.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { renderRoutes } from 'react-router-config';
+import Proptypes from 'prop-types';
+import Header from 'components/header';
+import Intro from 'components/intro';
+import AutocompleteSearch from 'components/autocomplete-search';
+import { NDC_LTS } from 'data/SEO';
+import { MetaDescription, SocialMetadata } from 'components/seo';
+
+import layout from 'styles/layout.scss';
+import styles from './ndcs-lts-styles.scss';
+
+const NDCSLTS = ({ query, route }) => (
+  <div>
+    <MetaDescription
+      descriptionContext={NDC_LTS}
+      subtitle="NDC LTS"
+    />
+    <SocialMetadata
+      descriptionContext={NDC_LTS}
+      href={location.href}
+    />
+    <Header route={route}>
+      <div className={layout.content}>
+        <div className="grid-column-item">
+          <div className={styles.headerLayout}>
+            <Intro
+              title="2020 Ambition Tracker"
+              description={`<p>The Paris Agreement calls on countries to deliver every five years new national climate commitments (NDCs) informed by the latest advances in technology, science and shifting economic trends.</p>`}
+            />
+            <AutocompleteSearch />
+          </div>
+        </div>
+      </div>
+    </Header>
+    <div className={styles.wrapper}>
+      <div className={layout.content}>
+        {route.sections &&
+          route.sections.length > 0 &&
+          route.sections.map(section => (
+            <div className={styles.section}>
+              <div id={section.hash} className={styles.sectionHash} />
+              <section.component />
+            </div>
+          ))}
+      </div>
+    </div>
+  </div>
+);
+
+NDCSLTS.propTypes = {
+  query: Proptypes.string,
+  route: Proptypes.object.isRequired
+};
+
+export default NDCSLTS;
