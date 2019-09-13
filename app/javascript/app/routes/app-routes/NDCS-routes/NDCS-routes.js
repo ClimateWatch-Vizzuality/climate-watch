43ff74be1e337ddf950f7066072550f516fce688
kevinlustig <kevin.lustig@gmail.com>
2019-09-13 11:11:41 -0400

    Hiding menu items when NDCS feature flags are false

--- app/javascript/app/routes/app-routes/NDCS-routes/NDCS-routes.js
+++ app/javascript/app/routes/app-routes/NDCS-routes/NDCS-routes.js
@@ -2,22 +2,26 @@ const activeId = 'ndc';
 const FEATURE_NDCS_ENHANCEMENTS =
   process.env.FEATURE_NDCS_ENHANCEMENTS === 'true';
 const FEATURE_NDCS_LTS = process.env.FEATURE_NDCS_LTS === 'true';
-export default [
+let routes = [
   {
     path: '/ndcs-content',
     label: 'NDC Content',
     activeId
-  },
-  FEATURE_NDCS_ENHANCEMENTS && {
-    path: '/ndcs-enhancements',
-    label: '2020 NDC Tracker',
-    activeId
-  },
-  FEATURE_NDCS_LTS && {
-    path: '/ndcs-lts',
-    label: 'Long-Term Strategy Tracker',
-    activeId
-  },
+  }
+]
+  
+FEATURE_NDCS_ENHANCEMENTS && routes.push({
+  path: '/ndcs-enhancements',
+  label: '2020 NDC Tracker',
+  activeId
+})
+FEATURE_NDCS_LTS && routes.push({
+  path: '/ndcs-lts',
+  label: 'Long-Term Strategy Tracker',
+  activeId
+})
+
+routes.push(
   {
     path: '/ndcs/compare/mitigation',
     label: 'NDC Comparison',
@@ -33,4 +37,6 @@ export default [
     label: 'NDC-SDG LINKAGES',
     activeId
   }
-];
+)
+
+export default routes;
