8bf2f046d4a6651cbab94139b6054b1a0780d433
Bluesmile82 <pursuance@gmail.com>
2019-12-03 13:39:04 +0100

    New LTS country page

--- app/javascript/app/data/SEO.js
+++ app/javascript/app/data/SEO.js
@@ -14,6 +14,8 @@ export const NDC_LTS =
   'Analyze and compare every national climate pledge (Nationally Determined Contribution – or NDC) under the Paris Agreement, with the ability to search key words and see summaries by topic across all.';
 export const NDC_COUNTRY = ({ countryName = '' }) =>
   `Explore the Commitments (NDCs) made by ${countryName} to act on climate change, as part of the Paris Agreement`;
+export const LTS_COUNTRY = ({ countryName = '' }) =>
+  `Explore the Long-term strategies (LTSs) made by ${countryName} to act on climate change`;
 export const NDC_SDG_LINKAGES =
   'Comprehensive mapping of linkages between Nationally Determined Contributions (NDCs) and the Sustainable Development Goals (SDGs) and associated targets of the 2030 Agenda for Sustainable Development.';
 export const HISTORICAL_GHG_EMISIONS =
