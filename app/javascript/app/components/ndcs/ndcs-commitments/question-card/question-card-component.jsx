700b5a5116d77206e2e0d0597d78c47790b56a12
Bluesmile82 <pursuance@gmail.com>
2020-01-02 20:11:39 +0100

    Fix error

--- app/javascript/app/components/ndcs/ndcs-commitments/question-card/question-card-component.jsx
+++ app/javascript/app/components/ndcs/ndcs-commitments/question-card/question-card-component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const QuestionCard = ({ link, slug, answerLabel, questionStats }) => {
-  const { answerNumber } = questionStats;
+  const { answerNumber } = questionStats || {};
   return (
     <div>
       {link}
