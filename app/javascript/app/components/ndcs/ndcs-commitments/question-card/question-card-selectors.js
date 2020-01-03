7fd1a7e380de196ab0dc69e81191de6064d668fc
Bluesmile82 <pursuance@gmail.com>
2020-01-03 13:05:57 +0100

    Add total parties number and fix answer number

--- app/javascript/app/components/ndcs/ndcs-commitments/question-card/question-card-selectors.js
+++ app/javascript/app/components/ndcs/ndcs-commitments/question-card/question-card-selectors.js
@@ -5,18 +5,21 @@ const getIndicators = state =>
 const getSlug = (state, { slug }) => slug || null;
 const getAnswerLabel = (state, { answerLabel }) => answerLabel || null;
 
+export const getTotalCountriesNumber = state =>
+  (state.countries && state.countries.data.length) || null;
+
 export const getQuestionStats = createSelector(
-  [getIndicators, getSlug, getAnswerLabel],
-  (indicators, slug, answerLabel) => {
+  [getIndicators, getSlug, getAnswerLabel, getTotalCountriesNumber],
+  (indicators, slug, answerLabel, maxPartiesNumber) => {
     if (!indicators || !slug || !answerLabel) return null;
     const indicator = indicators.find(i => i.slug === slug);
     if (!indicator) return null;
     const answerNumberLocations = Object.values(indicator.locations).filter(
-      l => l.answer === answerLabel
+      l => l.value === answerLabel
     );
     return {
       answerNumber: answerNumberLocations.length,
-      totalCountries: 197,
+      maxPartiesNumber,
       emissionPercentage: 82
     };
   }
