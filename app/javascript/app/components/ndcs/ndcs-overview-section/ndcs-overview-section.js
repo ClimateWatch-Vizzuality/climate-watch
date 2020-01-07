5ddf50f840b0060f71fc1fc488678ba104a4c103
Bluesmile82 <pursuance@gmail.com>
2020-01-07 20:16:28 +0100

    Move to hooks

--- app/javascript/app/components/ndcs/ndcs-overview-section/ndcs-overview-section.js
+++ app/javascript/app/components/ndcs/ndcs-overview-section/ndcs-overview-section.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import actions from 'pages/ndcs/ndcs-actions';
 import PropTypes from 'prop-types';
@@ -7,37 +7,35 @@ import { withRouter } from 'react-router';
 import Component from './ndcs-overview-section-component';
 import { commitmentsData } from './ndcs-overview-section-data';
 
-class NdcsOverviewSection extends PureComponent {
-  componentWillMount() {
-    this.props.fetchNDCS(true);
-  }
+const NdcsOverviewSection = props => {
+  useEffect(() => {
+    const { fetchNDCS } = props;
+    fetchNDCS(true);
+  }, []);
 
-  render() {
-    const { match, location } = this.props;
-    let { section } = this.props;
-    section = section || match.params.section;
-    if (!section) {
-      return commitmentsData.map((commitmentData, index) => (
-        <Component
-          data={commitmentData}
-          key={commitmentData.title}
-          section={index + 1}
-          location={location}
-        />
-      ));
-    }
-    return (
+  const { match, location } = props;
+  let { section } = props;
+  section = section || match.params.section;
+  if (!section) {
+    return commitmentsData.map((commitmentData, index) => (
       <Component
-        data={commitmentsData[section - 1]}
-        section={section}
+        data={commitmentData}
+        key={commitmentData.title}
+        section={index + 1}
         location={location}
       />
-    );
+    ));
   }
-}
+  return (
+    <Component
+      data={commitmentsData[section - 1]}
+      section={section}
+      location={location}
+    />
+  );
+};
 
 NdcsOverviewSection.propTypes = {
-  fetchNDCS: PropTypes.func.isRequired,
   section: PropTypes.number,
   location: PropTypes.object,
   match: PropTypes.object
