814711c12cd5a4973c776ba9683abd949a68c897
Bluesmile82 <pursuance@gmail.com>
2020-01-30 15:33:26 +0100

    Compress images to webp

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -9,6 +9,7 @@ const { sync } = require('glob');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 
 const extname = require('path-complete-extname');
 const { env, settings, output, loadersDir } = require('./configuration.js');
@@ -51,7 +52,8 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.JSCOV': JSON.stringify(false),
       'process.env.NODE_ENV': env.NODE_ENV
-    })
+    }),
+    new ImageminWebpWebpackPlugin()
   ],
 
   resolve: {
