10d05016a973b1f78001bbed7252e77d964f1316
Bluesmile82 <pursuance@gmail.com>
2020-01-31 11:35:14 +0100

    Use just one file as we have large points for all maps

--- app/javascript/app/data/world-50m-paths.js
+++ app/javascript/app/data/world-50m-paths.js
@@ -1,20 +1,10 @@
 import { feature } from 'topojson-client';
-import { PATH_LAYERS } from 'app/data/constants';
 
 import topojson from 'app/data/world-50m-topo.json';
-import pointsTopojson from 'app/data/world-50m-points.json';
 
 const paths = feature(
   topojson,
   topojson.objects[Object.keys(topojson.objects)[0]]
 ).features;
 
-const largerPointLayerPaths = feature(
-  pointsTopojson,
-  pointsTopojson.objects[Object.keys(pointsTopojson.objects)[0]]
-).features;
-
-export default [
-  ...paths.filter(p => p.properties.layer !== PATH_LAYERS.POINTS),
-  ...largerPointLayerPaths
-];
+export default paths;
