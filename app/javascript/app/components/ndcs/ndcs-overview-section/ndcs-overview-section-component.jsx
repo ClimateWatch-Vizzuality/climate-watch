78546e73c45b7a2281f4f0d6d40de6fab5671d97
Bluesmile82 <pursuance@gmail.com>
2020-02-03 18:38:57 +0100

    Update links, spacing  and add external link icon

--- app/javascript/app/components/ndcs/ndcs-overview-section/ndcs-overview-section-component.jsx
+++ app/javascript/app/components/ndcs/ndcs-overview-section/ndcs-overview-section-component.jsx
@@ -10,7 +10,6 @@ import QuestionCard from './question-card';
 const NdcsOverviewSection = ({ data, section, location, handleInfoClick }) => {
   const { title, description, hint, questions, color } = data;
   const isEmbed = isEmbededComponent(location);
-
   return (
     <div
       className={cx({
@@ -25,7 +24,13 @@ const NdcsOverviewSection = ({ data, section, location, handleInfoClick }) => {
                 <h1 className={styles.title}>{`${
                   isEmbed ? '' : `${section} `
                 }${title}`}</h1>
-                <p className={styles.description}>{description}</p>
+                <p
+                  className={cx(styles.description, {
+                    [styles.firstDescription]: parseInt(section, 10) === 1
+                  })}
+                >
+                  {description}
+                </p>
               </div>
               <p className={styles.hint}>{hint}</p>
             </div>
@@ -44,10 +49,10 @@ const NdcsOverviewSection = ({ data, section, location, handleInfoClick }) => {
                 metadataSlug={question.metadataSlug}
                 link={question.link}
                 color={color}
-                linkSlug={question.linkSlug}
                 questionText={question.questionText}
                 answerLabel={question.answerLabel}
                 handleInfoClick={handleInfoClick}
+                hasExternalLink={question.hasExternalLink}
               />
             ))}
           </div>
--- app/javascript/app/components/ndcs/ndcs-overview-section/question-card/question-card-component.jsx
+++ app/javascript/app/components/ndcs/ndcs-overview-section/question-card/question-card-component.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Progress from 'components/progress';
 import Icon from 'components/icon';
 import infoIcon from 'assets/icons/info.svg';
+import externalLinkIcon from 'assets/icons/external-link.svg';
 import ReactTooltip from 'react-tooltip';
 import Loading from 'components/loading';
 import styles from './question-card.scss';
@@ -12,6 +13,7 @@ const QuestionCard = ({
   link,
   questionText,
   questionStats,
+  hasExternalLink,
   color,
   handleInfoClick
 }) => {
@@ -33,9 +35,14 @@ const QuestionCard = ({
         className={styles.questionCardLink}
         href={link}
         target="_blank"
-        title="Paris agreement - Status of ratification"
+        title={questionText}
       >
-        <div className={styles.questionText}>{questionText}</div>
+        <div className={styles.questionText}>
+          {questionText}
+          {hasExternalLink && (
+            <Icon className={styles.externalLinkIcon} icon={externalLinkIcon} />
+          )}
+        </div>
         {answerNumber || answerNumber === 0 ? (
           <React.Fragment>
             <div className={styles.answerText}>
@@ -66,6 +73,7 @@ const QuestionCard = ({
 QuestionCard.propTypes = {
   questionText: PropTypes.string,
   link: PropTypes.string,
+  hasExternalLink: PropTypes.bool,
   metadataSlug: PropTypes.string,
   handleInfoClick: PropTypes.func.isRequired,
   color: PropTypes.string,
