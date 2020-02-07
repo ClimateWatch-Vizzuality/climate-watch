840d777f59dfe41c501632a248dd256de5639e06
Kamil Filip <kamil.filip94@gmail.com>
2020-02-07 17:04:06 +0000

    Add filters components to the Custom-compare page

--- app/javascript/app/pages/custom-compare/custom-compare-component.jsx
+++ app/javascript/app/pages/custom-compare/custom-compare-component.jsx
@@ -6,10 +6,46 @@ import cx from 'classnames';
 import Sticky from 'react-stickynode';
 import AnchorNav from 'components/anchor-nav';
 import BackButton from 'components/back-button';
+import Dropdown from 'components/dropdown';
 
 import anchorNavRegularTheme from 'styles/themes/anchor-nav/anchor-nav-regular.scss';
+
 import layout from 'styles/layout.scss';
 import styles from './custom-compare-styles.scss';
+
+const tempOptions = [
+  { label: 'Option 1', value: 'option1' },
+  { label: 'Option 2', value: 'option2' },
+  { label: 'Option 3', value: 'option3' },
+  { label: 'Option 4', value: 'option4' }
+];
+
+const Filter = ({ key }) => (
+  <div className={styles.filter}>
+    <Dropdown
+      key={`${key}-country`}
+      label=""
+      options={tempOptions}
+      onValueChange={() => {}}
+      value={tempOptions[0]}
+      hideResetButton
+      theme={{ dropdown: styles.dropdown }}
+      className={styles.dropdown}
+    />
+    <Dropdown
+      key={`${key}-submission`}
+      label=""
+      options={tempOptions}
+      onValueChange={() => {}}
+      value={tempOptions[0]}
+      hideResetButton
+    />
+  </div>
+);
+
+Filter.propTypes = {
+  key: PropTypes.string.isRequired
+};
 
 const CustomComparisonComponent = props => {
   const { route, anchorLinks } = props;
@@ -35,7 +71,18 @@ const CustomComparisonComponent = props => {
           />
         </Sticky>
       </Header>
-      <div className={styles.content}>CONTENT HERE</div>
+      <div className={styles.filtersWrapper}>
+        <div className={styles.content}>
+          <div className={styles.filters}>
+            <Filter key="1" />
+            <Filter key="2" />
+            <Filter key="3" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.accordions}>CONTENT HERE</div>
+      </div>
     </div>
   );
 };
