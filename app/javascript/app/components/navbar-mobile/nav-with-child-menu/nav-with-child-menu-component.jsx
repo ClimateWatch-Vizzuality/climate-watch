c06e6b3e73767821b4a3333737de91dbcfdcb720
Bluesmile82 <pursuance@gmail.com>
2020-02-14 14:53:58 +0100

    Add arrows, style and colons

--- app/javascript/app/components/navbar-mobile/nav-with-child-menu/nav-with-child-menu-component.jsx
+++ app/javascript/app/components/navbar-mobile/nav-with-child-menu/nav-with-child-menu-component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import Icon from 'components/icon';
 import cx from 'classnames';
+import arrow from 'assets/icons/arrow-down-tiny.svg';
 import styles from './nav-with-child-menu-styles.scss';
 
 const NavWithChildMenu = ({ title, options, closeMenu, theme }) => (
   <div className={cx(styles.container, theme.navWithChildContainer)}>
-    <div className={cx(styles.title, theme.title)}>{title}</div>
+    <div className={cx(styles.title, theme.title)}>{title}:</div>
     {options.map(option => (
       <NavLink
         key={option.label}
@@ -16,6 +18,7 @@ const NavWithChildMenu = ({ title, options, closeMenu, theme }) => (
         activeClassName={styles.active}
       >
         {option.label.toUpperCase()}
+        <Icon icon={arrow} className={cx(styles.arrowIcon, theme.arrowIcon)} />
       </NavLink>
     ))}
   </div>
