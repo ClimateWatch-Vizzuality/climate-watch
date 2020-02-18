cf85bab9f796e17cd7aa4954c9457df1ac167e8c
Simao Belchior <simaobelchior@gmail.com>
2020-02-18 08:50:11 +0000

    Use inheritance as methods are all very similar

--- app/controllers/api/v1/data/lts_content/data_sources_controller.rb
+++ app/controllers/api/v1/data/lts_content/data_sources_controller.rb
@@ -2,13 +2,11 @@ module Api
   module V1
     module Data
       module LtsContent
-        class DataSourcesController < Api::V1::Data::ApiController
-          def index
-            data_sources = ::Indc::Source.lts
-            render json: data_sources,
-                   adapter: :json,
-                   each_serializer: Api::V1::Data::NdcContent::DataSourceSerializer,
-                   root: :data
+        class DataSourcesController < Api::V1::Data::NdcContent::DataSourcesController
+          private
+
+          def data_sources
+            ::Indc::Source.lts
           end
         end
       end
