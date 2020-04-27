439e792820776c2bea0b9b38a3ab3bfb83f46d4a
Simao Belchior <simaobelchior@gmail.com>
2020-04-27 17:07:31 +0100

    use iso_code3 as the key and slugs in an array

--- app/serializers/api/v1/indc/countries_documents_serializer.rb
+++ app/serializers/api/v1/indc/countries_documents_serializer.rb
@@ -2,9 +2,18 @@ module Api
   module V1
     module Indc
       class CountriesDocumentsSerializer < ActiveModel::Serializer
-        has_many :data, serializer: CountryDocumentSerializer
+        attributes :documents, :laws, :policies, :data
 
-        attributes :documents, :laws, :policies
+        def data
+          result = object.data.map do |datum|
+            [datum.iso_code3,
+             ::Indc::Document.joins(values: :location).
+             where(locations: {iso_code3: datum.iso_code3}).
+             order(:ordering).distinct.to_a
+             ]
+          end.to_h
+          result
+        end
 
         def documents
           ::Indc::Document.order(:ordering)
