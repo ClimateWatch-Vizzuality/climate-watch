fc364742bc9f55f3b17013830d783fa672c87cc0
Bluesmile82 <pursuance@gmail.com>
2020-04-27 18:59:04 +0200

    Revert "Remove LTS tracker and redirect to LTS explore"
    This reverts commit e46a0ac8dbe37c3cc31b01f3a6f807af78867e5f.

---
+++ app/javascript/app/components/ndcs/ndcs-lts-table/ndcs-lts-table-component.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Search from 'components/search';
+import { Table } from 'cw-components';
+import NoContent from 'components/no-content';
+import Loading from 'components/loading';
+import styles from './ndcs-lts-table-styles.scss';
+
+const renderSearch = (searchHandler, query) => (
+  <Search
+    value={query}
+    onChange={searchHandler}
+    className={styles.searchBox}
+    placeholder="Search table data"
+  />
+);
+
+const NDCSLTSTable = ({
+  loading,
+  tableData,
+  query,
+  handleSearchChange,
+  noContentMsg,
+  columns,
+  setColumnWidth
+}) => (
+  <div>
+    <div className={styles.wrapper}>
+      {loading && <Loading light className={styles.loader} />}
+      {!loading && (
+        <div className={styles.filtersLayout}>
+          {renderSearch(handleSearchChange, query)}
+        </div>
+      )}
+      {!loading && tableData && tableData.length > 0 && (
+        <div className={styles.tableWrapper}>
+          <Table
+            data={tableData}
+            horizontalScroll
+            parseHtml
+            dynamicRowsHeight
+            setColumnWidth={setColumnWidth}
+            defaultColumns={columns}
+          />
+        </div>
+      )}
+      {!loading && (!tableData || tableData.length <= 0) && (
+        <NoContent className={styles.noContent} message={noContentMsg} />
+      )}
+    </div>
+  </div>
+);
+
+NDCSLTSTable.propTypes = {
+  loading: PropTypes.bool,
+  noContentMsg: PropTypes.string,
+  query: PropTypes.string,
+  tableData: PropTypes.array,
+  handleSearchChange: PropTypes.func.isRequired,
+  columns: PropTypes.array.isRequired,
+  setColumnWidth: PropTypes.func.isRequired
+};
+
+export default NDCSLTSTable;
