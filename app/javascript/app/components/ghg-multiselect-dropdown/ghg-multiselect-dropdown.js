f3584634f3cdd0b77401809efa424b36af6ab36f
javiabia <weberjavi@gmail.com>
2020-05-06 11:38:07 +0200

    Split container and component

--- app/javascript/app/components/ghg-multiselect-dropdown/ghg-multiselect-dropdown.js
+++ app/javascript/app/components/ghg-multiselect-dropdown/ghg-multiselect-dropdown.js
@@ -1,118 +1,32 @@
-import React, { useState } from 'react';
-import { useCombobox, useMultipleSelection } from 'downshift';
-import styles from './ghg-multiselect-dropdown-styles.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Component from './ghg-multiselect-dropdown-component';
 
-function isRegion(option) {
-  return option.groupId === 'regions';
-}
+const GhgMultiselectDropdownContainer = props => {
+  const { onSelectionChange } = props;
 
-function regionIncludesInputValue(option, inputValue) {
-  return (
-    isRegion(option) &&
-    option.expandsTo.find(country =>
-      country.label.toLowerCase().startsWith(inputValue.toLowerCase())
-    )
-  );
-}
+  const handleClearSelection = () => onSelectionChange([]);
 
-function countryIncludesInputValue(option, inputValue) {
-  return option.label.toLowerCase().startsWith(inputValue.toLowerCase());
-}
-
-const GhgMultiselectDropdow = ({
-  label,
-  options,
-  values,
-  onSelectionChange
-}) => {
-  const [inputValue, setInputValue] = useState('');
-  const {
-    // getSelectedItemProps,
-    getDropdownProps,
-    addSelectedItem,
-    // removeSelectedItem,
-    selectedItems
-  } = useMultipleSelection({ initialSelectedItems: [] });
-
-  const getFilteredItems = () =>
-    options.filter(
-      option =>
-        countryIncludesInputValue(option, inputValue) ||
-        regionIncludesInputValue(option, inputValue)
-    );
-
-  const {
-    isOpen,
-    // getToggleButtonProps,
-    // getLabelProps,
-    getMenuProps,
-    getInputProps,
-    getComboboxProps,
-    highlightedIndex,
-    getItemProps
-  } = useCombobox({
-    inputValue,
-    selectedItem: null,
-    items: getFilteredItems(),
-    onStateChange: ({ inputValue, type, selectedItem }) => {
-      switch (type) {
-        case useCombobox.stateChangeTypes.InputChange:
-          setInputValue(inputValue);
-          break;
-        case useCombobox.stateChangeTypes.InputKeyDownEnter:
-        case useCombobox.stateChangeTypes.ItemClick:
-        case useCombobox.stateChangeTypes.InputBlur:
-          if (selectedItem) {
-            setInputValue('');
-            addSelectedItem(selectedItem);
-            onSelectionChange(selectedItem);
-          }
-          break;
-        default:
-          break;
-      }
+  const handleSelectionUpdate = (sectedItems, clickedOption) => {
+    if (sectedItems.some(item => item.label === clickedOption.label)) {
+      onSelectionChange(
+        sectedItems.filter(v => v.label !== clickedOption.label)
+      );
+    } else {
+      onSelectionChange([...sectedItems, clickedOption]);
     }
-  });
-
+  };
   return (
-    <div className={styles.container}>
-      <div {...getComboboxProps()}>
-        <label className={styles.label}>
-          {label}
-          <input
-            className={styles.input}
-            {...getInputProps(getDropdownProps({ preventKeyAction: isOpen }))}
-            placeholder={selectedItems.length}
-            value={values.length && values[0].label}
-          />
-        </label>
-      </div>
-      <ul {...getMenuProps()}>
-        {isOpen &&
-          getFilteredItems(options).map(
-            (item, index) =>
-              item && (
-                <li
-                  style={
-                    highlightedIndex === index
-                      ? { backgroundColor: '#bde4ff' }
-                      : {}
-                  }
-                  key={item.label}
-                  {...getItemProps({ item, index })}
-                >
-                  <p>{item.label}</p>
-                  {isRegion(item) &&
-                    item.expandsTo.map(country => (
-                      <p key={country.iso}>{country.label}</p>
-                    ))}
-                  <div />
-                </li>
-              )
-          )}
-      </ul>
-    </div>
+    <Component
+      handleClearSelection={handleClearSelection}
+      handleSelectionUpdate={handleSelectionUpdate}
+      {...props}
+    />
   );
 };
 
-export default GhgMultiselectDropdow;
+GhgMultiselectDropdownContainer.propTypes = {
+  onSelectionChange: PropTypes.func
+};
+
+export default GhgMultiselectDropdownContainer;
