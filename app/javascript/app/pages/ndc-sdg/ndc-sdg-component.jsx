ab81c5c02fda0b8e7f1b6a62b0162d82b411544c
javiabia <weberjavi@gmail.com>
2020-05-11 15:58:31 +0200

    Update button on ndcs-sdgs

--- app/javascript/app/pages/ndc-sdg/ndc-sdg-component.jsx
+++ app/javascript/app/pages/ndc-sdg/ndc-sdg-component.jsx
@@ -8,6 +8,7 @@ import Header from 'components/header';
 import Intro from 'components/intro';
 import AutocompleteSearch from 'components/autocomplete-search';
 import NdcSdgLinkagesContent from 'components/ndc-sdg/ndc-sdg-linkages-content';
+import ModalShare from 'components/modal-share';
 import { NDC_SDG_LINKAGES } from 'data/SEO';
 import { MetaDescription, SocialMetadata } from 'components/seo';
 import { isPageContained } from 'utils/navigation';
@@ -52,6 +53,7 @@ class NdcSdg extends PureComponent {
         >
           <NdcSdgLinkagesContent />
         </div>
+        <ModalShare analyticsName={'Ndcs-Sdgs'} />
       </div>
     );
   }
