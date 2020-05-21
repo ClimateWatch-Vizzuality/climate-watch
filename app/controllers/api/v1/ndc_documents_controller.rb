d853d9bfeff47c3c1843cbf9a17d0e0365b3034a
Simao Belchior <simaobelchior@gmail.com>
2020-05-21 12:11:12 +0100

    WIP: starting to fetch data from LSE

--- app/controllers/api/v1/ndc_documents_controller.rb
+++ app/controllers/api/v1/ndc_documents_controller.rb
@@ -1,11 +1,15 @@
 module Api
   module V1
-    CountriesDocuments = Struct.new(:data) do
+    CountriesDocuments = Struct.new(:data, :laws_info) do
       alias_method :read_attribute_for_serialization, :send
     end
     class NdcDocumentsController < ApiController
+      LSE_API = 'https://climate-laws.org/cclow/api/targets'.freeze
+
       def index
-        render json: CountriesDocuments.new(set_locations(params)),
+        laws_info = SingleRecordFetcher.new(LSE_API, 'laws-info').call
+
+        render json: CountriesDocuments.new(set_locations(params), laws_info),
                serializer: Api::V1::Indc::CountriesDocumentsSerializer
       end
 
