f2a90d5cb763ee16d446e024f3dd050564f5700b
Simao Belchior <simaobelchior@gmail.com>
2020-05-26 10:05:44 +0100

    Integrates information about presence of targets in laws and policies

--- app/serializers/api/v1/indc/countries_documents_serializer.rb
+++ app/serializers/api/v1/indc/countries_documents_serializer.rb
@@ -6,13 +6,29 @@ module Api
 
         def data
           object.data.map do |datum|
-            [datum.iso_code3,
-             ::Indc::Document.joins(values: :location).
+            docs = ::Indc::Document.joins(values: :location).
                joins('LEFT OUTER JOIN indc_submissions ON indc_submissions.document_id = indc_documents.id AND indc_submissions.location_id = locations.id').
                where(locations: {iso_code3: datum.iso_code3}).
                select('indc_documents.*, indc_submissions.submission_date').
                order(:ordering).distinct.to_a
-             ]
+
+            if object.laws_info[datum.iso_code3]
+              ordering = docs.empty? ? 0 : docs.last["ordering"]
+              docs += object.laws_info[datum.iso_code3].map do |key, val|
+                next unless val
+                ordering += 1
+                {
+                  id: nil,
+                  ordering: ordering,
+                  slug: key.gsub('in_', ''),
+                  long_name: key.gsub('in_', '').titleize,
+                  description: "Targets in #{key.gsub('in_', '').titleize}",
+                  is_ndc: false
+                }
+              end.compact
+            end
+
+            [datum.iso_code3, docs]
           end.to_h
         end
 
