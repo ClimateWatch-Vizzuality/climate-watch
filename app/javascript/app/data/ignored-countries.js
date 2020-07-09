a5d81b05c3405e98cdf177c553d6864650ae4675
Bluesmile82 <pursuance@gmail.com>
2020-07-09 15:22:09 +0200

    Add greenland to ignored countries

--- app/javascript/app/data/ignored-countries.js
+++ app/javascript/app/data/ignored-countries.js
@@ -1,2 +1,2 @@
 // We won't show Taiwan and Western Sahara as an independent country
-export const IGNORED_COUNTRIES_ISOS = ['TWN', 'ESH'];
+export const IGNORED_COUNTRIES_ISOS = ['TWN', 'ESH', 'GRL'];
