3a70daeb6fb9f62d4be3db996e6e80c57f1ecf96
Simao Belchior <simaobelchior@gmail.com>
2020-08-04 17:12:30 +0100

    remove unnecessary methods

--- app/serializers/api/v1/indc/value_serializer.rb
+++ app/serializers/api/v1/indc/value_serializer.rb
@@ -7,14 +7,6 @@ module Api
         attribute :label_slug, if: -> { object.label_slug }
         attribute :sector_id, if: -> { object.sector_id }
         attribute :document_slug
-
-        def label_slug
-          object.label_slug
-        end
-
-        def document_slug
-          object.document_slug
-        end
       end
     end
   end
