ac5561d28ae45057eeaa5994b13a16d2c766062e
Simao Belchior <simaobelchior@gmail.com>
2020-08-04 17:04:12 +0100

    fetch attributes on select instead of throw Active record

--- app/serializers/api/v1/indc/value_serializer.rb
+++ app/serializers/api/v1/indc/value_serializer.rb
@@ -4,16 +4,16 @@ module Api
       class ValueSerializer < ActiveModel::Serializer
         attribute :value
         attribute :label_id, if: -> { object.label_id }
-        attribute :label_slug, if: -> { object.label&.slug }
+        attribute :label_slug, if: -> { object.label_slug }
         attribute :sector_id, if: -> { object.sector_id }
         attribute :document_slug
 
         def label_slug
-          object.label&.slug
+          object.label_slug
         end
 
         def document_slug
-          object.document&.slug
+          object.document_slug
         end
       end
     end
