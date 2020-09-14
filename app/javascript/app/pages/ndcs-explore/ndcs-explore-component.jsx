ffaa7538dc4a44522a4a5adb5d5c073e016e15ca
Bluesmile82 <pursuance@gmail.com>
2020-09-14 13:01:31 +0200

    Remove header from contained NDC explorer

--- app/javascript/app/pages/ndcs-explore/ndcs-explore-component.jsx
+++ app/javascript/app/pages/ndcs-explore/ndcs-explore-component.jsx
@@ -9,6 +9,7 @@ import { MetaDescription, SocialMetadata } from 'components/seo';
 import AutocompleteSearch from 'components/autocomplete-search';
 import ModalMetadata from 'components/modal-metadata';
 import ModalShare from 'components/modal-share';
+import { isPageContained } from 'utils/navigation';
 
 import layout from 'styles/layout.scss';
 import styles from './ndcs-explore-styles.scss';
@@ -20,28 +21,30 @@ const NDCSExplore = ({ route }) => (
       subtitle="NDCS CONTENT"
     />
     <SocialMetadata descriptionContext={NDCS_EXPLORE} href={location.href} />
-    <Header route={route}>
-      <div className={layout.content}>
-        <div className="grid-column-item">
-          <div className={styles.headerLayout}>
-            <Intro
-              title="Explore Nationally Determined Contributions (NDCs)"
-              description="Under the Paris Agreement, nearly every nation made a commitment to tackle climate change and strengthen their efforts over time. Explore the content of these Nationally Determined Contributions (NDCs) by searching for key terms. You can analyze and compare NDCs using over 150 structured indicators."
-            />
-          </div>
-          <div className={styles.searchWrapper}>
-            <Button
-              variant="primary"
-              link="/2020-ndc-tracker"
-              className="link-button"
-            >
-              Go to 2020 NDC Tracker
-            </Button>
-            <AutocompleteSearch placeholder="Search NDCs for a keyword or phrase (e.g., ‘forest’ or ‘CO2’)" />
+    {!isPageContained && (
+      <Header route={route}>
+        <div className={layout.content}>
+          <div className="grid-column-item">
+            <div className={styles.headerLayout}>
+              <Intro
+                title="Explore Nationally Determined Contributions (NDCs)"
+                description="Under the Paris Agreement, nearly every nation made a commitment to tackle climate change and strengthen their efforts over time. Explore the content of these Nationally Determined Contributions (NDCs) by searching for key terms. You can analyze and compare NDCs using over 150 structured indicators."
+              />
+            </div>
+            <div className={styles.searchWrapper}>
+              <Button
+                variant="primary"
+                link="/2020-ndc-tracker"
+                className="link-button"
+              >
+                Go to 2020 NDC Tracker
+              </Button>
+              <AutocompleteSearch placeholder="Search NDCs for a keyword or phrase (e.g., ‘forest’ or ‘CO2’)" />
+            </div>
           </div>
         </div>
-      </div>
-    </Header>
+      </Header>
+    )}
     <div className={styles.wrapper}>
       {route.sections &&
         route.sections.length > 0 &&
