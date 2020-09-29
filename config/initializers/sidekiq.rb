3ba840cc3939183adf406a76d3db7cc5ed5c90ea
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2020-09-29 12:43:19 +0200

    Fixed reference to env var

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: REDIS_SERVER, namespace: "climate_watch_#{Rails.env}" }
+  config.redis = { url: ENV['REDIS_SERVER'], namespace: "climate_watch_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: REDIS_SERVER, namespace: "climate_watch_#{Rails.env}" }
+  config.redis = { url: ENV['REDIS_SERVER'], namespace: "climate_watch_#{Rails.env}" }
 end
