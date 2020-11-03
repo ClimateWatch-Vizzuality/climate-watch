ec2c1e47ce7b48ba4d83a197c3ea79974b683d69
Rui Jiang <rjiang.bb@gmail.com>
2020-11-03 13:03:58 -0500

    Started on provider.

---
+++ app/javascript/app/providers/key-visualizations-provider/key-visualizations-actions.js
@@ -0,0 +1,32 @@
+import { createAction } from 'redux-actions';
+import { createThunkAction } from 'utils/redux';
+
+const getKeyVisualizationsInit = createAction('getKeyVisualizationsInit');
+const getKeyVisualizationsFail = createAction('getKeyVisualizationsFail');
+const getKeyVisualizationsReady = createAction('getKeyVisualizationsReady');
+const getKeyVisualizations = createThunkAction(
+  'getKeyVisualizations',
+  () => dispatch => {
+    dispatch(getKeyVisualizationsInit());
+
+    fetch('/api/v1/key_visualizations')
+      .then(response => {
+        if (response.ok) return response.json();
+        throw Error(response.statusText);
+      })
+      .then(data => {
+        dispatch(getKeyVisualizationsReady(data));
+      })
+      .catch(error => {
+        console.warn(error);
+        dispatch(getKeyVisualizationsFail());
+      });
+  }
+);
+
+export default {
+  getKeyVisualizationsInit,
+  getKeyVisualizationsFail,
+  getKeyVisualizationsReady,
+  getKeyVisualizations
+};
