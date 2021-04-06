87b4cf3a271b96cd7848ac035afff374006c9fe8
Alvaro Leal <pursuance@gmail.com>
2021-04-06 17:18:54 +0200

    Correct calculation of world and test

--- app/javascript/app/components/ghg-emissions/ghg-emissions-selectors/ghg-emissions-selectors.test.js
+++
@@ -1,8 +0,0 @@
-import { getData } from './ghg-emissions-selectors-get';
-
-describe('Get selectors', () => {
-  const state = { emissions: { data: 1 } };
-  it('Gets data', () => {
-    expect(getData(state)).toBe(1);
-  });
-});
