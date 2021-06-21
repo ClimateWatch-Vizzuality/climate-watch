b7baec29ba673424c70a8cd260c81a2874299559
Alvaro Leal <pursuance@gmail.com>
2021-06-21 16:27:43 +0200

    Use meta instead of endpoint

--- app/javascript/app/data/world-50m-paths.js
+++ app/javascript/app/data/world-50m-paths.js
@@ -1,32 +1,25 @@
 import { feature } from 'topojson-client';
-import { createSelector } from 'reselect';
 
 import topojsonWorld from 'app/data/world-50m-topo.json';
 import topojsonChina from /* webpackChunkName: "world-geo-china" */ 'app/data/world-50m-china-topo.json';
 import topojsonIndia from /* webpackChunkName: "world-geo-india" */ 'app/data/world-50m-india-topo.json';
 
 const countryTopojsons = {
-  china: topojsonChina,
-  india: topojsonIndia
+  CH: topojsonChina,
+  IN: topojsonIndia
 };
 
 const getPaths = topojson =>
   feature(topojson, topojson.objects[Object.keys(topojson.objects)[0]])
     .features;
 
-const getIpCountry = state => state.ipCountry && state.ipCountry.data;
-const getIpCountryLoading = state => state.ipCountry && state.ipCountry.loading;
-const getIpCountryError = state => state.ipCountry && state.ipCountry.error;
-
-const getIPPaths = createSelector(
-  [getIpCountryLoading, getIpCountry, getIpCountryError],
-  (loading, ipCountry, error) => {
-    if (loading && !error) return null;
-    if (!ipCountry || !countryTopojsons[ipCountry]) {
-      return getPaths(topojsonWorld);
-    }
-    return getPaths(countryTopojsons[ipCountry]);
+const getIPPaths = () => {
+  const ipCountry = document.querySelector('meta[name="cw-user-country"]')
+    .content;
+  if (!ipCountry || !countryTopojsons[ipCountry]) {
+    return getPaths(topojsonWorld);
   }
-);
+  return getPaths(countryTopojsons[ipCountry]);
+};
 
 export default getIPPaths;
