18d4f3f471f38c8a63197691dbdcfeb8904aae94
Tomasz Subik <tsubik@gmail.com>
2021-07-15 16:57:13 +0200

    better spec for generate and upload zip files

--- spec/services/import_zip_files_spec.rb
+++ spec/services/import_zip_files_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+# rubocop:disable LineLength
 
 object_contents = {
   "#{CW_FILES_PREFIX}zip_files/zip_files.csv" => <<~END,
@@ -9,6 +10,8 @@ object_contents = {
 "NDC CONTENT","ClimateWatch_NDC_Content.zip","NDC_WB
 NDC_DIE
 NDC_CW","indc","NDC_data.csv","CW_NDC_data_highlevel.csv"
+"LTS CONTENT","ClimateWatch_LTS.zip","NDC_LTS","indc","NDC_LTS_data.csv","CW_LTS_data_highlevel.csv"
+"LTS CONTENT","ClimateWatch_LTS.zip",,"indc","NDC_LTS_data_sectoral.csv","CW_LTS_data_sector.csv"
   END
   "#{CW_FILES_PREFIX}indc/NDC_metadata.csv" => <<~END,
     global_category,overview_category,map_category,column_name,long_name,Definition,Source,multiple_version
@@ -24,8 +27,12 @@ NDC_CW","indc","NDC_data.csv","CW_NDC_data_highlevel.csv"
     Afghanistan,AFG,First NDC Submitted,First NDC Submitted,11/23/2016
   END
   "#{CW_FILES_PREFIX}wri_metadata/metadata_sources.csv" => <<~END,
-    dataset,title,subtitle
-    historical_emissions_CAIT,CAIT Historical Emissions,CAIT Historical Emissions
+    ﻿dataset,title
+    2020_NDC,title
+    NDC_WB,title
+    NDC_DIE,title
+    NDC_CW,title
+    NDC_LTS,title
   END
 }
 
@@ -54,10 +61,11 @@ RSpec.describe ImportZIPFiles do
   end
 
   it 'Creates new zip files records' do
-    expect { subject }.to change { ZipFile.count }.by(2)
+    expect { subject }.to change { ZipFile.count }.by(3)
 
     z1 = ZipFile.find_by(dropdown_title: 'ALL DATA')
     z2 = ZipFile.find_by(dropdown_title: 'NDC CONTENT')
+    z3 = ZipFile.find_by(dropdown_title: 'LTS CONTENT')
 
     expect(z1.zip_filename).to eq('ClimateWatch_AllData.zip')
     expect(z1.metadata).to eq([])
@@ -70,6 +78,14 @@ RSpec.describe ImportZIPFiles do
         {s3_folder: 'indc', filename_original: 'NDC_metadata.csv', filename_zip: 'CW_NDC_metadata.csv'},
         {s3_folder: 'indc', filename_original: 'NDC_single_version.csv', filename_zip: 'CW_NDC_tracker.csv'},
         {s3_folder: 'indc', filename_original: 'NDC_data.csv', filename_zip: 'CW_NDC_data_highlevel.csv'}
+      ]
+    )
+    expect(z3.zip_filename).to eq('ClimateWatch_LTS.zip')
+    expect(z3.metadata).to eq(%w(NDC_LTS))
+    expect(z3.files.map(&:symbolize_keys)).to eq(
+      [
+        {s3_folder: 'indc', filename_original: 'NDC_LTS_data.csv', filename_zip: 'CW_LTS_data_highlevel.csv'},
+        {s3_folder: 'indc', filename_original: 'NDC_LTS_data_sectoral.csv', filename_zip: 'CW_LTS_data_sector.csv'}
       ]
     )
   end
@@ -85,7 +101,10 @@ RSpec.describe ImportZIPFiles do
       Aws.config[:s3] = Aws.config[:s3].deep_merge(
         stub_responses: {
           put_object: lambda { |context|
-            uploaded_files << context.params[:body]
+            uploaded_files << {
+              name: File.basename(context.params[:body].to_path),
+              content: File.read(context.params[:body])
+            }
             {etag: 'etag'}
           }
         }
@@ -93,10 +112,28 @@ RSpec.describe ImportZIPFiles do
 
       subject
 
-      uploaded_file_names = uploaded_files.map(&:to_path).map { |p| File.basename(p) }
+      expect(uploaded_files.map { |f| f[:name] }).to eq(['ClimateWatch_NDC_Content.zip', 'ClimateWatch_LTS.zip', 'ClimateWatch_AllData.zip'])
 
-      expect(uploaded_files.count).to eq(2)
-      expect(uploaded_file_names).to eq(['ClimateWatch_NDC_Content.zip', 'ClimateWatch_AllData.zip'])
+      all_data_file = uploaded_files.find { |f| f[:name].include?('AllData.zip') }
+
+      Zip::File.open_buffer(all_data_file[:content]) do |zipfile|
+        expect(zipfile.entries.map(&:name)).to eq(['ClimateWatch_NDC_Content.zip', 'ClimateWatch_LTS.zip'])
+      end
+
+      ndc_content_file = uploaded_files.find { |f| f[:name].include?('NDC_Content.zip') }
+
+      Zip::File.open_buffer(ndc_content_file[:content]) do |zipfile|
+        expect(zipfile.entries.map(&:name)).to eq(['CW_NDC_metadata.csv', 'CW_NDC_tracker.csv', 'CW_NDC_data_highlevel.csv', 'metadata.csv'])
+        metadata_file = zipfile.entries.find { |f| f.name == 'metadata.csv' }
+        metadata_csv_content = metadata_file.get_input_stream.read
+        saved_metadata = []
+        CSV.parse(metadata_csv_content, headers: true).each do |row|
+          saved_metadata << row['dataset']
+        end
+        expect(saved_metadata).to eq(%w(2020_NDC NDC_WB NDC_DIE NDC_CW))
+      end
     end
   end
 end
+
+# rubocop:enable LineLength
