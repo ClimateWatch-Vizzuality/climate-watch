0c7f866dab3bf629b34a22b6f3b16524aba257ed
Tiago Garcia <tiagojsag@gmail.com>
2021-09-01 14:15:15 +0200

    Try to make EB happy

--- infrastructure/modules/eb_rails/main.tf
+++ infrastructure/modules/eb_rails/main.tf
@@ -25,6 +25,49 @@ resource "aws_iam_role" "ec2_profile_role" {
 EOF
 }
 
+//- name: POSTGRES_URL
+//valueFrom:
+//secretKeyRef:
+//key: CW_POSTGRES_URL
+//name: dbsecrets
+//- name: SHARED_POSTGRES_URL
+//valueFrom:
+//secretKeyRef:
+//key: CW_SHARED_POSTGRES_URL
+//name: dbsecrets
+//- name: AWS_ACCESS_KEY_ID
+//valueFrom:
+//secretKeyRef:
+//key: CW_PLATFORMS_S3_ACCESS_KEY
+//name: appsecrets
+//- name: AWS_SECRET_ACCESS_KEY
+//valueFrom:
+//secretKeyRef:
+//key: CW_PLATFORMS_S3_SECRET_ACCESS_KEY
+//name: appsecrets
+
+
+# Secrets for env vars
+resource "aws_secretsmanager_secret" "appsignal_api_key" {
+  name        = "AppSignal API key"
+}
+
+resource "aws_secretsmanager_secret_version" "appsignal_api_key" {
+  secret_id = aws_secretsmanager_secret.appsignal_api_key.id
+  secret_string = var.appsignal_api_key
+}
+
+resource "aws_secretsmanager_secret" "appsignal_api_key" {
+  name        = "AppSignal API key"
+}
+
+resource "aws_secretsmanager_secret_version" "appsignal_api_key" {
+  secret_id = aws_secretsmanager_secret.appsignal_api_key.id
+  secret_string = var.appsignal_api_key
+}
+
+
+
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "ec2_profile_role"
   role = aws_iam_role.ec2_profile_role.name
@@ -35,6 +78,74 @@ resource "aws_elastic_beanstalk_environment" "eb_app_production" {
   application         = aws_elastic_beanstalk_application.eb_app.name
   solution_stack_name = "64bit Amazon Linux 2018.03 v2.12.10 running Ruby 2.5 (Puma)"
 
+  # Actual application env vars
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "REDIS_SERVER"
+    value     = "redis://localhost:6379"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "CORS_WHITELIST"
+    value     = "*"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "GFW_API"
+    value     = "https://production-api.globalforestwatch.org"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "ESP_API"
+    value     = "https://data.emissionspathways.org/api/v1"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "AWS_REGION"
+    value     = "eu-west-1"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "S3_BUCKET_NAME"
+    value     = "wri-sites"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "RAILS_LOG_TO_STDOUT"
+    value     = "true"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "PASSWORD_PROTECT"
+    value     = "false"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "RAILS_SERVE_STATIC_FILES"
+    value     = "true"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "PORT"
+    value     = 3000
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "CW_FILES_PREFIX"
+    value     = "climatewatch.org/www.climatewatch.org/climate-watch/"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "RAILS_ENV"
+    value     = "production"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "RACK_ENV"
+    value     = "production"
+  }
+
+  # Beanstalk settings
   setting {
     namespace = "aws:ec2:vpc"
     name      = "VPCId"
--- infrastructure/modules/eb_rails/variables.tf
+++ infrastructure/modules/eb_rails/variables.tf
@@ -10,3 +10,8 @@ variable "public_subnets" {}
 variable "private_subnets" {}
 
 variable "key_pair" {}
+
+variable "appsignal_api_key" {
+  type = string
+  sensitive   = true
+}
