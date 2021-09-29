6d9ca946db1addf4f956bdbf164a4592d20ebe43
Tiago Garcia <tiagojsag@gmail.com>
2021-09-29 14:00:39 +0200

    Set correct branches on deploy config; fix bug on staging lb sg

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ server '44.198.104.48', user: 'ubuntu', roles: %w{web app db}, primary: true
 
 set :ssh_options, forward_agent: true
 
-set :branch, 'feature/infrastructure-migration'
+set :branch, 'master'
 
 set :default_env, {
 }
