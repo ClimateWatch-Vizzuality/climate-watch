ec907d522a00f41e463b81c6b27843a7b0ae8c40
Tiago Garcia <tiagojsag@gmail.com>
2021-09-30 13:23:10 +0200

    try to make webpacker happy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ server '18.215.21.86', user: 'ubuntu', roles: %w{web app db}, primary: true
 
 set :ssh_options, forward_agent: true
 
-set :branch, 'sandbox'
+set :branch, 'fix/webpacker_deploy_issue'
 
 set :default_env, {
   'RAILS_ENV' => 'staging',
