29d70d35c356b651ab1285d3ea2e773dc24713aa
Tiago Garcia <tiagojsag@gmail.com>
2021-10-09 07:43:59 +0200

    Finalize infrastructure config

--- infrastructure/modules/lb/main.tf
+++ infrastructure/modules/lb/main.tf
@@ -20,6 +20,15 @@ resource "aws_security_group_rule" "lb_access_http_ingress" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
+resource "aws_security_group_rule" "lb_access_https_ingress" {
+  type              = "ingress"
+  from_port         = "443"
+  to_port           = "443"
+  protocol          = "TCP"
+  security_group_id = aws_security_group.lb_access_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "lb_access_all_egress" {
   type              = "egress"
   from_port         = "0"
@@ -45,6 +54,24 @@ resource "aws_lb_listener" "site_lb_listener" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+      host        = "www.climatewatchdata.org"
+    }
+  }
+}
+
+resource "aws_lb_listener" "site_lb_listener_https" {
+  load_balancer_arn = aws_alb.site_lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.domain_cert.arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.site_lb_tg.arn
   }
@@ -63,10 +90,10 @@ resource "aws_lb_target_group_attachment" "site_lb_target_group_attachment" {
   port             = 80
 }
 
-#resource "aws_alb_listener_certificate" "alb_listener_certificate" {
-#  listener_arn = aws_lb_listener.site_lb_listener.arn
-#  certificate_arn = aws_acm_certificate_validation.domain_cert_validation.certificate_arn
-#}
+resource "aws_alb_listener_certificate" "alb_listener_certificate" {
+  listener_arn = aws_lb_listener.site_lb_listener_https.arn
+  certificate_arn = aws_acm_certificate_validation.domain_cert_validation.certificate_arn
+}
 
 resource "aws_acm_certificate" "domain_cert" {
   domain_name       = var.domain
@@ -78,6 +105,6 @@ resource "aws_acm_certificate" "domain_cert" {
   }
 }
 
-#resource "aws_acm_certificate_validation" "domain_cert_validation" {
-#  certificate_arn = aws_acm_certificate.domain_cert.arn
-#}
+resource "aws_acm_certificate_validation" "domain_cert_validation" {
+  certificate_arn = aws_acm_certificate.domain_cert.arn
+}
--- infrastructure/modules/lb/outputs.tf
+++ infrastructure/modules/lb/outputs.tf
@@ -2,8 +2,12 @@ output "lb_security_group_id" {
   value = aws_security_group.lb_access_sg.id
 }
 
-output "lb_listener_arn" {
+output "lb_listener_http_arn" {
   value = aws_lb_listener.site_lb_listener.arn
+}
+
+output "lb_listener_https_arn" {
+  value = aws_lb_listener.site_lb_listener_https.arn
 }
 
 output "lb_dns" {
