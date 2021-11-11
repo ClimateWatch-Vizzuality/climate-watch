c7221e8258710f4b8141f5cfb830da10ec58c63a
Alvaro leal <pursuance@gmail.com>
2021-11-11 14:12:07 +0100

    Add feature tags and description text

--- app/javascript/app/pages/country/country.js
+++ app/javascript/app/pages/country/country.js
@@ -5,10 +5,14 @@ import { actions } from 'components/anchor-nav';
 import {
   getCountryName,
   getDescriptionText,
-  getAnchorLinks
+  getLegacyDescriptionText,
+  getAnchorLinks,
+  getEmissionProviderFilters
 } from './country-selectors';
 
 import Component from './country-component';
+
+const FEATURE_COUNTRY_CHANGES = process.env.FEATURE_COUNTRY_CHANGES === 'true';
 
 const mapStateToProps = (state, { location, match, route }) => {
   const iso = match.params.iso;
@@ -16,7 +20,8 @@ const mapStateToProps = (state, { location, match, route }) => {
   const stateWithIso = {
     iso,
     countries: state.countries,
-    socioeconomics: data ? data[iso] : {}
+    socioeconomics: data ? data[iso] : {},
+    ...state
   };
 
   const routeData = {
@@ -30,8 +35,11 @@ const mapStateToProps = (state, { location, match, route }) => {
       iso,
       name: getCountryName(stateWithIso)
     },
-    description: getDescriptionText(stateWithIso),
-    anchorLinks: getAnchorLinks(routeData)
+    description: FEATURE_COUNTRY_CHANGES
+      ? getDescriptionText(stateWithIso)
+      : getLegacyDescriptionText(stateWithIso),
+    anchorLinks: getAnchorLinks(routeData),
+    emissionProviderFilters: getEmissionProviderFilters(stateWithIso)
   };
 };
 
