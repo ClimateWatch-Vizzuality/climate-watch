100e227599a39863723abd91e45da72466bc0158
Tomasz Subik <tsubik@gmail.com>
2021-11-12 13:38:20 +0100

    improve wb extra importer

--- app/services/import_wb_extra.rb
+++ app/services/import_wb_extra.rb
@@ -1,13 +1,20 @@
 class ImportWbExtra
+  include ClimateWatchEngine::CSVImporter
+
   POPULATION_FILEPATH = "#{CW_FILES_PREFIX}wb_extra/population.csv".freeze
   GDP_FILEPATH = "#{CW_FILES_PREFIX}wb_extra/gdp.csv".freeze
-  FIRST_YEAR = 1960
+  US_POPULATION_FILEPATH = "#{CW_FILES_PREFIX}wb_extra/us_population.csv".freeze
+  US_GDP_FILEPATH = "#{CW_FILES_PREFIX}wb_extra/us_gdp.csv".freeze
+
   def call
-    cleanup
-    year_range
-    load_csvs
-    read_data
-    import_data
+    prepare_cache
+
+    ActiveRecord::Base.transaction do
+      cleanup
+      load_csvs
+      read_data
+      import_data
+    end
   end
 
   private
@@ -16,63 +23,86 @@ class ImportWbExtra
     WbExtra::CountryData.delete_all
   end
 
-  def year_range
-    current_year = DateTime.now.year
-    @year_range = (FIRST_YEAR..current_year)
+  def prepare_cache
+    @locations = Location.all.map { |l| [l.iso_code3, l] }.to_h
   end
 
   def load_csvs
     @population = S3CSVReader.read(POPULATION_FILEPATH).map(&:to_h)
+    @us_population = S3CSVReader.read(US_POPULATION_FILEPATH).map(&:to_h)
     @gdp = S3CSVReader.read(GDP_FILEPATH).map(&:to_h)
+    @us_gdp = S3CSVReader.read(US_GDP_FILEPATH).map(&:to_h)
   end
 
   def read_data
-    @population_by_country = parse_data(@population)
-    @gdp_by_country = parse_data(@gdp)
+    @population_by_location = {}
+    @gdp_by_location = {}
+
+    @population_by_location.merge!(parse_data(@population))
+    @population_by_location.merge!(parse_data(@us_population))
+    @gdp_by_location.merge!(parse_data(@gdp))
+    @gdp_by_location.merge!(parse_data(@us_gdp))
+    @year_range = [
+      @population_by_location.values.map(&:keys),
+      @gdp_by_location.values.map(&:keys)
+    ].flatten.uniq
   end
 
   def import_data
-    all_countries = Location.all
-    not_included_countries = []
+    all_locations = Location.all
+    not_included_locations = []
+    location_data = []
 
-    all_countries.each do |country|
-      country_code = country.iso_code3
+    all_locations.each do |location|
+      location_code = location.iso_code3
 
-      @year_range.map do |year|
-        if @population_by_country[country_code]
-          create_country_data(country_code, year)
-        else
-          not_included_countries << country_code
+      if @population_by_location[location_code]
+        @year_range.each do |year|
+          location_data << build_location_data(location_code, year)
         end
+      else
+        not_included_locations << location_code
       end
     end
-    Rails.logger.info "Countries not included in the data
-      #{not_included_countries.uniq}"
+
+    WbExtra::CountryData.import! location_data
+
+    Rails.logger.info "Locations not included in the data
+      #{not_included_locations.uniq}"
   end
 
-  def create_country_data(country_code, year)
-    year_index = (year - FIRST_YEAR)
-    country_location = Location.find_by(
-      iso_code3: country_code
-    )
-
-    WbExtra::CountryData.create(
-      location: country_location,
+  def build_location_data(location_code, year)
+    WbExtra::CountryData.new(
+      location: find_location(location_code),
       year: year,
-      population: @population_by_country[country_code][year_index]&.to_f,
-      gdp: @gdp_by_country[country_code][year_index]&.to_f
+      population: @population_by_location.dig(location_code, year)&.to_f,
+      gdp: @gdp_by_location.dig(location_code, year)&.to_f
     )
   end
 
   def parse_data(csv_data)
     parsed_data = {}
-    csv_data.each do |country_data|
-      country_code = country_data[:country_code]
-      parsed_data[country_code] = @year_range.map do |year|
-        year_sym = year.to_s.to_sym
-        country_data[year_sym] == 'null' ? nil : country_data[year_sym]
+    csv_data.each do |row|
+      location_code = row[:country_code] || row[:state]
+      parse_years(row).each do |year, value|
+        parsed_data[location_code] ||= {}
+        parsed_data[location_code][year] = value == 'null' ? nil : value
       end
     end
     parsed_data
+  end
+
+  def find_location(iso)
+    return if iso.blank?
+
+    @locations[iso] or raise ActiveRecord::RecordNotFound, "Cannot find location with iso code #{iso}"
+  end
+
+  def parse_years(row)
+    row.keys.grep(/\d{4}/).reduce({}) do |acc, year|
+      next acc unless row[year].present?
+
+      acc.merge(year.to_s.to_i => row[year].to_f)
+    end
   end
 end
