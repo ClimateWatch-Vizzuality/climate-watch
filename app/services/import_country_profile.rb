fe80afad561b8ed3c9eb95728489b0b81c2b90c9
Tomasz Subik <tsubik@gmail.com>
2021-11-19 11:43:55 +0100

    refactor import script

--- app/services/import_country_profile.rb
+++ app/services/import_country_profile.rb
@@ -17,12 +17,12 @@ class ImportCountryProfile
       cleanup
       import_indicators(COUNTRY_KEY_FILEPATH)
 
-      import_subnational_cities(SUBNATIONAL_CITIES_FILEPATH)
-      import_subnational_companies(SUBNATIONAL_COMPANIES_FILEPATH)
-      import_subnational_count(SUBNATIONAL_COUNT_FILEPATH)
+      import_time_series(SUBNATIONAL_CITIES_FILEPATH, :city_badge_type)
+      import_time_series(SUBNATIONAL_COMPANIES_FILEPATH, :company_target_qualification)
 
-      import_country_context(COUNTRY_CONTEXT_FILEPATH)
-      import_country_adaptation(COUNTRY_ADAPTATION_FILEPATH)
+      import_multiple_columns_file(SUBNATIONAL_COUNT_FILEPATH)
+      import_multiple_columns_file(COUNTRY_CONTEXT_FILEPATH)
+      import_multiple_columns_file(COUNTRY_ADAPTATION_FILEPATH)
     end
   end
 
@@ -48,11 +48,14 @@ class ImportCountryProfile
         slug: row[:column_name]
       )
     end
-    @indicators = CountryProfile::Indicator.all.map { |i| [i.slug, i] }.to_h
   end
 
-  def import_subnational_cities(path)
-    indicator = CountryProfile::Indicator.find_by(slug: :badge_type)
+  def import_time_series(path, indicator_slug)
+    indicator = CountryProfile::Indicator.find_by(slug: indicator_slug)
+    if indicator.nil?
+      raise "#{File.basename(path)} Indicator #{indicator_slug} does not exist, check country_key file"
+    end
+
     values = []
 
     import_each_with_logging(S3CSVReader.read(path), path) do |row|
@@ -63,7 +66,7 @@ class ImportCountryProfile
         v.location = find_location(row[:iso])
         v.year = year
         v.value = value
-        v.category = row[:badge_type]
+        v.category = row[indicator_slug.to_sym]
 
         v.validate!
 
@@ -74,81 +77,14 @@ class ImportCountryProfile
     CountryProfile::Value.import! values, all_or_none: true
   end
 
-  def import_subnational_companies(path)
-    indicator = CountryProfile::Indicator.find_by(slug: :target_qualification)
+  def import_multiple_columns_file(path)
+    basename = File.basename(path)
+    indicators = CountryProfile::Indicator.where(file: basename)
+
     values = []
 
     import_each_with_logging(S3CSVReader.read(path), path) do |row|
-      parse_years(row).each do |year, value|
-        v = CountryProfile::Value.new
-
-        v.indicator = indicator
-        v.location = find_location(row[:iso])
-        v.year = year
-        v.value = value
-        v.category = row[:target_qualification]
-
-        v.validate!
-
-        values << v
-      end
-    end
-
-    CountryProfile::Value.import! values, all_or_none: true
-  end
-
-  def import_subnational_count(path)
-    values = []
-
-    import_each_with_logging(S3CSVReader.read(path), path) do |row|
-      %w[city_commited city_ppl company_target company_commited].each do |indicator|
-        values << build_value(row, indicator)
-      end
-    end
-
-    CountryProfile::Value.import! values.compact, all_or_none: true
-  end
-
-  def import_country_context(path)
-    values = []
-
-    import_each_with_logging(S3CSVReader.read(path), path) do |row|
-      %w[emissions_total
-         emissions_capita
-         emissions_gdp
-         emissions_total_rank
-         emissions_capita_rank
-         emissions_gdp_rank
-         population
-         gdp_capita
-         population_rank
-         gdp_capita_rank].each do |indicator|
-        values << build_value(row, indicator)
-      end
-    end
-
-    CountryProfile::Value.import! values.compact, all_or_none: true
-  end
-
-  def import_country_adaptation(path)
-    values = []
-
-    import_each_with_logging(S3CSVReader.read(path), path) do |row|
-      %w[vulnerability
-         vulnerability_rank
-         readiness
-         readiness_rank
-         hazard_5
-         hazard_2
-         hazard_3
-         hazard_7
-         hazard_1
-         hazard_4
-         hazard_8
-         hazard_6
-         hazard_9
-         hazard_10
-         hazard_11].each do |indicator|
+      indicators.each do |indicator|
         values << build_value(row, indicator)
       end
     end
@@ -157,12 +93,14 @@ class ImportCountryProfile
   end
 
   def build_value(row, indicator)
-    value = row[indicator.to_sym]
+    raise "Column for #{indicator.slug} does not exist" unless row.headers.include?(indicator.slug.to_sym)
+
+    value = row[indicator.slug.to_sym]
     return if value.blank?
 
     value = CountryProfile::Value.new(
       location: find_location(row[:iso] || row[:country]),
-      indicator: find_indicator(indicator),
+      indicator: indicator,
       value: value
     )
     value.validate!
@@ -173,12 +111,6 @@ class ImportCountryProfile
     return if iso.blank?
 
     @locations[iso] or raise ActiveRecord::RecordNotFound, "Cannot find location with iso code #{iso}"
-  end
-
-  def find_indicator(slug)
-    return if slug.blank?
-
-    @indicators[slug] or raise ActiveRecord::RecordNotFound, "Cannot find indicator with code #{slug}"
   end
 
   def parse_years(row)
