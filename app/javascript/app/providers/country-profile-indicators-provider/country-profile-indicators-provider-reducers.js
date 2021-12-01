96e0fee4885b1b445334859b21bbe83b55013532
Tomasz Subik <tsubik@gmail.com>
2021-12-01 19:08:22 +0100

    keep previous values for indicators

--- app/javascript/app/providers/country-profile-indicators-provider/country-profile-indicators-provider-reducers.js
+++ app/javascript/app/providers/country-profile-indicators-provider/country-profile-indicators-provider-reducers.js
@@ -1,3 +1,7 @@
+import keyBy from 'lodash/keyBy';
+import mergeWith from 'lodash/mergeWith';
+import union from 'lodash/union';
+
 export const initialState = {
   loading: false,
   loaded: false,
@@ -11,7 +15,23 @@ const setError = (state, error) => ({ ...state, error });
 
 export default {
   fetchIndicatorsInit: state => setLoading(true, state),
-  fetchIndicatorsReady: (state, { payload }) =>
-    setLoaded(true, setLoading(false, { ...state, data: payload })),
+  fetchIndicatorsReady: (state, { payload }) => {
+    const byIndicator = keyBy(payload, 'slug');
+    const newData = mergeWith(
+      state.data,
+      byIndicator,
+      (oldValues, newValues, key) => {
+        if (key === 'values') {
+          return union(oldValues, newValues);
+        }
+        return undefined;
+      }
+    );
+    const newState = {
+      ...state,
+      data: newData
+    };
+    return setLoaded(true, setLoading(false, newState));
+  },
   fetchIndicatorsFail: state => setLoading(setError(state, true), false)
 };
