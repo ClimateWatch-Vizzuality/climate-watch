dc21f17474c49a7e63b1876c291252b82aaf1e87
Tomasz Subik <tsubik@gmail.com>
2021-12-03 13:34:31 +0100

    fix country profile providers

--- app/javascript/app/providers/country-profile-indicators-provider/country-profile-indicators-provider.js
+++ app/javascript/app/providers/country-profile-indicators-provider/country-profile-indicators-provider.js
@@ -10,10 +10,12 @@ export { default as actions } from './country-profile-indicators-provider-action
 
 const CountryProfileIndicatorsProvider = props => {
   const { fetchIndicators, indicatorSlugs, locations } = props;
+  const slugs = indicatorSlugs.join(',');
+  const locs = locations.join(',');
 
   useEffect(() => {
     fetchIndicators({ indicatorSlugs, locations });
-  }, []);
+  }, [slugs, locs]);
 
   return null;
 };
