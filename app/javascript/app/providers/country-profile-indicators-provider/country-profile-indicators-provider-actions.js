3d802c9c03a2496c0fd2282310f0e8b093721ea7
Tomasz Subik <tsubik@gmail.com>
2021-12-03 13:34:31 +0100

    fix country profile providers

--- app/javascript/app/providers/country-profile-indicators-provider/country-profile-indicators-provider-actions.js
+++ app/javascript/app/providers/country-profile-indicators-provider/country-profile-indicators-provider-actions.js
@@ -1,6 +1,5 @@
 import { createAction } from 'redux-actions';
 import { createThunkAction } from 'utils/redux';
-import isEmpty from 'lodash/isEmpty';
 
 const fetchIndicatorsInit = createAction('fetchIndicatorsInit');
 const fetchIndicatorsReady = createAction('fetchIndicatorsReady');
@@ -8,39 +7,33 @@ const fetchIndicatorsFail = createAction('fetchIndicatorsFail');
 
 const fetchIndicators = createThunkAction(
   'fetchIndicators',
-  ({ indicatorSlugs = [], locations = [] } = {}) => (dispatch, state) => {
-    const { countryProfileIndicators } = state();
-    if (
-      isEmpty(countryProfileIndicators.data) &&
-      !countryProfileIndicators.loading
-    ) {
-      dispatch(fetchIndicatorsInit());
+  ({ indicatorSlugs = [], locations = [] } = {}) => dispatch => {
+    dispatch(fetchIndicatorsInit());
 
-      const params = new URLSearchParams();
-      if (indicatorSlugs?.length > 0) {
-        params.append('indicator', indicatorSlugs.join(','));
-      }
-      if (locations?.length > 0) params.append('location', locations.join(','));
-      const queryString =
-        Array.from(params).length > 0 ? `?${params.toString()}` : '';
-
-      fetch(`/api/v1/country_profile/indicators${queryString}`)
-        .then(response => {
-          if (response.ok) return response.json();
-          throw Error(response.statusText);
-        })
-        .then(data => {
-          if (data && data.data) {
-            dispatch(fetchIndicatorsReady(data.data));
-          } else {
-            dispatch(fetchIndicatorsReady([]));
-          }
-        })
-        .catch(error => {
-          console.warn(error);
-          dispatch(fetchIndicatorsFail());
-        });
+    const params = new URLSearchParams();
+    if (indicatorSlugs?.length > 0) {
+      params.append('indicator', indicatorSlugs.join(','));
     }
+    if (locations?.length > 0) params.append('location', locations.join(','));
+    const queryString =
+      Array.from(params).length > 0 ? `?${params.toString()}` : '';
+
+    fetch(`/api/v1/country_profile/indicators${queryString}`)
+      .then(response => {
+        if (response.ok) return response.json();
+        throw Error(response.statusText);
+      })
+      .then(data => {
+        if (data && data.data) {
+          dispatch(fetchIndicatorsReady(data.data));
+        } else {
+          dispatch(fetchIndicatorsReady([]));
+        }
+      })
+      .catch(error => {
+        console.warn(error);
+        dispatch(fetchIndicatorsFail());
+      });
   }
 );
 
