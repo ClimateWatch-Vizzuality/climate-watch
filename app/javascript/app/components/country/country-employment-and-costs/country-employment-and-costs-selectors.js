3c5dd4534bf404b056ca20b7c413cf09d29e576c
Alvaro leal <pursuance@gmail.com>
2022-01-18 14:01:49 +0100

    Finish employment and costs section. Missing real data

--- app/javascript/app/components/country/country-employment-and-costs/country-employment-and-costs-selectors.js
+++ app/javascript/app/components/country/country-employment-and-costs/country-employment-and-costs-selectors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-confusing-arrow */
 import { createSelector } from 'reselect';
 import isEmpty from 'lodash/isEmpty';
-// import { CHART_NAMED_EXTENDED_COLORS } from 'app/styles/constants';
+import { CHART_NAMED_EXTENDED_COLORS } from 'app/styles/constants';
 
 const getCountryIndicators = state =>
   state.countryProfileIndicators.data || null;
@@ -11,9 +11,25 @@ export const getSectionData = createSelector(
   countryIndicators => {
     const hasCountryIndicators = !isEmpty(countryIndicators);
     if (!countryIndicators || !hasCountryIndicators) return null;
+    const employment = [
+      { name: 'Oil and gas technologies', value: 900 },
+      { name: 'All renewable', value: 843.23 }
+    ];
+    const employmentMax = Math.max(...employment.map(e => e.value));
+    const costs = [{ name: 'costs', value: 220 }];
+    const costsMax = Math.max(...costs.map(e => e.value));
+    const colors = Object.values(CHART_NAMED_EXTENDED_COLORS);
     return {
-      employment: {},
-      costs: {}
+      employment: employment.map((e, i) => ({
+        ...e,
+        percentage: e.value && (e.value / employmentMax) * 100,
+        color: colors[i]
+      })),
+      costs: costs.map((e, i) => ({
+        ...e,
+        percentage: (e.value / costsMax) * 100,
+        color: colors[i]
+      }))
     };
   }
 );
