837c5164d97d12ae3ecf745d60ce49deec5671b8
Bluesmile82 <pursuance@gmail.com>
2022-11-16 18:02:12 +0100

    Add google tag manager

--- app/javascript/app/layouts/root/root.js
+++ app/javascript/app/layouts/root/root.js
@@ -2,11 +2,17 @@ import { PureComponent } from 'react';
 import Proptypes from 'prop-types';
 import { withRouter } from 'react-router';
 import ReactGA from 'react-ga';
+import TagManager from 'react-gtm-module';
 import { renderRoutes } from 'react-router-config';
 import './root-styles.scss';
 
 // Do not destructure this
 const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
+const GOOGLE_TAG_MANAGER_ID = process.env.GOOGLE_TAG_MANAGER_ID;
+
+const tagManagerArgs = {
+  gtmId: GOOGLE_TAG_MANAGER_ID
+};
 
 function trackPage(page) {
   ReactGA.set({ page });
@@ -14,9 +20,12 @@ function trackPage(page) {
 }
 
 let gaInitialized = false;
+let gtmInitialized = false;
+
 function handleTrack(location, prevLocation) {
   if (GOOGLE_ANALYTICS_ID) {
     if (!gaInitialized) {
+      TagManager.initialize(tagManagerArgs);
       ReactGA.initialize(GOOGLE_ANALYTICS_ID);
       gaInitialized = true;
     }
@@ -30,6 +39,11 @@ function handleTrack(location, prevLocation) {
         trackPage(page);
       }
     }
+  }
+
+  if (GOOGLE_TAG_MANAGER_ID && !gtmInitialized) {
+    TagManager.initialize(tagManagerArgs);
+    gtmInitialized = true;
   }
 }
 
