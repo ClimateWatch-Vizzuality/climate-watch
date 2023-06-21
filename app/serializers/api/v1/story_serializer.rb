c29585ecd0607f904d76b70626bab165a25bbff7
martintomas <martintomas.it@gmail.com>
2023-06-21 17:27:30 +0200

    refactoring: Resize background images returned by WRI Stories

--- app/serializers/api/v1/story_serializer.rb
+++ app/serializers/api/v1/story_serializer.rb
@@ -6,6 +6,10 @@ module Api
       attribute :background_image_url
       attribute :tags
       attribute :published_at
+
+      def background_image_url
+        object.resized_background_image_url
+      end
     end
   end
 end
--- spec/controllers/api/v1/stories_controller_spec.rb
+++ spec/controllers/api/v1/stories_controller_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe Api::V1::StoriesController, type: :controller do
   context do
-    let!(:some_stories) {
-      FactoryBot.create_list(:story, 7)
-    }
+    let!(:some_stories) { FactoryBot.create_list(:story, 7) }
+    let!(:story_with_wri_background_image) do
+      create :story, background_image_url: 'https://files.wri.org/d8/s3fs-public/2023-03/ipcc-flooding_0.jpg'
+    end
 
     describe 'GET index' do
       it 'returns a successful 200 response' do
@@ -16,6 +17,13 @@ describe Api::V1::StoriesController, type: :controller do
         get :index, params: {limit: 6}
         parsed_body = JSON.parse(response.body)
         expect(parsed_body.length).to eq(6)
+      end
+
+      it 'returns resized background image url for WRI images' do
+        get :index
+        parsed_body = JSON.parse response.body
+        response = parsed_body.find { |story| story['title'] == story_with_wri_background_image.title }
+        expect(response['background_image_url']).to include('https://files.wri.org/d8/s3fs-public/styles/500x300/s3/2023-03/ipcc-flooding_0.jpg')
       end
     end
   end
