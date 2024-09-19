256296e537233d9618906289e9d9e33ce4ca3d54
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-09-19 12:00:30 +0100

    Bring back the 2025 tracker chart component section

--- app/javascript/app/routes/app-routes/ndcs-enhancements-2025-sections/ndcs-enhancements-2025-sections.js
+++ app/javascript/app/routes/app-routes/ndcs-enhancements-2025-sections/ndcs-enhancements-2025-sections.js
@@ -1,7 +1,14 @@
+import NDCSEnhancements2025TrackerChart from 'components/ndcs/ndcs-enhancements-2025-tracker-chart';
 import NDCSEnhancements2025Map from 'components/ndcs/ndcs-enhancements-2025-map';
 import NDCSEnhancements2025Table from 'components/ndcs/ndcs-enhancements-2025-table';
 
 export default [
+  {
+    hash: 'chart',
+    anchor: false,
+    label: 'Chart',
+    component: NDCSEnhancements2025TrackerChart
+  },
   {
     hash: 'map',
     anchor: true,
