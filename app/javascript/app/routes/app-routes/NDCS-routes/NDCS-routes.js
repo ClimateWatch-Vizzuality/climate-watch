206db25c4848f64c5d5c723c8ab0889d8ae50bda
Simão Rodrigues <arodrigues.simao@gmail.com>
2024-11-20 11:52:47 +0000

    Add NDC Tracker to the commitments dropdown

--- app/javascript/app/routes/app-routes/NDCS-routes/NDCS-routes.js
+++ app/javascript/app/routes/app-routes/NDCS-routes/NDCS-routes.js
@@ -23,11 +23,11 @@ export default [
     label: 'NDC Enhancement Tracker',
     activeId
   },
-  // {
-  //   path: '/ndc-tracker',
-  //   label: 'NDC Tracker',
-  //   activeId
-  // },
+  {
+    path: '/ndc-tracker',
+    label: 'NDC Tracker',
+    activeId
+  },
   {
     path: '/ndcs-sdg',
     label: 'EXPLORE NDC-SDG LINKAGES',
