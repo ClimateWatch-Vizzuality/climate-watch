6cb7197571cceeba12c9504152750b275e2867e5
Simão Rodrigues <arodrigues.simao@gmail.com>
2024-12-06 11:42:05 +0000

    Hide certain columns from the CompareAllTargets module table - no longer to be used

--- app/javascript/app/data/country-documents.js
+++ app/javascript/app/data/country-documents.js
@@ -1,12 +1,12 @@
 export const DOCUMENT_COLUMNS_SLUGS = {
-  'Pre-2020 Pledge': 'pledges',
+  // 'Pre-2020 Pledge': 'pledges',
   INDC: 'indc',
   NDC: 'first_ndc',
   'Updated First NDC': 'revised_first_ndc',
   '2nd NDC': 'second_ndc',
-  LTS: 'lts',
-  'Climate Framework Laws or Policies': 'framework',
-  'Sectoral Laws or Policies': 'sectoral'
+  LTS: 'lts'
+  // 'Climate Framework Laws or Policies': 'framework',
+  // 'Sectoral Laws or Policies': 'sectoral'
 };
 
 // Needed for sorting
