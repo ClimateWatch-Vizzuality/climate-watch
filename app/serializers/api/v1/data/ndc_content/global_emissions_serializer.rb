11ed8408f3a25fb7f25e9e0eba0735394bc090fb
leo guercio <lpguercio@gmail.com>
2025-01-14 13:34:46 +0100

    Add global emissions importer and controller

---
+++ app/serializers/api/v1/data/ndc_content/global_emissions_serializer.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    module Data
+      module NdcContent
+        class GlobalEmissionsSerializer < ActiveModel::Serializer
+          attributes :id,
+                     :year,
+                     :historical_emissions,
+                     :current_policies_scenario,
+                     :ndcs_conditional_2020,
+                     :ndcs_unconditional_2020,
+                     :ndcs_conditional_2025,
+                     :ndcs_unconditional_2025,
+                     :target_2c,
+                     :target_1_5c,
+                     :created_at,
+                     :updated_at
+        end
+      end
+    end
+  end
+end
