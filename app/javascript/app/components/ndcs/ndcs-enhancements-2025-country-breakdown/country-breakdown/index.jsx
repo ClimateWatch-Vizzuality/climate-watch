434bdbdf15bd07e14204dd8a0789e7530aa97bd3
Simão Rodrigues <arodrigues.simao@gmail.com>
2025-01-22 11:33:29 +0000

    Connect global and country iconic visual charts to the data

--- app/javascript/app/components/ndcs/ndcs-enhancements-2025-country-breakdown/country-breakdown/index.jsx
+++ app/javascript/app/components/ndcs/ndcs-enhancements-2025-country-breakdown/country-breakdown/index.jsx
@@ -1,3 +1,15 @@
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+
+import { getData } from './selectors';
 import CountryBreakdownComponent from './component';
 
-export default CountryBreakdownComponent;
+const mapStateToProps = state => ({
+  data: getData(state)
+});
+
+const actions = {};
+
+export default withRouter(
+  connect(mapStateToProps, actions)(CountryBreakdownComponent)
+);
--- app/javascript/app/components/ndcs/ndcs-enhancements-2025-country-breakdown/global-view/index.jsx
+++ app/javascript/app/components/ndcs/ndcs-enhancements-2025-country-breakdown/global-view/index.jsx
@@ -1,6 +1,15 @@
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
 
+import { getData } from './selectors';
 import GlobalViewComponent from './component';
+
+const mapStateToProps = state => ({
+  data: getData(state)
+});
+
+const actions = {};
 
 export const chartConfigPropTypes = {
   chartId: PropTypes.string.isRequired,
@@ -21,4 +30,6 @@ export const chartConfigPropTypes = {
   })
 };
 
-export default GlobalViewComponent;
+export default withRouter(
+  connect(mapStateToProps, actions)(GlobalViewComponent)
+);
