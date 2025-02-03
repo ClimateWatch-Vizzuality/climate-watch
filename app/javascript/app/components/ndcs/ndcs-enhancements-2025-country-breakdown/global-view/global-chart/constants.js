e467134b4ecb987922a7e2d7c374854ac135858b
Simão Rodrigues <arodrigues.simao@gmail.com>
2025-02-03 15:07:01 +0000

    Style global tooltips colors to match their respective display

--- app/javascript/app/components/ndcs/ndcs-enhancements-2025-country-breakdown/global-view/global-chart/constants.js
+++ app/javascript/app/components/ndcs/ndcs-enhancements-2025-country-breakdown/global-view/global-chart/constants.js
@@ -2,18 +2,21 @@ export const TOOLTIPS = {
   historical: {
     marker: {
       id: 'global-historical-marker-tooltip',
-      label: '2021 - Historical Emissions'
+      label: '2021 - Historical Emissions',
+      color: '#999C9F'
     }
   },
   targets: {
     2035: {
       '2.0C': {
         id: 'global-targets-2035-2_0C-tooltip',
-        label: '2035 - 2°C Target'
+        label: '2035 - 2°C Target',
+        color: '#579B7D'
       },
       '1.5C': {
         id: 'global-targets-2035-1_5-tooltipC',
-        label: '2035 - 1.5°C Target'
+        label: '2035 - 1.5°C Target',
+        color: '#8CB73F'
       }
     }
   },
@@ -21,30 +24,36 @@ export const TOOLTIPS = {
     emissionsReductions: {
       id: 'global-emissions-reductions-tooltip',
       label: 'Emissions Reductions from 2020 NDCs',
+      color: '#8F8F9F',
       markers: {
         upperLimit: {
           id: 'global-emissions-reductions-upper-limit-tooltip',
-          label: '2030 - BAU'
+          label: '2030 - BAU',
+          color: '#8F8F9F'
         },
         lowerLimit: {
           id: 'global-emissions-reductions-lower-limit-tooltip',
-          label: '2030 - Unconditional 2020 NDC'
+          label: '2030 - Unconditional 2020 NDC',
+          color: '#8F8F9F'
         }
       }
     },
     additionalReductions: {
       id: 'global-emissions-additional-reductions-tooltip',
-      label: '2035 - Emissions Reductions from 2025 NDCs'
+      label: '2035 - Emissions Reductions from 2025 NDCs',
+      color: '#0845CB'
     }
   },
   targetGaps: {
     upperLimit: {
       id: 'global-target-gaps-upper-limit-tooltip',
-      label: '2035 - Emissions Gap to 2°C'
+      label: '2035 - Emissions Gap to 2°C',
+      color: '#579B7D'
     },
     lowerLimit: {
       id: 'global-target-gaps-lower-limit-tooltip',
-      label: '2035 - Emissions Gap to 1.5°C'
+      label: '2035 - Emissions Gap to 1.5°C',
+      color: '#8CB73F'
     }
   }
 };
