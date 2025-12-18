c18f006d052e39e5304dd15a17eeb07728c8efb0
leo guercio <lpguercio@gmail.com>
2025-12-18 15:35:07 +0100

    Add rescue and logging for failing endpoint

--- app/services/single_record_fetcher.rb
+++ app/services/single_record_fetcher.rb
@@ -10,8 +10,31 @@ class SingleRecordFetcher
   def call
     Rails.cache.fetch("#{cache_key}/laws_and_policies", expires: 7.days) do
       path = [url, id].compact.join('/')
-      response = Net::HTTP.get(URI(path))
-      JSON.parse(response)
+      
+      begin
+        uri = URI(path)
+        response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https', 
+                                    open_timeout: 10, read_timeout: 10) do |http|
+          request = Net::HTTP::Get.new(uri)
+          http.request(request)
+        end
+
+        unless response.is_a?(Net::HTTPSuccess)
+          Rails.logger.error("SingleRecordFetcher API error for #{path}: #{response.code} - #{response.message}")
+          return {}
+        end
+
+        JSON.parse(response.body)
+      rescue JSON::ParserError => e
+        Rails.logger.error("SingleRecordFetcher JSON parse error for #{path}: #{e.message}")
+        {}
+      rescue Net::OpenTimeout, Net::ReadTimeout => e
+        Rails.logger.error("SingleRecordFetcher timeout for #{path}: #{e.message}")
+        {}
+      rescue StandardError => e
+        Rails.logger.error("SingleRecordFetcher unexpected error for #{path}: #{e.class} - #{e.message}")
+        {}
+      end
     end
   end
 end
